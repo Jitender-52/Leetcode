40795bc7becb4b7894f440f4003f2aa297a76eac
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-01-09 08:22:19 +0530

    Time: 242 ms (19.16%), Space: 12.5 MB (70.78%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,107 +1,110 @@
 class Solution {
 public:
+    
+//     Runtime: 62 ms, faster than 61.06% of C++ online submissions for Max Points on a Line.
+// Memory Usage: 12.5 MB, less than 66.56% of C++ online submissions for Max Points on a Line.
    
-        int maxPoints(vector<vector<int>> &points){
-            int n = points.size();
-            int ans = 1;
-            for(int i = 0; i < n-1; i++)
-            {
-                map<double,int> mp;
-                for(int j = i+1; j < n; j++)
-                {
-                    if(points[i][0] == points[j][0])
-                    {
-                        mp[1e9]++;
-                    }
-                    else
-                    {
-                        double d = double(points[i][1] - points[j][1]) / double(points[i][0] - points[j][0]);
-                        mp[d]++;
-                    }
-                    for(auto i : mp)
-                        {
-                            ans = max(ans, i.second + 1);
-                        }
-                }
-            }
-            
-            return ans;
-        }
-};
-    
-    
-//     int maxPoints(vector<vector<int>> &points){
-//         int n = points.size();
-        
-//         set<pair<double,double>> st;
-        
-//         for(int i = 0; i < n; i++)
-//         {
-//             for(int j = i+1; j < n; j++)
+//         int maxPoints(vector<vector<int>> &points){
+//             int n = points.size();
+//             int ans = 1;
+//             for(int i = 0; i < n-1; i++)
 //             {
-//                 double x1 = (double)points[i][0];
-//                 double y1 = (double)points[i][1];
-//                 double x2 = (double)points[j][0];
-//                 double y2 = (double)points[j][1];
-                
-//                 double m;
-//                 if(x2 - x1 == 0)
+//                 map<double,int> mp;
+//                 for(int j = i+1; j < n; j++)
 //                 {
-//                     m = 1e9;
-//                 }
-//                 else
-//                 {
-//                     m = (double(y2 - y1)) / (x2-x1);
-//                 }
-//                 double c = double(y1) - (m*x1); 
-//                 if(m == 1e9)
-//                 {
-//                     c = x1;
-//                 }
-//                 st.insert({m, c});
-//             }
-//         }
-        
-//         int ans = 1;
-//         cout << endl;
-//         for(auto i : st)
-//         {
-//             cout << i.first << " " << i.second << endl;
-//             int count = 0;
-//             for(int j = 0; j < n; j++)
-//             {
-//                 double x = (double)points[j][0];
-//                 double y = (double)points[j][1];
-//                 double m = i.first;
-//                 double c = i.second;
-//                 if(m == 1e9)
-//                 {
-//                     if(x == c)
+//                     if(points[i][0] == points[j][0])
 //                     {
-//                         count++;
+//                         mp[1e9]++;
 //                     }
-//                 }
-                
-//                 // cout << y << " " << m * x + c << endl;
-                
-//                 double a = (double)y;
-//                 double b = (double)m * (double)x + (double)c;
-
-//                 if(a == b)
-//                 {
-//                     cout << a << " and " << b << " are equal" << endl;
-//                     count++;
-//                 }
-//                 else
-//                 {
-//                     cout << a << " and " << b << " are not equal" << endl; 
+//                     else
+//                     {
+//                         double d = double(points[i][1] - points[j][1]) / double(points[i][0] - points[j][0]);
+//                         mp[d]++;
+//                     }
+//                     for(auto i : mp)
+//                         {
+//                             ans = max(ans, i.second + 1);
+//                         }
 //                 }
 //             }
-//             ans = max(ans, count);
+            
+//             return ans;
 //         }
-//     return ans;
-//     }
 // };
+
+    
+    
+//     Whats Wrong with this code with test cases
+// [[1,1],[2,2],[3,3]]
+// [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+// [[0,1],[0,0],[0,4],[0,-2],[0,-1],[0,3],[0,-4]]
+// [[0,0],[1,-1],[1,1]]
+// [[0,0]]
+// [[-6,-1],[3,1],[12,3]] // with this last testcase
+    
+    
+    
+   int maxPoints(vector<vector<int>> &points){
+        int n = points.size();
+        
+        set<pair<double,double>> st;
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i+1; j < n; j++)
+            {
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                
+                double m;
+                if(x2 - x1 == 0)
+                {
+                    m = 1e6;
+                }
+                else
+                {
+                    m = (double(y2 - y1)) / (x2-x1);
+                }
+                double c = double(y1) - (m*x1); 
+                if(m == 1e6)
+                {
+                    c = x1;
+                }
+                st.insert({m, c});
+            }
+        }
+        
+        int ans = 1;
+        for(auto i : st)
+        {
+            // cout << i.first << " " << i.second << endl;
+            int count = 0;
+            for(int j = 0; j < n; j++)
+            {
+                double x = points[j][0];
+                double y = points[j][1];
+                double m = i.first;
+                double c = i.second;
+                if(m == 1e6)
+                {
+                    if(x == c)
+                    {
+                        count++;
+                    }
+                }
+                // else if(y == (m*x + c))
+                else if(abs(y - (m*x + c)) <= 1e-6)
+                {
+                    count++;
+                }
+                ans = max(ans, count);
+            }
+        }
+    return ans;
+    }
+};
     
     
     
