be4175ba1cbd94912f2131de31d08aecb8114577
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-01-20 18:52:37 +0530

    Time: 136 ms (33.66%), Space: 34.1 MB (30.54%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     
+//     Runtime: 667 ms, faster than 5.09% of C++ online submissions for Non-decreasing Subsequences.
+// Memory Usage: 102.4 MB, less than 8.21% of C++ online submissions for Non-decreasing Subsequences.
+    
+    
+    
+    
     void find(int i, vector<int> &nums, vector<int> v, map<vector<int>, int> &mp)
     {
         int n = nums.size();
@@ -11,17 +17,22 @@ public:
         if(i >= n)
             return;
         
-        find(i + 1, nums, v, mp);
+        // find(i + 1, nums, v, mp);
         
-        if(nums[i] >= v[v.size()-1])
-            v.push_back(nums[i]);
+//         if(nums[i] >= v[v.size()-1])
+//             v.push_back(nums[i]);
         
-        find(i+1, nums, v, mp);
+//         find(i+1, nums, v, mp);
         
-        // for(int  j = i; j < n; j++)
-        // {
-        //     find(j+1, nums, v, mp);
-        // }
+        for(int  j = i; j < n; j++)
+        {
+            if(nums[j] >= v[v.size() - 1])
+            {
+                v.push_back(nums[j]);
+                find(j+1, nums, v, mp);
+                v.pop_back();
+            }
+        }
     }
     
     vector<vector<int>> findSubsequences(vector<int>& nums) {
