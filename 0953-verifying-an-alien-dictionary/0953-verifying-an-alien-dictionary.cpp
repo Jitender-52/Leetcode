ba9494e662ae001b4e1529d51b9a53d1f78d01eb
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-02 19:45:48 +0530

    Time: 6 ms (65.24%), Space: 9.7 MB (25.79%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    
+    // What is the difference betweeen this code and down one this one is copied and down one is mine
+    
+      bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> map;
+        for (int i = 0; i < order.length(); i++) {
+            map[order[i]] = i;
+        }
+        for (int i = 1; i < words.size(); i++) {
+            string first = words[i - 1];
+            string second = words[i];
+            int n = min(first.length(), second.length());
+            bool flag = false;
+            for (int j = 0; j < n; j++) {
+                if (map[first[j]] < map[second[j]]) {
+                    flag = true;
+                    break;
+                }
+                else if (map[first[j]] > map[second[j]]) {
+                    return false;
+                }
+            }
+            if (!flag && first.length() > second.length()) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+    
+//     bool isAlienSorted(vector<string>& words, string order) {
+//         int n = words.size();
+//         map<char,int> mp;
+//         for(int i = 0; i < order.size(); i++)
+//         {
+//             mp[order[i]] = i;
+//         }
+        
+//         for(int i = 1; i < n; i++)
+//         {
+//             bool check = false;
+//             for(int j = 0; j < min(words[i-1].length(), words[i].length()); j++)
+//             {
+//                 // cout << mp[words[i-1][j]] << " " << mp[words[i][j]] << endl;
+//                  if(mp[words[i-1][j]] < mp[words[i][j]])
+//                 {
+//                     check = true;
+//                     break;
+//                 }
+//                 // else if(mp[words[i-1][j]] > words[i][j])
+//                 else if(mp[words[i][j]] > mp[words[i-1][j]])
+//                     return false;
+//                 // else if(mp[words[i-1][j]] < mp[words[i][j]])
+//                 // {
+//                 //     check = true;
+//                 //     break;
+//                 // }
+//             }
+//             if(!check && words[i].length() < words[i-1].length())
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
+
