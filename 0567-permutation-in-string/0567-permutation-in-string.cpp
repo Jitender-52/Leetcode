11e9b513b096dd6c92ae29591b80df27bba4d966
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-04 10:44:35 +0530

    Time: 1293 ms (6.80%), Space: 81.9 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+    
+//     Runtime: 2265 ms, faster than 5.02% of C++ online submissions for Permutation in String.
+// Memory Usage: 93.3 MB, less than 5.01% of C++ online submissions for Permutation in String.
+    
+    
     bool checkInclusion(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
-        map<char,int> mp;
+        unordered_map<char,int> mp;
         for(int i = 0; i < n; i++)
         {
             mp[s1[i]]++;
         }
         for(int i = 0; i <= m - n; i++)
         {
-            map<char,int> mp1;
+            unordered_map<char,int> mp1;
             for(int j = i; j < i + n; j++)
             {
                 // cout << j <<" ";
