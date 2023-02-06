18084a58f9360a03e8a0c112b587b90774a2d1b4
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-06 15:07:34 +0530

    Time: 67 ms (33.87%), Space: 29.4 MB (37.74%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+    
+//     Runtime: 56 ms, faster than 66.77% of C++ online submissions for Check If It Is a Good Array.
+// Memory Usage: 29.4 MB, less than 37.74% of C++ online submissions for Check If It Is a Good Array.
+
+    
     bool isGoodArray(vector<int>& nums) {
         int n = nums.size();
         int a = nums[0];
         for(int i = 1; i < n; i++)
         {
-            a = __gcd(a, nums[i]);
+            a = gcd(a, nums[i]);
         }
         if(a == 1)
             return true;
