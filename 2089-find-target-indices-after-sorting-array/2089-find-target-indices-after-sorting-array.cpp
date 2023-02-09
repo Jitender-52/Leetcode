db945bdfd4c5ff41e5d726cfb299a7d85220844f
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-09 23:35:30 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (69.87%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,14 +1,43 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
+    
+     vector<int> targetIndices(vector<int>& nums, int target) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<int> ans;
+        int a = 0;
+        int b = 0;
         for(int i = 0; i < n; i++)
         {
-            while(i < n && nums[i] == target)
-                ans.push_back(i++);
+            if(nums[i] < target)
+                a++;
+            else if(nums[i] == target)
+                b++;
+        }
+        
+        vector<int> ans;
+        for(int i = 0; i < b; i++)
+        {
+            ans.push_back(a);
+            a++;
         }
         return ans;
     }
 };
+    
+    
+//     Runtime: 8 ms, faster than 53.51% of C++ online submissions for Find Target Indices After Sorting Array.
+// Memory Usage: 11.8 MB, less than 22.89% of C++ online submissions for Find Target Indices After Sorting Array.
+
+    
+    // vector<int> targetIndices(vector<int>& nums, int target) {
+    //     int n = nums.size();
+    //     sort(nums.begin(), nums.end());
+    //     vector<int> ans;
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         while(i < n && nums[i] == target)
+    //             ans.push_back(i++);
+    //     }
+    //     return ans;
+    // }
+    
+// };
