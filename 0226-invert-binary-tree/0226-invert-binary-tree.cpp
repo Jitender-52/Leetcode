6719f8738327885cfc138f1a9cb46c90582aefcd
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-18 17:27:55 +0530

    Time: 3 ms (62.83%), Space: 9.8 MB (46.85%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -11,24 +11,49 @@
  */
 class Solution {
 public:
-    TreeNode* invertTree(TreeNode* root) {
         
-        queue<TreeNode *> q;
-        if(root)
-            q.push(root);
-        
-        while(!q.empty())
+        void dfs(TreeNode* &root)
         {
-            TreeNode *front = q.front();
-            q.pop();
-            TreeNode *temp = front->left;
-            front->left = front->right;
-            front->right = temp;
-            if(front->left)
-                q.push(front->left);
-            if(front->right)
-                q.push(front->right);
+            if(!root)
+                return;
+            TreeNode *temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            dfs(root->left);
+            dfs(root->right);
         }
-        return root;   
-    }
+    
+        TreeNode* invertTree(TreeNode* root) {
+            dfs(root);
+            return root;
+        }
 };
+    
+    
+    
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Invert Binary Tree.
+// Memory Usage: 9.8 MB, less than 7.06% of C++ online submissions for Invert Binary Tree.
+
+    
+//     TreeNode* invertTree(TreeNode* root) {
+        
+//         queue<TreeNode *> q;
+//         if(root)
+//             q.push(root);
+        
+//         while(!q.empty())
+//         {
+//             TreeNode *front = q.front();
+//             q.pop();
+//             TreeNode *temp = front->left;
+//             front->left = front->right;
+//             front->right = temp;
+//             if(front->left)
+//                 q.push(front->left);
+//             if(front->right)
+//                 q.push(front->right);
+//         }
+//         return root;   
+//     }
+// };
