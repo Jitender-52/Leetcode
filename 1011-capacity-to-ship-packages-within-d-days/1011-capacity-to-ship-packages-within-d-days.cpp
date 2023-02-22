131903b1158fc1e54d40eaaedb9c47f650ab888d
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-22 07:11:40 +0530

    Time: 83 ms (25.11%), Space: 26.2 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     
-    bool isFeasible(vector<int> &weights, int maxi, int days)
+    
+     bool isFeasible(vector<int> &weights, int maxi, int days)
     {
         int n = weights.size();
         int count = 0;
@@ -19,7 +20,6 @@ public:
             sum += weights[i];
         }
         count++;
-        // cout << "count = " << count << endl;
         if(count <= days)
             return true;
         return false;
@@ -29,18 +29,17 @@ public:
     {
         int n = weights.size();
         int ans = INT_MAX;
-        int left = INT_MAX;
+        int left = INT_MIN;
         int right = 0;
         for(int i = 0; i < n; i++)
         {
-            left = min(left, weights[i]);
+            left = max(left, weights[i]);
             right += weights[i];
         }
     
         while(left <= right)
         {
             int mid = (left + right) / 2;
-            // cout << mid << endl;
             if(isFeasible(weights, mid, days))
             {
                 ans = min(ans, mid);
@@ -52,7 +51,6 @@ public:
             }
            
         }
-        // cout << endl;
         return ans;
     }
     
@@ -61,3 +59,71 @@ public:
         return minimum(weights, days);
     }
 };
+    
+    
+    
+    
+//     Runtime: 79 ms, faster than 31.73% of C++ online submissions for Capacity To Ship Packages Within D Days.
+// Memory Usage: 26.1 MB, less than 37.41% of C++ online submissions for Capacity To Ship Packages Within D Days.
+    
+    
+//     bool isFeasible(vector<int> &weights, int maxi, int days)
+//     {
+//         int n = weights.size();
+//         int count = 0;
+//         int sum = 0;
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(weights[i] > maxi)
+//                 return false;
+//             if(sum + weights[i] > maxi)
+//             {
+//                 sum = 0;
+//                 count++;
+//             }
+//             sum += weights[i];
+//         }
+//         count++;
+//         // cout << "count = " << count << endl;
+//         if(count <= days)
+//             return true;
+//         return false;
+//     }
+    
+//     int minimum(vector<int> &weights, int days)
+//     {
+//         int n = weights.size();
+//         int ans = INT_MAX;
+//         int left = INT_MAX;
+//         int right = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             left = min(left, weights[i]);
+//             right += weights[i];
+//         }
+    
+//         while(left <= right)
+//         {
+//             int mid = (left + right) / 2;
+//             // cout << mid << endl;
+//             if(isFeasible(weights, mid, days))
+//             {
+//                 ans = min(ans, mid);
+//                 right = mid - 1;
+//             }
+//             else
+//             {
+//                 left = mid + 1;
+//             }
+           
+//         }
+//         // cout << endl;
+//         return ans;
+//     }
+    
+//     int shipWithinDays(vector<int>& weights, int days) {
+//         int n = weights.size();
+//         return minimum(weights, days);
+//     }
+// };
