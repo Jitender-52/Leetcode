86c4fc90b7b9c5362778e21d9681c7f7b35e605b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-23 21:03:42 +0530

    Time: 205 ms (95.93%), Space: 82.1 MB (53.52%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        
+        vector<pair<int,int>> v;
+        for(int i = 0; i < n; i++)
+        {
+            v.push_back({capital[i], profits[i]});
+        }
+        
+        sort(v.begin(), v.end());
+        
+        priority_queue<int, vector<int>> pq;
+        
+        int j = 0;
+        for(int i = 0; i < k; i++)
+        {
+            while(j < n && v[j].first <= w)
+            {
+                pq.push(v[j].second);
+                j++;
+            }
+            if(pq.empty())
+                break;
+            w += pq.top();
+            pq.pop();
+        }
+        
+        return w;
+            
+//         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> pq;
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(profit[i] > capital[i])
+//                 pq.push({profit[i] - capital[i], capital[i]});
+//         }
+        
+//         while(k > 0 && !pq.empty())
+//         {
+//             pair<int,int> p = pq.top();
+//             pq.pop();
+//             if(p.second <= w)
+//             {
+//                 w -= p.second;
+//                 w += p.first;
+//                 k--;
+//             }
+//         }
+//         return w;
+    }
+};
