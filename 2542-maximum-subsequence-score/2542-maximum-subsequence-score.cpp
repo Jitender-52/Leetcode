8c62854cda38439d119f3d2c2d02df3b668de0cb
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-02-24 22:32:46 +0530

    Time: 273 ms (82.89%), Space: 92 MB (48.24%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -0,0 +1,126 @@
+class Solution {
+public:
+    
+        long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+            int n = nums1.size();
+            vector<pair<int,int>> v;
+            for(int i = 0; i < n; i++)
+            {
+                v.push_back({nums2[i], nums1[i]});
+            }
+            
+            sort(v.rbegin(), v.rend());
+            
+            long long sum = 0;
+            int mini = INT_MAX;
+            priority_queue<int, vector<int>, greater<int>> pq;
+            for(int i = 0; i < k; i++)
+            {
+                sum += v[i].second;
+                pq.push(v[i].second);
+                mini = min(mini, v[i].first);
+            }
+            
+            long long ans = sum * mini;
+            for(int i = k; i < n; i++)
+            {
+                sum -= pq.top();
+                pq.pop();
+                sum += v[i].second;
+                mini = min(mini, v[i].first);
+                pq.push(v[i].second);
+                ans = max(ans, sum * mini);
+            }
+            return ans;
+        }
+};
+    
+    
+    
+//     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+//         int n = nums1.size();
+        
+//         vector<pair<int,int>> v;
+        
+//         long long ans = 0;
+//         long long sum = 0;
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             v.push_back({nums1[i], i});
+//         }
+        
+//         vector<bool> vis(n, 0);
+        
+//         sort(v.rbegin(), v.rend());
+        
+//         int mini = INT_MAX;
+//         for(int i = 0; i < k-1; i++)
+//         {
+//             sum += v[i].first;
+//             mini = min(mini, nums2[v[i].second]);
+//             vis[v[i].second] = 1;
+//         }
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(vis[i] == 1)
+//             {
+//                 vis[i] = 0;
+//                 continue;
+//             }
+//             ans = max(ans, (sum + nums1[i]) * min(mini, nums2[i]));
+//         }
+        
+        
+//         // vis(n, 0);
+        
+//         sum = 0;
+        
+//         sort(v.begin(), v.end());
+        
+//         mini = INT_MAX;
+//         for(int i = 0; i < k-1; i++)
+//         {
+//             sum += v[i].first;
+//             mini = min(mini, nums2[v[i].second]);
+//             vis[v[i].second] = 1;
+//         }
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(vis[i] == 1)
+//                 continue;
+//             ans = max(ans, (sum + nums1[i]) * min(mini, nums2[i]));
+//         }
+        
+//         return ans;
+        
+// //         map<int,int> mp;
+
+// //         for(int i = 0; i < k; i++)
+// //         {
+// //             sum += nums1[i];
+// //             mp[nums2[i]]++;
+// //         }
+        
+// //         ans = sum * mp.begin()->first;
+// //         cout << ans << endl;
+// //         for(int i = k; i < n; i++)
+// //         {
+// //             cout << i << " " << i-k << endl;
+// //             sum += nums1[i];
+// //             sum -= nums1[i-k];
+// //             mp[nums2[i]]++;
+// //             mp[nums2[i-k]]--;
+// //             if(mp[nums2[i-k]] == 0)
+// //                 mp.erase(nums1[i-k]);
+// //             ans = max(ans, sum * mp.begin()->first);
+// //         }
+// //         return ans;        
+//     }
+// };
+
+// [2,1,14,12]
+// [11,7,13,6]
+// 3
