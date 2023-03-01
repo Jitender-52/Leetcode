9c299c5e5476e588b8f0d261c96086f2b52f3bc4
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-01 18:52:34 +0530

    Time: 50 ms (20.76%), Space: 56.5 MB (20.41%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -11,28 +11,53 @@
  */
 class Solution {
 public:
-
     
+        string duplicates(TreeNode *root, map<string, int> &mp, vector<TreeNode *> &ans)
+        {
+            if(!root)
+                return "*";
+            string left = duplicates(root->left, mp, ans);
+            string right = duplicates(root->right, mp, ans);
+            string s = left + "*" + right + "*" + to_string(root->val);
+            if(mp[s] == 1)
+            {
+                ans.push_back(root);
+            }
+            mp[s]++;
+            return s;
+        }
+
+        vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+            map<string,int> mp;
+            vector<TreeNode *> ans;
+            duplicates(root, mp, ans);
+            return ans;
+        } 
+};
+    
+    
+//     Runtime: 28 ms, faster than 75.69% of C++ online submissions for Find Duplicate Subtrees.
+// Memory Usage: 36.5 MB, less than 77.18% of C++ online submissions for Find Duplicate Subtrees.
     
 
     // Just copy paste for streak
     
-    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        unordered_map<string, int> cnt;
-        vector<TreeNode*> res;
-        function<string(TreeNode*)> traverse = [&cnt, &res, &traverse](TreeNode* node) -> string {
-            if (node == nullptr) {
-                return "";
-            }
-            string representation = "(" + traverse(node->left) + ")" + to_string(node->val) + "(" +
-                                    traverse(node->right) + ")";
-            cnt[representation]++;
-            if (cnt[representation] == 2) {
-                res.push_back(node);
-            }
-            return representation;
-        };
-        traverse(root);
-        return res;
-    }
-};
+//     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+//         unordered_map<string, int> cnt;
+//         vector<TreeNode*> res;
+//         function<string(TreeNode*)> traverse = [&cnt, &res, &traverse](TreeNode* node) -> string {
+//             if (node == nullptr) {
+//                 return "";
+//             }
+//             string representation = "(" + traverse(node->left) + ")" + to_string(node->val) + "(" +
+//                                     traverse(node->right) + ")";
+//             cnt[representation]++;
+//             if (cnt[representation] == 2) {
+//                 res.push_back(node);
+//             }
+//             return representation;
+//         };
+//         traverse(root);
+//         return res;
+//     }
+// };
