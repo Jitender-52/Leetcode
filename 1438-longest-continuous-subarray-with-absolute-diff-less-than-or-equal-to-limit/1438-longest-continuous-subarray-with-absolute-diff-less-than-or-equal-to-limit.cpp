64c19ae238ccea6f2dbe5ec45ecbc1ccf38e8682
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-04 18:06:46 +0530

    Time: 175 ms (56.80%), Space: 62.7 MB (41.53%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 274 ms, faster than 17.50% of C++ online submissions for Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.
+// Memory Usage: 77.8 MB, less than 33.75% of C++ online submissions for Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.
+    
+    
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
         map<int,int> mp;
@@ -21,22 +26,22 @@ public:
             // cout << mp.rbegin()->first << " " << mp.begin()->first << endl;
             ans = max(ans, i - j + 1);
         }
-        j = n-1;
-        mp.clear();
-        for(int i = n-1; i >= 0; i--)
-        {
-            mp[nums[i]]++;
-            while(mp.rbegin()->first - mp.begin()->first > limit)
-            {
-                mp[nums[j]]--;
-                if(mp[nums[j]] == 0)
-                {
-                    mp.erase(nums[j]);
-                }
-                j--;
-            }
-            ans = max(ans, j - i + 1);
-        }
+        // j = n-1;
+        // mp.clear();
+        // for(int i = n-1; i >= 0; i--)
+        // {
+        //     mp[nums[i]]++;
+        //     while(mp.rbegin()->first - mp.begin()->first > limit)
+        //     {
+        //         mp[nums[j]]--;
+        //         if(mp[nums[j]] == 0)
+        //         {
+        //             mp.erase(nums[j]);
+        //         }
+        //         j--;
+        //     }
+        //     ans = max(ans, j - i + 1);
+        // }
         return ans;
     }
 };
