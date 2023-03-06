e7ae0872efd984bc6f5a6b436fc95a70ebb52044
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-06 16:55:08 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.17%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -1,52 +1,67 @@
 class Solution {
 public:
-    int nextGreaterElement(int n) {
-        vector<int> v;
-        int m = n;
-        while(n != 0)
-        {
-            v.push_back(n % 10);
-            n /= 10;
+    
+        int nextGreaterElement(int n) {
+            auto s = to_string(n);
+            next_permutation(begin(s), end(s));
+            auto ans = stoll(s);
+            return (ans > INT_MAX || ans <= n) ? -1 : ans;
         }
-        n = v.size();
-        reverse(v.begin(), v.end());
-        
-        for(int i = n-2; i >= 0; i--)
-        {
-            for(int j = n-1; j > i; j--)
-            {
-                if(v[i] < v[j])
-                {
-                    vector<int> v1 = v;
-                    swap(v1[i], v1[j]);
-                    sort(v1.begin()+i+1, v1.end());
-                    // break;
-                    long long ans = 0;
-                    for(int i = 0; i < n; i++)
-                    {
-                        ans = ans * 10 + v1[i];
-                    }
-                    if(ans <= INT_MAX && ans > m)
-                        return ans;
-                }
-            }
-            // if(v[i] < v[i+1])
-            // {
-            //     swap(v[i], v[i+1]);
-            //     sort(v.begin()+i+1, v.end());
-            //     break;
-            // }
-        }
-        return -1;
-        // long long ans = 0;
-        // for(int i = 0; i < n; i++)
-        // {
-        //     ans = ans * 10 + v[i];
-        // }
-        // if(ans > INT_MAX || ans == m)
-        //     return -1;
-        // return ans;
-    }
 };
+    
+    
+    
+//     Runtime: 3 ms, faster than 42.60% of C++ online submissions for Next Greater Element III.
+// Memory Usage: 6.1 MB, less than 8.43% of C++ online submissions for Next Greater Element III.
+    
+    
+//     int nextGreaterElement(int n) {
+//         vector<int> v;
+//         int m = n;
+//         while(n != 0)
+//         {
+//             v.push_back(n % 10);
+//             n /= 10;
+//         }
+//         n = v.size();
+//         reverse(v.begin(), v.end());
+        
+//         for(int i = n-2; i >= 0; i--)
+//         {
+//             for(int j = n-1; j > i; j--)
+//             {
+//                 if(v[i] < v[j])
+//                 {
+//                     vector<int> v1 = v;
+//                     swap(v1[i], v1[j]);
+//                     sort(v1.begin()+i+1, v1.end());
+//                     // break;
+//                     long long ans = 0;
+//                     for(int i = 0; i < n; i++)
+//                     {
+//                         ans = ans * 10 + v1[i];
+//                     }
+//                     if(ans <= INT_MAX && ans > m)
+//                         return ans;
+//                 }
+//             }
+//             // if(v[i] < v[i+1])
+//             // {
+//             //     swap(v[i], v[i+1]);
+//             //     sort(v.begin()+i+1, v.end());
+//             //     break;
+//             // }
+//         }
+//         return -1;
+//         // long long ans = 0;
+//         // for(int i = 0; i < n; i++)
+//         // {
+//         //     ans = ans * 10 + v[i];
+//         // }
+//         // if(ans > INT_MAX || ans == m)
+//         //     return -1;
+//         // return ans;
+//     }
+// };
 
 // 123
