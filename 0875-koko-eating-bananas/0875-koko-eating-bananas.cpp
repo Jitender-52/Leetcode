7627fb1407a5d939f3c44b1af71a162895198234
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-08 06:59:57 +0530

    Time: 56 ms (49.09%), Space: 19 MB (45.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     
+    
     bool isFeasible(vector<int> &piles, int mid, int h)
     {
         int n = piles.size();
         long long count = 0;
-        // cout << mid << endl;
         for(int i = 0; i < n; i++)
         {
-            if(mid > 0)
                 count += ceil(double(piles[i]) / mid);
         }
         return (count <= h);
@@ -26,7 +25,7 @@ public:
             int mid = (mini + maxi) / 2;
             if(isFeasible(piles, mid, h))
             {
-                ans = min(ans, mid);
+                ans = mid;
                 maxi = mid - 1;
             }
             else
@@ -37,3 +36,49 @@ public:
         return ans;
     }
 };
+    
+    
+    
+//     Runtime: 39 ms, faster than 93.73% of C++ online submissions for Koko Eating Bananas.
+// Memory Usage: 19.1 MB, less than 11.65% of C++ online submissions for Koko Eating Bananas.
+
+    
+//     Main condition to learn about is taking mini and maxi value and to maintain the condition when mid will be zero.
+    
+    
+//     bool isFeasible(vector<int> &piles, int mid, int h)
+//     {
+//         int n = piles.size();
+//         long long count = 0;
+//         // cout << mid << endl;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(mid > 0)
+//                 count += ceil(double(piles[i]) / mid);
+//         }
+//         return (count <= h);
+//     }
+    
+//     int minEatingSpeed(vector<int>& piles, int h) {
+//         int n = piles.size();
+//         // int mini = n; 
+//         int mini = 1;
+//         int maxi = *max_element(piles.begin(), piles.end());
+        
+//         int ans = maxi;
+//         while(mini <= maxi)
+//         {
+//             int mid = (mini + maxi) / 2;
+//             if(isFeasible(piles, mid, h))
+//             {
+//                 ans = min(ans, mid);
+//                 maxi = mid - 1;
+//             }
+//             else
+//             {
+//                 mini = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
