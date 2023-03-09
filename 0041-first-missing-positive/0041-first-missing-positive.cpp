aa4efcc5b9a2d530db1db6adf08a402d5e6a55ec
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-09 12:05:48 +0530

    Time: 126 ms (17.18%), Space: 47.6 MB (11.73%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
+    
+        int firstMissingPositive(vector<int>& nums) {
        int n = nums.size();
         map<int,int> mp;
         for(int i = 0; i < n; i++)
@@ -8,11 +9,33 @@ public:
             if(nums[i] >= 1 && nums[i] <= 1e5)
                 mp[nums[i]] = 1;
         }
-        for(int i = 1; i <= 1e5+1; i++)
+        int x = 1;
+        for(auto i : mp)
         {
-            if(mp.find(i) == mp.end())
-                return i;
+            if(i.first != x)
+                return x;
+            x++;
         }
-        return -1;
+        return x;
     }
 };
+    
+//     Runtime: 164 ms, faster than 8.08% of C++ online submissions for First Missing Positive.
+// Memory Usage: 47.7 MB, less than 8.81% of C++ online submissions for First Missing Positive.
+    
+//     int firstMissingPositive(vector<int>& nums) {
+//        int n = nums.size();
+//         map<int,int> mp;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(nums[i] >= 1 && nums[i] <= 1e5)
+//                 mp[nums[i]] = 1;
+//         }
+//         for(int i = 1; i <= 1e5+1; i++)
+//         {
+//             if(mp.find(i) == mp.end())
+//                 return i;
+//         }
+//         return -1;
+//     }
+// };
