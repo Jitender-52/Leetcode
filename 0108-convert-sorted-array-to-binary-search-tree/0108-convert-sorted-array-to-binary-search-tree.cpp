ee5b69ba2bf05a06c0070fd8e0a48e4a983e5092
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-11 08:30:18 +0530

    Time: 4 ms (98.90%), Space: 21.4 MB (82.93%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -25,9 +25,31 @@ public:
     
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         int n = nums.size();
-        TreeNode *root = new TreeNode(nums[n/2]);
-        root->left = bst(nums, 0, n/2 - 1);
-        root->right = bst(nums, n/2+1, n-1);
-        return root;
+        return bst(nums, 0, n-1);
     }
 };
+    
+    
+    
+//     Runtime: 17 ms, faster than 46.58% of C++ online submissions for Convert Sorted Array to Binary Search Tree.
+// Memory Usage: 21.5 MB, less than 49.46% of C++ online submissions for Convert Sorted Array to Binary Search Tree.
+    
+//     TreeNode *bst(vector<int> &nums, int left, int right)
+//     {
+//         if(left > right)
+//             return NULL;
+//         int mid = (left + right) / 2;
+//         TreeNode *root = new TreeNode(nums[mid]);
+//         root->left = bst(nums, left, mid-1);
+//         root->right = bst(nums, mid+1, right);
+//         return root;
+//     }
+    
+//     TreeNode* sortedArrayToBST(vector<int>& nums) {
+//         int n = nums.size();
+//         TreeNode *root = new TreeNode(nums[n/2]);
+//         root->left = bst(nums, 0, n/2 - 1);
+//         root->right = bst(nums, n/2+1, n-1);
+//         return root;
+//     }
+// };
