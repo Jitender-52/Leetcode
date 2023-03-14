544dec13cf5f159f9f888fe1b5289388442a65e4
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-14 18:02:26 +0530

    Time: 11 ms (92.43%), Space: 13.3 MB (77.98%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     
+    
         void setZeroes(vector<vector<int>>& matrix) {
             int n = matrix.size();
             int m = matrix[0].size();
             
             bool check = false;
+            bool flag = false;
+            for(int j = 0; j < m; j++)
+            {
+                if(matrix[0][j] == 0)
+                    flag = true;
+            }
             for(int i = 0; i < n; i++)
             {
                 if(matrix[i][0] == 0)
@@ -14,7 +21,7 @@ public:
             
             for(int i = 0; i < n; i++)
             {
-                for(int j = 1; j < m; j++)
+                for(int j = 0; j < m; j++)
                 {
                     if(matrix[i][j] == 0)
                     {
@@ -33,7 +40,7 @@ public:
                 }
             }
             
-            if(matrix[0][0] == 0)
+            if(flag)
             {
                 for(int j = 0; j < m; j++)
                 {
@@ -50,6 +57,61 @@ public:
             }   
         }
 };
+    
+    
+    
+//     Runtime: 18 ms, faster than 43.34% of C++ online submissions for Set Matrix Zeroes.
+// Memory Usage: 13.3 MB, less than 77.98% of C++ online submissions for Set Matrix Zeroes.
+    
+//         void setZeroes(vector<vector<int>>& matrix) {
+//             int n = matrix.size();
+//             int m = matrix[0].size();
+            
+//             bool check = false;
+//             for(int i = 0; i < n; i++)
+//             {
+//                 if(matrix[i][0] == 0)
+//                     check = true;
+//             }
+            
+//             for(int i = 0; i < n; i++)
+//             {
+//                 for(int j = 1; j < m; j++)
+//                 {
+//                     if(matrix[i][j] == 0)
+//                     {
+//                         matrix[i][0] = 0;
+//                         matrix[0][j] = 0;
+//                     }
+//                 }
+//             }
+            
+//             for(int i = 1; i < n; i++)
+//             {
+//                 for(int j = 1; j < m; j++)
+//                 {
+//                     if(matrix[i][0] == 0 || matrix[0][j] == 0)
+//                         matrix[i][j] = 0;
+//                 }
+//             }
+            
+//             if(matrix[0][0] == 0)
+//             {
+//                 for(int j = 0; j < m; j++)
+//                 {
+//                     matrix[0][j] = 0;
+//                 }
+//             }
+            
+//             if(check)
+//             {
+//                 for(int i = 0; i < n; i++)
+//                 {
+//                     matrix[i][0] = 0;
+//                 }
+//             }   
+//         }
+// };
     
     
     
