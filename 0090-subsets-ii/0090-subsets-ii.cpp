a115b11e6081937633a08d65a9f085e504830bf4
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-16 07:11:40 +0530

    Time: 11 ms (18.91%), Space: 17.5 MB (9.46%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     
-        void subsets(int i, vector<int> &nums, vector<int> v, set<vector<int>> &ans)
+    
+    void subsets(int i, vector<int> &nums, vector<int> v, vector<vector<int>> &ans)
         {
             int n = nums.size();
-            ans.insert(v);
+            ans.push_back(v);
             if(i >= n)
                 return;
             subsets(i+1, nums, v, ans);
@@ -13,20 +14,47 @@ public:
         }
     
         vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-            int n = nums.size();
             sort(nums.begin(), nums.end());
-            set<vector<int>> ans;
+            vector<vector<int>> ans;
             vector<int> v;
             subsets(0, nums, v, ans);
             
-            vector<vector<int>> v1;
-            for(auto i : ans)
-            {
-                v1.push_back(i);
-            }
-            return v1;
+            sort(ans.begin(), ans.end());
+            ans.erase(unique(ans.begin(), ans.end()), ans.end());
+            return ans;
         }
 };
+    
+    
+//     Runtime: 14 ms, faster than 10.96% of C++ online submissions for Subsets II.
+// Memory Usage: 17.3 MB, less than 11.66% of C++ online submissions for Subsets II.
+     
+//         void subsets(int i, vector<int> &nums, vector<int> v, set<vector<int>> &ans)
+//         {
+//             int n = nums.size();
+//             ans.insert(v);
+//             if(i >= n)
+//                 return;
+//             subsets(i+1, nums, v, ans);
+//             v.push_back(nums[i]);
+//             subsets(i+1, nums, v, ans);
+//         }
+    
+//         vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//             int n = nums.size();
+//             sort(nums.begin(), nums.end());
+//             set<vector<int>> ans;
+//             vector<int> v;
+//             subsets(0, nums, v, ans);
+            
+//             vector<vector<int>> v1;
+//             for(auto i : ans)
+//             {
+//                 v1.push_back(i);
+//             }
+//             return v1;
+//         }
+// };
     
 //     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 //         int n = nums.size();
