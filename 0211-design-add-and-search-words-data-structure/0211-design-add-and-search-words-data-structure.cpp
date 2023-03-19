bc365b7c693146db3f2a1f02dbdb0121a0b88343
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-19 16:40:13 +0530

    Time: 2923 ms (5.01%), Space: 136 MB (99.65%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,6 +1,7 @@
 class WordDictionary {
 public:
     
+      
       vector<string> v;
     WordDictionary() {
         
@@ -11,27 +12,71 @@ public:
     }
     
     bool search(string word) {
-        int count=0;
-        int n=word.size();
+        int n = word.length();
         for(int i=0; i<v.size(); i++){
-            if(n==v[i].size()){
-                for(int j=0; j<n; j++){
-                    if(v[i][j]==word[j]||word[j]=='.'){
-                        count++;
-                    }
-                    else{
-                        count=0;
-                        break;
-                    }
+            // if(v[i].length() != word.length())
+            if(v[i].length() != n)
+                continue;
+            
+            bool check = true;
+            for(int j = 0; j < n; j++)
+            {
+                if(word[j] == '.')
+                {
+                    continue;
                 }
-                if(count==n) return true;
+                if(v[i][j] != word[j])
+                {
+                    check = false;
+                    break;
+                }
             }
+            if(check)
+                return true;
         }
         return false;
     }
 };
     
-//     set<string> st;
+    
+    
+    
+//     Runtime: 2777 ms, faster than 5.01% of C++ online submissions for Design Add and Search Words Data Structure.
+// Memory Usage: 136.1 MB, less than 99.65% of C++ online submissions for Design Add and Search Words Data Structure.
+    
+//       vector<string> v;
+//     WordDictionary() {
+        
+//     }
+    
+//     void addWord(string word) {
+//         v.push_back(word);
+//     }
+    
+//     bool search(string word) {
+//         int count=0;
+//         int n=word.size();
+//         for(int i=0; i<v.size(); i++){
+//             if(n==v[i].size()){
+//                 for(int j=0; j<n; j++){
+//                     if(v[i][j]==word[j]||word[j]=='.'){
+//                         count++;
+//                     }
+//                     else{
+//                         count=0;
+//                         break;
+//                     }
+//                 }
+//                 if(count==n) return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+    
+    
+    
+//     unordered_set<string> st;
     
 //     WordDictionary() {
         
