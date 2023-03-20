57cc4cb955d6c02337df1dff55f795bd9ac274ec
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-20 15:06:45 +0530

    Time: 20 ms (53.38%), Space: 20.2 MB (92.39%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
+    
+//     Runtime: 11 ms, faster than 97.02% of C++ online submissions for Can Place Flowers.
+// Memory Usage: 20.3 MB, less than 34.30% of C++ online submissions for Can Place Flowers.
+    
+    
     bool canPlaceFlowers(vector<int>& flowerbed, int x) {
         int n = flowerbed.size();
+        
+        if(x == 0)
+            return true;
+        
         for(int i = 0; i < n; i++)
         {
             if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == n-1 || flowerbed[i+1] == 0))
