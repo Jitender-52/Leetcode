c8058578fa2720e0705f6d6a9cbfad7dc24fefec
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-22 10:28:51 +0530

    Time: 963 ms (6.01%), Space: 163.3 MB (12.16%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     
-    int dfs(int src, vector<int> &vis, vector<vector<int>> &adj, map<pair<int,int>,int> &mp)
-    {
-        vis[src] = 1;
-        int ans = INT_MAX;
-        for(auto i : adj[src])
-        {
-            ans = min(ans, mp[{src, i}]);
-            ans = min(ans, mp[{i, src}]);
-            if(!vis[i])
-            {
-                ans = min(ans, dfs(i, vis, adj, mp));
-            }
-        }
-        return ans;
-    }
+    
     
     int minScore(int n, vector<vector<int>>& roads) {
         int m = roads.size();
@@ -34,7 +20,72 @@ public:
             adj[roads[i][1]].push_back(roads[i][0]);
         }
         
+        // vector<int> vis(n+1, 0);
+        // return dfs(1, vis, adj, mp);
+        
+        int ans = INT_MAX;
+        queue<int> q;
         vector<int> vis(n+1, 0);
-        return dfs(1, vis, adj, mp);
+        q.push(1);
+        vis[1] = 1;
+        
+        while(!q.empty())
+        {
+            int src = q.front();
+            q.pop();
+            
+            for(auto i : adj[src])
+            {
+                ans = min({ans, mp[{src, i}], mp[{i, src}]});
+                if(!vis[i])
+                {
+                    q.push(i);
+                    vis[i] = 1;
+                }
+            }
+        }
+        return ans;
     }
 };
+    
+    
+//     Runtime: 972 ms, faster than 5.70% of C++ online submissions for Minimum Score of a Path Between Two Cities.
+// Memory Usage: 167.1 MB, less than 10.62% of C++ online submissions for Minimum Score of a Path Between Two Cities.
+    
+//     int dfs(int src, vector<int> &vis, vector<vector<int>> &adj, map<pair<int,int>,int> &mp)
+//     {
+//         vis[src] = 1;
+//         int ans = INT_MAX;
+//         for(auto i : adj[src])
+//         {
+//             ans = min(ans, mp[{src, i}]);
+//             ans = min(ans, mp[{i, src}]);
+//             if(!vis[i])
+//             {
+//                 ans = min(ans, dfs(i, vis, adj, mp));
+//             }
+//         }
+//         return ans;
+//     }
+    
+//     int minScore(int n, vector<vector<int>>& roads) {
+//         int m = roads.size();
+        
+//         map<pair<int,int>,int> mp;
+//         for(int i = 0; i < m; i++)
+//         {
+//             mp[{roads[i][0], roads[i][1]}] = roads[i][2];
+//             mp[{roads[i][1], roads[i][0]}] = roads[i][2];
+//         }
+
+//         vector<vector<int>> adj(n+1);
+//         for(int i = 0; i < m; i++)
+//         {
+//             adj[roads[i][0]].push_back(roads[i][1]);
+//             adj[roads[i][1]].push_back(roads[i][0]);
+//         }
+        
+//         vector<int> vis(n+1, 0);
+//         return dfs(1, vis, adj, mp);
+//     }
+// };
