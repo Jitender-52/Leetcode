20d98d25d33e6d0687f1170eb2ebdfc0d7a0ef70
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-23 15:54:53 +0530

    Time: 133 ms (70.50%), Space: 52.1 MB (33.75%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     
-    void dfs(int src, vector<int> &vis, int &count, vector<vector<int>> &adj)
+    void dfs(int src, vector<int> &vis, vector<vector<int>> &adj)
     {
         vis[src] = 1;
         for(auto i : adj[src])
         {
             if(!vis[i])
             {
-                dfs(i, vis, count, adj);
+                dfs(i, vis, adj);
             }
-            // else if(i != src)
-            // {
-            //     cout << src << " " << i << endl;
-            //     count++;
-            // }
         }
     }
     
@@ -31,7 +26,6 @@ public:
             adj[connections[i][1]].push_back(connections[i][0]);
         }
         
-        int count = 0;
         int ans = 0;
         vector<int> vis(n, 0);
         for(int i = 0; i < n; i++)
@@ -39,17 +33,65 @@ public:
             if(!vis[i])
             {
                 ans++;
-                dfs(i, vis, count, adj);
+                dfs(i, vis, adj);
             }
         }
-        ans--;
-        return ans;
-        
-        // cout << count << " " << ans << endl;
-        // if(ans > count)
-        // {
-        //     return -1;
-        // }
-        // return count - ans;
+        return ans-1;
     }
 };
+    
+//     Runtime: 136 ms, faster than 65.83% of C++ online submissions for Number of Operations to Make Network Connected.
+// Memory Usage: 52 MB, less than 34.51% of C++ online submissions for Number of Operations to Make Network Connected.
+    
+//     void dfs(int src, vector<int> &vis, int &count, vector<vector<int>> &adj)
+//     {
+//         vis[src] = 1;
+//         for(auto i : adj[src])
+//         {
+//             if(!vis[i])
+//             {
+//                 dfs(i, vis, count, adj);
+//             }
+//             // else if(i != src)
+//             // {
+//             //     cout << src << " " << i << endl;
+//             //     count++;
+//             // }
+//         }
+//     }
+    
+//     int makeConnected(int n, vector<vector<int>>& connections) {
+//         int m = connections.size();
+        
+//         if(m < n-1)
+//             return -1;
+        
+//         vector<vector<int>> adj(n+1);
+//         for(int i = 0; i < m; i++)
+//         {
+//             adj[connections[i][0]].push_back(connections[i][1]);
+//             adj[connections[i][1]].push_back(connections[i][0]);
+//         }
+        
+//         int count = 0;
+//         int ans = 0;
+//         vector<int> vis(n, 0);
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(!vis[i])
+//             {
+//                 ans++;
+//                 dfs(i, vis, count, adj);
+//             }
+//         }
+//         ans--;
+//         return ans;
+        
+//         // cout << count << " " << ans << endl;
+//         // if(ans > count)
+//         // {
+//         //     return -1;
+//         // }
+//         // return count - ans;
+//     }
+// };
