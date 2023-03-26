5ec9d96c95bb658c9963b85a1291a7b72d2ecc44
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-03-26 12:27:13 +0530

    Time: 418 ms (48.12%), Space: 184.3 MB (28.85%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     
-    void cycle(int src, vector<int> &vis, vector<int> &dfs, vector<vector<int>> &adj, int count, int &ans)
+    
+    
+    // without passing & before dfs vector it will give TLE
+    
+        void cycle(int src, vector<int> &vis, vector<int> &dfs, vector<vector<int>> &adj, int count, int &ans)
     {
         vis[src] = 1;
         dfs[src] = count;
@@ -18,7 +22,7 @@ public:
                 cycle(i, vis, dfs, adj, count+1, ans);
             }
         }
-        dfs[src] = -1;
+        dfs[src] = -1; // Without passing &(address) it will give TLE
     }
     
     int longestCycle(vector<int>& edges) {
@@ -44,3 +48,53 @@ public:
         return ans;
     }
 };
+    
+    
+    
+//     Runtime: 390 ms, faster than 52.39% of C++ online submissions for Longest Cycle in a Graph.
+// Memory Usage: 184.2 MB, less than 28.97% of C++ online submissions for Longest Cycle in a Graph.
+
+    
+//     void cycle(int src, vector<int> &vis, vector<int> &dfs, vector<vector<int>> &adj, int count, int &ans)
+//     {
+//         vis[src] = 1;
+//         dfs[src] = count;
+        
+//         for(auto i : adj[src])
+//         {
+//             if(vis[i] && dfs[i] != -1)
+//             {
+//                 ans = max(ans, dfs[src] - dfs[i] + 1);
+//                 return;
+//             }
+//             else if(!vis[i])
+//             {
+//                 cycle(i, vis, dfs, adj, count+1, ans);
+//             }
+//         }
+//         dfs[src] = -1;
+//     }
+    
+//     int longestCycle(vector<int>& edges) {
+//         int n = edges.size();
+//         vector<vector<int>> adj(n+1);
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(edges[i] != -1)
+//                 adj[i].push_back(edges[i]);
+//         }
+        
+//         vector<int> dfs(n+1, -1);
+//         vector<int> vis(n+1, 0);
+        
+//         int ans = -1;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(!vis[i])
+//             {
+//                 cycle(i, vis, dfs, adj, 0, ans);
+//             }
+//         }
+//         return ans;
+//     }
+// };
