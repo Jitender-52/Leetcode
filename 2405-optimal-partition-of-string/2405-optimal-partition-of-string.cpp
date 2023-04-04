2962503f53753e523e1a98b0e8a338bc3e432e39
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-04-04 22:08:45 +0530

    Time: 266 ms (19.32%), Space: 72.1 MB (8.67%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 253 ms, faster than 28.98% of C++ online submissions for Optimal Partition of String.
+// Memory Usage: 72.1 MB, less than 8.67% of C++ online submissions for Optimal Partition of String.
+    
     int partitionString(string s) {
         map<char,int> mp;
         int ans = 1;
