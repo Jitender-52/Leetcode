02dcc7c9f542b55dd597ebce105d20f8a53ab18e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-04-13 22:06:55 +0530

    Time: 7 ms (85.57%), Space: 15.4 MB (24.28%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 15 ms, faster than 18.21% of C++ online submissions for Validate Stack Sequences.
+// Memory Usage: 15.5 MB, less than 24.28% of C++ online submissions for Validate Stack Sequences.
+    
+    
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         int n = pushed.size();
         int i = 0;
