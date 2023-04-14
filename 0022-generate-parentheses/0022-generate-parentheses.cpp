40edfd5c6fb624804dcb9d5745b5d586774b3d47
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-04-14 12:38:17 +0530

    Time: 6 ms (42.25%), Space: 11.4 MB (85.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,44 +1,83 @@
 class Solution {
 public:
     
-    bool isValid(string &s)
+    void generate(int open, int close, string &s, vector<string> &ans, int max)
     {
-        int n = s.length();
-        int count = 0;
-        for(int i = 0; i < n; i++)
+        if(s.length() == 2 * max)
         {
-            if(s[i] == '(')
-                count++;
-            else
-                count--;
-            if(count < 0)
-                return 0;
-        }
-        return count == 0;
-    }
-    
-    void generate(int n, string s, vector<string> &ans)
-    {
-        // cout << s.length() << endl;
-        // cout << s << endl << endl;
-        if(s.length() == 2 * n)
-        {
-            if(isValid(s))
-                ans.push_back(s);
+            ans.push_back(s);
             return;
         }
         
-        s += '(';
-        generate(n, s, ans);
-        s.pop_back();
-        s += ')';
-        generate(n, s, ans);
+        if(close > open)
+            return;
+        
+        if(open < max)
+        {
+            s += '(';
+            generate(open+1, close, s, ans, max);
+            s.pop_back();
+        }
+        if(close < max)
+        {
+            s += ')';
+            generate(open, close+1, s, ans, max);
+            s.pop_back();
+        }
     }
     
     vector<string> generateParenthesis(int n) {
         string s = "";
         vector<string> ans;
-        generate(n, s, ans);
+        generate(0, 0, s, ans, n);
         return ans;
     }
 };
+    
+    
+    
+    
+//     Runtime: 59 ms, faster than 5.01% of C++ online submissions for Generate Parentheses.
+// Memory Usage: 51.1 MB, less than 5.03% of C++ online submissions for Generate Parentheses.
+    
+//     bool isValid(string &s)
+//     {
+//         int n = s.length();
+//         int count = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(s[i] == '(')
+//                 count++;
+//             else
+//                 count--;
+//             if(count < 0)
+//                 return 0;
+//         }
+//         return count == 0;
+//     }
+    
+//     void generate(int n, string s, vector<string> &ans)
+//     {
+//         // cout << s.length() << endl;
+//         // cout << s << endl << endl;
+//         if(s.length() == 2 * n)
+//         {
+//             if(isValid(s))
+//                 ans.push_back(s);
+//             return;
+//         }
+        
+//         s += '(';
+//         generate(n, s, ans);
+//         s.pop_back();
+//         s += ')';
+//         generate(n, s, ans);
+//     }
+    
+//     vector<string> generateParenthesis(int n) {
+//         string s = "";
+//         vector<string> ans;
+//         generate(n, s, ans);
+//         return ans;
+//     }
+// };
