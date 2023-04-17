b750d4d2e6c5452216340793ca43533f2ae7d77c
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-04-17 06:10:51 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (86.24%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
+    
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         int maxi = *max_element(candies.begin(), candies.end());
         vector<bool> ans;
         for(auto i : candies)
-            i + extraCandies >= maxi ? ans.push_back(1) : ans.push_back(0);
+            ans.push_back(i + extraCandies >= maxi ? 1 : 0);
         return ans;
     }
 };
+
+
+    
+//     Runtime: 4 ms, faster than 61.53% of C++ online submissions for Kids With the Greatest Number of Candies.
+// Memory Usage: 9 MB, less than 16.58% of C++ online submissions for Kids With the Greatest Number of Candies.
+    
+    
+    // vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    //     int maxi = *max_element(candies.begin(), candies.end());
+    //     vector<bool> ans;
+    //     for(auto i : candies)
+    //         i + extraCandies >= maxi ? ans.push_back(1) : ans.push_back(0);
+    //     return ans;
+//     }
+// };
