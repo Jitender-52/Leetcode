4e2d0b16437f4cc9ef8b2cded57276de2015cc5c
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-04-18 21:57:51 +0530

    Time: 23 ms (40.75%), Space: 11.3 MB (68.55%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,17 +1,51 @@
 class Solution {
 public:
+    
+    void dfs(int x, int n, vector<int> &ans)
+    {
+        if(x == 0)
+        {
+            for(int i = 1; i <= 9; i++)
+            {
+                dfs(i, n, ans);
+            }
+        }
+        if(x > 0 && x <= n)
+        {
+            ans.push_back(x);
+            for(int i = 0; i <= 9; i++)
+            {
+                int a = x * 10 + i;
+                dfs(a, n, ans);
+            }
+        }
+    }
+    
     vector<int> lexicalOrder(int n) {
-        vector<string> ans;
-        vector<int> v;
-        for(int i = 0; i < n; i++)
-        {
-            ans.push_back(to_string(i + 1));
-        }
-        sort(ans.begin(), ans.end());
-        for(int i = 0; i < n; i++)
-        {
-            v.push_back(stoi(ans[i]));
-        }
-        return v;
+        vector<int> ans;
+        dfs(0, n, ans);
+        return ans;
     }
 };
+    
+    
+//     Runtime: 104 ms, faster than 14.96% of C++ online submissions for Lexicographical Numbers.
+// Memory Usage: 20.3 MB, less than 32.39% of C++ online submissions for Lexicographical Numbers.
+    
+//     vector<int> lexicalOrder(int n) {
+//         vector<string> ans;
+//         vector<int> v;
+//         for(int i = 0; i < n; i++)
+//         {
+//             ans.push_back(to_string(i + 1));
+//         }
+//         sort(ans.begin(), ans.end());
+//         for(int i = 0; i < n; i++)
+//         {
+//             v.push_back(stoi(ans[i]));
+//         }
+//         return v;
+//     }
+// };
+    
+    
