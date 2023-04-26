9f8853ef5171e1760c7a9458ad4e7b4758370f38
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-04-26 05:42:30 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (7.98%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int addDigits(int num) {
-        while(num > 9)
-        {
-            int a = 0;
-            while(num > 0)
-            {
-                a += num % 10;
-                num /= 10;
-            }
-            num = a;
+    
+        int addDigits(int num) {
+            return num == 0 ? 0 : 1 + (num - 1) % 9;
         }
-        return num;
-    }
 };
+    
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Add Digits.
+// Memory Usage: 6 MB, less than 63.10% of C++ online submissions for Add Digits.
+    
+//     int addDigits(int num) {
+//         while(num > 9)
+//         {
+//             int a = 0;
+//             while(num > 0)
+//             {
+//                 a += num % 10;
+//                 num /= 10;
+//             }
+//             num = a;
+//         }
+//         return num;
+//     }
+// };
