bf08fb28e1efadf3f6ea0e1f4f47f190c7a21086
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-04-26 05:37:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.10%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num > 9)
+        {
+            int a = 0;
+            while(num > 0)
+            {
+                a += num % 10;
+                num /= 10;
+            }
+            num = a;
+        }
+        return num;
+    }
+};
