bcebbb2ea626979bc8a14c99a003cc656f5ae025
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-01 06:45:39 +0530

    Time: 173 ms (10.09%), Space: 73.3 MB (25.05%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     
-        int minNumberOperations(vector<int>& target) {
-            int n = target.size();
-            int ans = target[0];
-            for(int i = 1; i < n; i++)
+    int minNumberOperations(vector<int>& target) {
+        int n = target.size();
+        stack<int> st;
+        st.push(0);
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(target[i] >= st.top())
             {
-                ans += max(0, target[i] - target[i-1]);
+                ans += target[i] - st.top();
             }
-            return ans;
+            st.pop();
+            st.push(target[i]);
         }
+        return ans;
+    }
 };
+    
+//     Runtime: 143 ms, faster than 30.63% of C++ online submissions for Minimum Number of Increments on Subarrays to Form a Target Array.
+// Memory Usage: 73.1 MB, less than 66.49% of C++ online submissions for Minimum Number of Increments on Subarrays to Form a Target Array.
+    
+//         int minNumberOperations(vector<int>& target) {
+//             int n = target.size();
+//             int ans = target[0];
+//             for(int i = 1; i < n; i++)
+//             {
+//                 ans += max(0, target[i] - target[i-1]);
+//             }
+//             return ans;
+//         }
+// };
     
     
 //     int minNumberOperations(vector<int>& target) {
