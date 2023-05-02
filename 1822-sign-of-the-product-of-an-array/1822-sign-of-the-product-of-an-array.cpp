1dab11faaec6f91b070169c9e9ef15aedc73d8c8
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-02 17:52:26 +0530

    Time: 8 ms (27.40%), Space: 10.2 MB (45.94%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
-    int arraySign(vector<int>& nums) {
-        int count = 0;
-        for(auto i : nums)
-        {
-            if(i == 0)
-                return 0;
-            else if(i < 0)
-                count++;
+    
+        int arraySign(vector<int>& nums) {
+            int ans = 1;
+            for(auto i : nums)
+            {
+                if(i == 0)
+                    return 0;
+                if(i < 0)
+                    ans *= -1;
+            }
+            return ans;
         }
-        if(count % 2)
-            return -1;
-        return 1;
-    }
 };
+    
+    
+//     Runtime: 11 ms, faster than 16.81% of C++ online submissions for Sign of the Product of an Array.
+// Memory Usage: 10.1 MB, less than 86.28% of C++ online submissions for Sign of the Product of an Array.
+    
+//     int arraySign(vector<int>& nums) {
+//         int count = 0;
+//         for(auto i : nums)
+//         {
+//             if(i == 0)
+//                 return 0;
+//             else if(i < 0)
+//                 count++;
+//         }
+//         if(count % 2)
+//             return -1;
+//         return 1;
+//     }
+// };
