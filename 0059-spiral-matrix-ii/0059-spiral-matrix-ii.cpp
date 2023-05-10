9db4c27a6f573a0450ecbf32250135124605670a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-10 20:32:27 +0530

    Time: 5 ms (6.36%), Space: 6.7 MB (23.20%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Spiral Matrix II.
+// Memory Usage: 6.4 MB, less than 90.43% of C++ online submissions for Spiral Matrix II.
+    
     vector<vector<int>> generateMatrix(int n) {
         int i = 0;
         int j = 0;
