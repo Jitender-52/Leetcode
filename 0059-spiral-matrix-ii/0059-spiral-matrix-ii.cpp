470527f40661f427ab02346482e6bc4d55097049
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-10 20:04:31 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (90.43%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int i = 0;
+        int j = 0;
+        int is = -1;
+        int ie = n;
+        int js = -1;
+        int je = n;
+        vector<vector<int>> v(n, vector<int>(n));
+        int k = 1;
+        int check = 0;
+
+        int p = pow(n, 2);
+        // cout << p << endl;
+        // cout << p << endl;
+
+        // v[i][j] = 1;
+        // return v;
+
+        while (k <= p)
+        {
+            // cout << "HII" << endl;
+            // cout << i << " " << j << endl;
+            v[i][j] = k;
+
+            if (check == 0)
+            {
+                j++;
+            }
+            else if (check == 1)
+            {
+                i++;
+            }
+            else if (check == 2)
+            {
+                j--;
+            }
+            else if (check == 3)
+            {
+                i--;
+            }
+
+            if (j == je)
+            {
+                j--;
+                is++;
+                check = 1;
+                i++;
+            }
+            else if (i == ie)
+            {
+                je--;
+                i--;
+                check = 2;
+                j--;
+            }
+            else if (j == js)
+            {
+                j++;
+                ie--;
+                check = 3;
+                i--;
+            }
+            else if (i == is)
+            {
+                i++;
+                js++;
+                check = 0;
+                j++;
+            }
+
+            k++;
+        }
+
+            // cout << p << endl;
+        // if(n % 2 == 1)
+        // {
+        //     // v[i/2][j/2] = p;
+        // }
+
+        // cout << "HII" << endl;
+        return v;  
+    }
+};
