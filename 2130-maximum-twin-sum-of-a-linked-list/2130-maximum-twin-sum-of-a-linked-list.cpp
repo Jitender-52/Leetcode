311457c1288e45f5984842704bc9a1e84ef697d0
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-17 11:06:15 +0530

    Time: 276 ms (40.47%), Space: 116.5 MB (99.29%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,17 +11,50 @@
 class Solution {
 public:
     
-    int twin(ListNode *head1, ListNode *&head2)
-    {
-        if(!head1)
-            return 0;
-        int ans = twin(head1->next, head2);
-        ans = max(ans, head1->val + head2->val);
-        head2 = head2->next;
+    int pairSum(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode *nextNode, *prev = NULL;
+        while(slow)
+        {
+            nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+        
+        int ans = 0;
+        ListNode *start = head;
+        while(prev)
+        {
+            ans = max(ans, start->val + prev->val);
+            start = start->next;
+            prev = prev->next;
+        }
         return ans;
     }
-    
-    int pairSum(ListNode* head) {
-        return twin(head, head);
-    }
 };
+    
+//     Runtime: 279 ms, faster than 34.99% of C++ online submissions for Maximum Twin Sum of a Linked List.
+// Memory Usage: 121.3 MB, less than 52.01% of C++ online submissions for Maximum Twin Sum of a Linked List.
+    
+//     int twin(ListNode *head1, ListNode *&head2)
+//     {
+//         if(!head1)
+//             return 0;
+//         int ans = twin(head1->next, head2);
+//         ans = max(ans, head1->val + head2->val);
+//         head2 = head2->next;
+//         return ans;
+//     }
+    
+//     int pairSum(ListNode* head) {
+//         return twin(head, head);
+//     }
+// };
