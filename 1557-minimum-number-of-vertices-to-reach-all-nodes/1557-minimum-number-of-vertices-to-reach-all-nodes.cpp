f6a8bcd330988993978018969b39dd4373350480
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-18 23:51:43 +0530

    Time: 315 ms (63.31%), Space: 94.2 MB (84.26%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 326 ms, faster than 58.94% of C++ online submissions for Minimum Number of Vertices to Reach All Nodes.
+// Memory Usage: 94.3 MB, less than 84.26% of C++ online submissions for Minimum Number of Vertices to Reach All Nodes.
+
+    
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         vector<int> incoming(n, 0);
         for(auto &i : edges)
