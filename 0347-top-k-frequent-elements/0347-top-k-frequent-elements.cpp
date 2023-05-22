34f52628a93871c081d49685a8bc5fe5bcfbc591
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-22 08:43:05 +0530

    Time: 20 ms (43.48%), Space: 13.8 MB (42.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int> mp;
-        for(auto &i : nums)
-            mp[i]++;
-        vector<pair<int,int>> v;
-        for(auto &i : mp)
-            v.push_back({i.second, i.first});
-        sort(v.rbegin(), v.rend());
-        vector<int> ans;
-        for(int i = 0; i < k; i++)
-            ans.push_back(v[i].second);
-        return ans;
-    }
+    
+        vector<int> topKFrequent(vector<int>& nums, int k) {
+            map<int,int> mp;
+            for(auto &i : nums)
+                mp[i]++;
+            priority_queue<pair<int,int>, vector<pair<int,int>>> pq;
+            for(auto &i : mp)
+                pq.push({i.second, i.first});
+            vector<int> ans;
+            for(int i = 0; i < k; i++)
+            {
+                ans.push_back(pq.top().second);
+                pq.pop();
+            }
+            return ans;
+        }
 };
+    
+//     Runtime: 23 ms, faster than 25.25% of C++ online submissions for Top K Frequent Elements.
+// Memory Usage: 13.8 MB, less than 42.78% of C++ online submissions for Top K Frequent Elements.
+
+    
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         map<int,int> mp;
+//         for(auto &i : nums)
+//             mp[i]++;
+//         vector<pair<int,int>> v;
+//         for(auto &i : mp)
+//             v.push_back({i.second, i.first});
+//         sort(v.rbegin(), v.rend());
+//         vector<int> ans;
+//         for(int i = 0; i < k; i++)
+//             ans.push_back(v[i].second);
+//         return ans;
+//     }
+// };
