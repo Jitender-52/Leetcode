198b8c94ed849223313b7ebf86593745be69158a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-29 14:19:16 +0530

    Time: 12 ms (38.85%), Space: 14.3 MB (16.72%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        int c1 = image[sr][sc];
+        queue<pair<int,int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;
+        
+        auto isValid = [&](int r, int c)
+        {
+            return (r >= 0 && r < n && c >= 0 && c < m);
+        };
+        
+        vector<int> dx = {1, -1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
+    
+        vector<vector<int>> vis(n, vector<int> (m , 0));
+        vis[sr][sc] = 1;
+        
+        while(!q.empty())
+        {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i = 0; i < 4; i++)
+            {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                // cout << nr << " " << nc << endl;
+                if(isValid(nr, nc) && image[nr][nc] == c1 && !vis[nr][nc])
+                {
+                    image[nr][nc] = color;
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+        return image;
+    }
+};
