e3e3129fa934f60b62a7b2bd2cd61f1b82240c92
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-05-30 22:45:22 +0530

    Time: 139 ms (36.58%), Space: 184.2 MB (20.50%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,6 +1,9 @@
 class MyHashSet {
 public:
     
+//     Runtime: 120 ms, faster than 42.72% of C++ online submissions for Design HashSet.
+// Memory Usage: 184.2 MB, less than 18.98% of C++ online submissions for Design HashSet.
+    
     vector<int> v;
     
     MyHashSet() {
