82fd5209de3439cadc5fd765dbe24ec9b2dcd29a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-06-08 20:14:39 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.40%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,31 +1,63 @@
 class Solution {
 public:
     
-        string getPermutation(int n, int k) {
-            vector<int> v;
-            vector<int> fac(n+1, 1);
-            int a = 1;
-            for(int i = 1; i <= n; i++)
-            {
-                v.push_back(i);
-                fac[i] = fac[i-1] * i; 
-            }
-            fac[0] = 1;
-            
-            string ans = "";
-            k--; // because of 0  indexing
-            for(int i = n; i > 0; i--)
-            {
-                a = k / fac[i-1];
-                k %= fac[i-1];
-                ans += (v[a] + '0');
-                v.erase(v.begin() + a);
-                // remove(v.begin(), v.end(), v.begin() + a);
-                // v.erase(v.begin(), v.end(), v.begin() + a);
-            }
-            return ans;
+    string getPermutation(int n, int k) {
+        vector<int> v;
+        int f = 1;
+        for(int i = 1; i < n; i++)
+        {
+            f *= i;
+            v.push_back(i);
         }
+        v.push_back(n);
+        
+        k--;
+        string ans = "";
+        for(int i = n-1; i >= 0; i--)
+        {
+            int a = k / f;
+            k %= f;
+            ans += (v[a] + '0');
+        
+            v.erase(v.begin() + a);
+            
+            if(i == 0)
+                break;
+            f /= i;
+        }
+        return ans;
+    }
 };
+    
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Permutation Sequence.
+// Memory Usage: 6.2 MB, less than 7.24% of C++ online submissions for Permutation Sequence.
+    
+//         string getPermutation(int n, int k) {
+//             vector<int> v;
+//             vector<int> fac(n+1, 1);
+//             int a = 1;
+//             for(int i = 1; i <= n; i++)
+//             {
+//                 v.push_back(i);
+//                 fac[i] = fac[i-1] * i; 
+//             }
+//             fac[0] = 1;
+            
+//             string ans = "";
+//             k--; // because of 0  indexing
+//             for(int i = n; i > 0; i--)
+//             {
+//                 a = k / fac[i-1];
+//                 k %= fac[i-1];
+//                 ans += (v[a] + '0');
+//                 v.erase(v.begin() + a);
+//                 // remove(v.begin(), v.end(), v.begin() + a);
+//                 // v.erase(v.begin(), v.end(), v.begin() + a);
+//             }
+//             return ans;
+//         }
+// };
     
     
 //         void permutation(int j, int num, int n, vector<int> &vis, vector<int> &ans)
