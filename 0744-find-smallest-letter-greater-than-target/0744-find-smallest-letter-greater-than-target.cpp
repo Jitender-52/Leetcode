9d17b1ce44f6079cace2e286ac812523f5cb0f84
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-06-09 16:40:20 +0530

    Time: 15 ms (75.46%), Space: 15.9 MB (72.43%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 11 ms, faster than 94.29% of C++ online submissions for Find Smallest Letter Greater Than Target.
+// Memory Usage: 15.9 MB, less than 32.10% of C++ online submissions for Find Smallest Letter Greater Than Target.
+    
     char nextGreatestLetter(vector<char>& letters, char target) {
         int n = letters.size(), l = 0, r = n-1;
         while(l <= r)
