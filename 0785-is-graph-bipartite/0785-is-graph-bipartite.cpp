0e6687080886b86ed2e8e0aceb9938b11f8ede4a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-06-14 10:28:06 +0530

    Time: 23 ms (88.95%), Space: 13.7 MB (26.54%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,46 +1,93 @@
 class Solution {
 public:
-     bool bipartiteDfs(int node, vector<vector<int>> adj, int color[])
-{
-    if (color[node] == -1)
+    
+    bool isBipartiteBFS(int src, vector<vector<int>> &adj, vector<int> &color)
     {
-        color[node] = 1;
-    }
-
-    for (auto i : adj[node])
-    {
-        if (color[i] == -1)
+        queue<int> q;
+        q.push(src);
+        color[src] = 1;
+        while (!q.empty())
         {
-            color[i] = 1 - color[node];
-            if (!bipartiteDfs(i, adj, color))
+            int parent = q.front();
+            q.pop();
+            for (auto child : adj[parent])
             {
-                return false;
+                if (color[child] == color[parent])
+                    return false;
+                if (color[child] == -1)
+                {
+                    color[child] = color[parent] ^ 1;
+                    q.push(child);
+                }
             }
         }
-        else if (color[node] == color[i]) // If we will use this condition first then it will take less time to run due to less number of recursive calls
-        {
-            return false;
-        }
+        return true;
     }
-    return true;
-}
+    
+    bool isBipartite(vector<vector<int>> &adj){
+        int n = adj.size();
+        vector<int> color(n + 1, -1);
 
-bool isBipartite(vector<vector<int>> &adj)
-{
-    int n = adj.size();
-    int color[n];
-    for (int i = 0; i < n; i++)
-    {
-        color[i] = -1;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        if (!bipartiteDfs(i, adj, color))
+        for (int i = 0; i < n; i++)
         {
-            return false;
+            if (color[i] == -1)
+            {
+                if (!isBipartiteBFS(i, adj, color))
+                {
+                    return 0;
+                }
+            }
         }
+        return 1;
     }
-    return true;
-}
 };
+
+    
+    
+//     Runtime: 102 ms, faster than 5.06% of C++ online submissions for Is Graph Bipartite?.
+// Memory Usage: 57.4 MB, less than 5.01% of C++ online submissions for Is Graph Bipartite?.
+    
+//     bool bipartiteDfs(int node, vector<vector<int>> adj, int color[])
+//     {
+//         if (color[node] == -1)
+//         {
+//             color[node] = 1;
+//         }
+
+//         for (auto i : adj[node])
+//         {
+//             if (color[i] == -1)
+//             {
+//                 color[i] = 1 - color[node];
+//                 if (!bipartiteDfs(i, adj, color))
+//                 {
+//                     return false;
+//                 }
+//             }
+//             else if (color[node] == color[i]) // If we will use this condition first then it will take less time to run due to less number of recursive calls
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     bool isBipartite(vector<vector<int>> &adj)
+//     {
+//         int n = adj.size();
+//         int color[n];
+//         for (int i = 0; i < n; i++)
+//         {
+//             color[i] = -1;
+//         }
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (!bipartiteDfs(i, adj, color))
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
