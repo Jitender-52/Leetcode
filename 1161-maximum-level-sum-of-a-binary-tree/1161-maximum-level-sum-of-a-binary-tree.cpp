ad1e061c578d65f8247ae23d7196494e3194f041
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-06-15 06:55:22 +0530

    Time: 183 ms (96.57%), Space: 104.7 MB (99.28%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,31 +11,65 @@
  */
 class Solution {
 public:
+    
+    void dfs(TreeNode *root, int level, vector<int> &v)
+    {
+        if(!root)
+            return;
+        if(v.size() == level)
+            v.push_back(root->val);
+        else
+            v[level] += root->val;
+        dfs(root->left, level + 1, v);
+        dfs(root->right, level + 1, v);
+    }
+        
+    
     int maxLevelSum(TreeNode* root) {
-        queue<TreeNode *> q;
-        q.push(root);
-        int ans = 0, sum = INT_MIN, level = 1;
-        while(!q.empty())
+        vector<int> v;
+        dfs(root, 0, v);
+        int sum = INT_MIN, ans = 0; // mybad used sum = 0;
+        for(int i = 0; i < v.size(); i++)
         {
-            int sz = q.size();
-            int count = 0;
-            for(int i = 0; i < sz; i++)
+            if(v[i] > sum)
             {
-                TreeNode *node = q.front();
-                q.pop();
-                count += node->val;
-                if(node->left)
-                    q.push(node->left);
-                if(node->right)
-                    q.push(node->right);
+                sum = v[i];
+                ans = i+1;
             }
-            if(count > sum)
-            {
-                sum = count;
-                ans = level;
-            }
-            level++;
         }
         return ans;
     }
 };
+    
+    
+//     Runtime: 180 ms, faster than 97.63% of C++ online submissions for Maximum Level Sum of a Binary Tree.
+// Memory Usage: 107.2 MB, less than 59.25% of C++ online submissions for Maximum Level Sum of a Binary Tree.
+    
+//     int maxLevelSum(TreeNode* root) {
+//         queue<TreeNode *> q;
+//         q.push(root);
+//         int ans = 0, sum = INT_MIN, level = 1;
+//         while(!q.empty())
+//         {
+//             int sz = q.size();
+//             int count = 0;
+//             for(int i = 0; i < sz; i++)
+//             {
+//                 TreeNode *node = q.front();
+//                 q.pop();
+//                 count += node->val;
+//                 if(node->left)
+//                     q.push(node->left);
+//                 if(node->right)
+//                     q.push(node->right);
+//             }
+//             if(count > sum)
+//             {
+//                 sum = count;
+//                 ans = level;
+//             }
+//             level++;
+//         }
+//         return ans;
+//     }
+// };
