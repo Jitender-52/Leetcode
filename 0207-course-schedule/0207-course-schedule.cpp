d0f309a5cc9825db7d247e1c1835a5cd3106436a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-06-15 17:29:10 +0530

    Time: 25 ms (60.97%), Space: 13.2 MB (84.80%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,82 @@
 class Solution {
 public:
     
-    bool isCycle(int src, vector<int> &vis, vector<int> adj[], vector<int> &dfs)
-    {
-        vis[src] = true;
-        dfs[src] = true;
-        for(auto i : adj[src])
-        {
-            if(vis[i] && dfs[i])
-                return true;
-            else if(!vis[i])
-                if(isCycle(i, vis, adj, dfs))
-                    return true;
-        }
-        dfs[src] = false;
-        return false;
-    }
-    
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(n+1);
         int m = prerequisites.size();
-        vector<int> adj[n];
+        
+        vector<vector<int>> adj(n);
         for(int i = 0; i < m; i++)
         {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj[u].push_back(v);
+            indegree[prerequisites[i][0]]++;
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
         
-        vector<int> vis(n);
-        vector<int> dfs(n);
-        for(int i = 0 ;i < n; i++)
-        { 
-            if(isCycle(i, vis, adj, dfs))
-                return false;
+        queue<int> q;
+        for(int i = 0; i < n; i++)
+        {
+            if(!indegree[i])
+                q.push(i);
         }
-        return true;
+        
+
+        
+        int count = 0;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for(auto i : adj[node])
+            {
+                indegree[i]--;
+                if(indegree[i] == 0)
+                    q.push(i);
+            }
+            count++;
+        }
+        // cout << count << endl;
+        return (count == n);
     }
 };
+    
+    
+//     Runtime: 17 ms, faster than 94.40% of C++ online submissions for Course Schedule.
+// Memory Usage: 13.6 MB, less than 71.26% of C++ online submissions for Course Schedule.
+    
+    
+//     bool isCycle(int src, vector<int> &vis, vector<int> adj[], vector<int> &dfs)
+//     {
+//         vis[src] = true;
+//         dfs[src] = true;
+//         for(auto i : adj[src])
+//         {
+//             if(vis[i] && dfs[i])
+//                 return true;
+//             else if(!vis[i])
+//                 if(isCycle(i, vis, adj, dfs))
+//                     return true;
+//         }
+//         dfs[src] = false;
+//         return false;
+//     }
+    
+//     bool canFinish(int n, vector<vector<int>>& prerequisites) {
+//         int m = prerequisites.size();
+//         vector<int> adj[n];
+//         for(int i = 0; i < m; i++)
+//         {
+//             int u = prerequisites[i][0];
+//             int v = prerequisites[i][1];
+//             adj[u].push_back(v);
+//         }
+        
+//         vector<int> vis(n);
+//         vector<int> dfs(n);
+//         for(int i = 0 ;i < n; i++)
+//         { 
+//             if(isCycle(i, vis, adj, dfs))
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
