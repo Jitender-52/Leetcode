5d0ded4c981052702b5d9de5f788bd36cb191e5e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-06-19 22:52:53 +0530

    Time: 0 ms (100.00%), Space: 8 MB (54.63%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 6 ms, faster than 28.46% of C++ online submissions for Find the Highest Altitude.
+// Memory Usage: 8 MB, less than 54.63% of C++ online submissions for Find the Highest Altitude.
+
+    
     int largestAltitude(vector<int>& gain) {
         int ans = 0, sum = 0;
         for(auto i : gain)
