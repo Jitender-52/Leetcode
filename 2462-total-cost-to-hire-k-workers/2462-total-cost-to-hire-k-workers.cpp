9be62dd67a21a78fe14837281624ed93cd3ba11e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-06-26 17:55:55 +0530

    Time: 202 ms (72.01%), Space: 72.9 MB (61.12%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 196 ms, faster than 82.48% of C++ online submissions for Total Cost to Hire K Workers.
+// Memory Usage: 72.8 MB, less than 82.21% of C++ online submissions for Total Cost to Hire K Workers.
+    
     long long totalCost(vector<int>& costs, int k, int candidates) {
         int n = costs.size();
         priority_queue<int,vector<int>, greater<int>> pq1, pq2;
@@ -7,6 +11,8 @@ public:
         for(i = 0; i < candidates; i++)
         {
             pq1.push(costs[i]);
+            
+            // Doing mistake so made three different methods 
             if(j >= candidates)
             {
                 pq2.push(costs[j]);
@@ -18,33 +24,10 @@ public:
         
         long long ans = 0;
         
-        while(k--)
-        {
-            if(pq1.top() <= pq2.top())
-            {
-                ans += pq1.top();
-                pq1.pop();
-                if(i <= j)
-                {
-                    pq1.push(costs[i]);
-                    i++;
-                }
-            }
-            else
-            {
-                ans += pq2.top();
-                pq2.pop();
-                if(i <= j)
-                {
-                    pq2.push(costs[j]);
-                    j--;
-                }
-            }
-        }
-        
+        // 1st method
         // while(k--)
         // {
-        //     if((!pq1.empty() || pq2.empty()) && (pq1.top() <= pq2.top()))
+        //     if(pq1.top() <= pq2.top())
         //     {
         //         ans += pq1.top();
         //         pq1.pop();
@@ -66,6 +49,32 @@ public:
         //     }
         // }
         
+        // 2nd method
+        while(k--)
+        {
+            if((!pq1.empty() || pq2.empty()) && (pq1.top() <= pq2.top()))
+            {
+                ans += pq1.top();
+                pq1.pop();
+                if(i <= j)
+                {
+                    pq1.push(costs[i]);
+                    i++;
+                }
+            }
+            else
+            {
+                ans += pq2.top();
+                pq2.pop();
+                if(i <= j)
+                {
+                    pq2.push(costs[j]);
+                    j--;
+                }
+            }
+        }
+        
+        // 3rd method
         // while(k--)
         // {
         //     if(!pq1.empty() && !pq2.empty())
