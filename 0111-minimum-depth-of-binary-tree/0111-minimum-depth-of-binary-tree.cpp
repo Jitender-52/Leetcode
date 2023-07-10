7029ee18902d4002b418ed4584b205ccd1973c9f
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-10 15:46:00 +0530

    Time: 265 ms (95.48%), Space: 144.7 MB (57.99%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,13 +12,42 @@
 class Solution {
 public:
     
-    int minDepth(TreeNode* root) {
-        if(!root)
-            return 0;
-        if(!root->left)
-            return 1 + minDepth(root->right);
-        if(!root->right)
-            return 1 + minDepth(root->left);
-        return 1 + min(minDepth(root->left), minDepth(root->right));   
-    }
+        int minDepth(TreeNode* root) {
+            queue<TreeNode *> q;
+            if(root)
+                q.push(root);
+            int ans = 0;
+            while(!q.empty())
+            {
+                ans++;
+                int sz = q.size();
+                for(int i = 0; i < sz; i++)
+                {
+                    auto it = q.front();
+                    q.pop();
+                    if(!it)
+                        continue;
+                    if(!it->left && !it->right)
+                        return ans;
+                    q.push(it->left);
+                    q.push(it->right);
+                }
+            }
+            return ans;
+        }
 };
+    
+    
+//     Runtime: 290 ms, faster than 62.88% of C++ online submissions for Minimum Depth of Binary Tree.
+// Memory Usage: 144.8 MB, less than 16.53% of C++ online submissions for Minimum Depth of Binary Tree.
+    
+//     int minDepth(TreeNode* root) {
+//         if(!root)
+//             return 0;
+//         if(!root->left)
+//             return 1 + minDepth(root->right);
+//         if(!root->right)
+//             return 1 + minDepth(root->left);
+//         return 1 + min(minDepth(root->left), minDepth(root->right));   
+//     }
+// };
