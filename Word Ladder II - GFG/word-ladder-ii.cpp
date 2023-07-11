b2932e481e2af50636f74cfc4c59d1b616f516f3
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-11 07:16:37 +0530

    Added solution - LeetHub

--- Word Ladder II - GFG/word-ladder-ii.cpp
+++ Word Ladder II - GFG/word-ladder-ii.cpp
@@ -9,93 +9,156 @@ using namespace std;
 class Solution {
 public:
 
-    map<string, int> mp;
-    vector<vector<string>> ans;
-    string b;
-    
-    void dfs(string word, vector<string> &v)
-    {
-        // cout << word << endl;
-        if(mp[word] == 1)
-        // if(word == b)
-        {
-            reverse(v.begin(), v.end());
-            ans.push_back(v);
-            reverse(v.begin(), v.end());
-            return;
-        }
+    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList){
+        set<string> st(wordList.begin(), wordList.end());
         
-        int sz = word.size();
-        int steps = mp[word];
-        for(int i = 0; i < sz; i++)
+        vector<vector<string>> ans;
+        set<string> used;
+        queue<vector<string>> q;
+        q.push({beginWord});
+        int level = 0;
+        used.insert(beginWord);
+        int sz = beginWord.size();
+        
+        while(!q.empty())
         {
-            char original = word[i];
-            for(char c = 'a'; c <= 'z'; c++)
+            auto v = q.front();
+            q.pop();
+            
+            if(v.size() > level)
             {
-                word[i] = c;
-                // cout << word << " " << mp[word] << endl;
-                if(mp.find(word) != mp.end() && mp[word] + 1 == steps)
+                level++;
+                for(auto it : used)
                 {
-                    v.push_back(word);
-                    dfs(word, v);
-                    v.pop_back();
+                    st.erase(it);
                 }
             }
-            word[i] = original;
-        }
-    }
-
-    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
-        // code here
-        
-        b = beginWord;
+            auto word = v.back();
             
-        set<string> st;
-        for(auto i : wordList)
-            st.insert(i);
-        
-        // for(auto i : st)
-        //     cout << i << " ";
-        
-        queue<string> q;
-        q.push(beginWord);
-        mp[beginWord] = 1;
-        st.erase(beginWord);
-        int sz = beginWord.size();
-        while(!q.empty())
-        { 
-            string word = q.front();
-            q.pop();
-            int steps = mp[word];
+            // Storing the answer if we have reached the endWord
             if(word == endWord)
-                break;
+            {
+                if(ans.size() == 0)
+                    ans.push_back(v);
+                else if(ans[0].size() == v.size()) // For checking the shortest path
+                    ans.push_back(v);
+                continue;
+            }
+            
             for(int i = 0; i < sz; i++)
             {
-                char original = word[i]; 
+                char original = word[i];
                 for(char c = 'a'; c <= 'z'; c++)
                 {
                     word[i] = c;
                     if(st.count(word))
                     {
-                        // cout << word << endl;
-                        q.push(word);
-                        st.erase(word);
-                        mp[word] = steps + 1;
+                        v.push_back(word);
+                        q.push(v);
+                        v.pop_back();
+                        used.insert(word);
                     }
                 }
                 word[i] = original;
             }
         }
-        
-        if(mp.find(endWord) != mp.end())
-        {
-            vector<string> v;
-            v.push_back(endWord);
-            dfs(endWord, v);
-        }
+        // while(!q.empty()) // This will not work because the queue will be already empty
+        // {
+        //     ans.push_back(q.front());
+        //     q.pop();
+        // }
         return ans;
     }
 };
+
+
+//     map<string, int> mp;
+//     vector<vector<string>> ans;
+//     string b;
+    
+//     void dfs(string word, vector<string> &v)
+//     {
+//         // cout << word << endl;
+//         if(mp[word] == 1)
+//         // if(word == b)
+//         {
+//             reverse(v.begin(), v.end());
+//             ans.push_back(v);
+//             reverse(v.begin(), v.end());
+//             return;
+//         }
+        
+//         int sz = word.size();
+//         int steps = mp[word];
+//         for(int i = 0; i < sz; i++)
+//         {
+//             char original = word[i];
+//             for(char c = 'a'; c <= 'z'; c++)
+//             {
+//                 word[i] = c;
+//                 // cout << word << " " << mp[word] << endl;
+//                 if(mp.find(word) != mp.end() && mp[word] + 1 == steps)
+//                 {
+//                     v.push_back(word);
+//                     dfs(word, v);
+//                     v.pop_back();
+//                 }
+//             }
+//             word[i] = original;
+//         }
+//     }
+
+//     vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
+//         // code here
+        
+//         b = beginWord;
+            
+//         set<string> st;
+//         for(auto i : wordList)
+//             st.insert(i);
+        
+//         // for(auto i : st)
+//         //     cout << i << " ";
+        
+//         queue<string> q;
+//         q.push(beginWord);
+//         mp[beginWord] = 1;
+//         st.erase(beginWord);
+//         int sz = beginWord.size();
+//         while(!q.empty())
+//         { 
+//             string word = q.front();
+//             q.pop();
+//             int steps = mp[word];
+//             if(word == endWord)
+//                 break;
+//             for(int i = 0; i < sz; i++)
+//             {
+//                 char original = word[i]; 
+//                 for(char c = 'a'; c <= 'z'; c++)
+//                 {
+//                     word[i] = c;
+//                     if(st.count(word))
+//                     {
+//                         // cout << word << endl;
+//                         q.push(word);
+//                         st.erase(word);
+//                         mp[word] = steps + 1;
+//                     }
+//                 }
+//                 word[i] = original;
+//             }
+//         }
+        
+//         if(mp.find(endWord) != mp.end())
+//         {
+//             vector<string> v;
+//             v.push_back(endWord);
+//             dfs(endWord, v);
+//         }
+//         return ans;
+//     }
+// };
 
 //{ Driver Code Starts.
 
