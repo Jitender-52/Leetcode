d7d56f6a2caab84d1ceb3bc607dd21257d5de961
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-12 18:01:33 +0530

    Time: 165 ms (81.35%), Space: 47.8 MB (79.00%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,49 +1,87 @@
 class Solution {
 public:
     
-    bool cycle(int src, vector<int> &vis, vector<int> &dfsVis, vector<vector<int>> &graph)
-    {
-        vis[src] = 1;
-        dfsVis[src] = 1;
-        for(auto it : graph[src])
+        bool cycle(int src, vector<int> &vis, vector<int> &dfsVis, vector<vector<int>> &graph)
         {
-            if(dfsVis[it])
-                return true;
-            else if(!vis[it] && cycle(it, vis, dfsVis, graph))
-                return true;
+            dfsVis[src] = 1;
+            vis[src] = 1;
+            for(auto it : graph[src])
+            {
+                if(dfsVis[it])
+                    return true;
+                if(!vis[it] && cycle(it, vis, dfsVis, graph))
+                    return true;
+            }
+            dfsVis[src] = 0;
+            return false;
         }
-        dfsVis[src] = 0;
-        return false;
-    }
     
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> ans;
-        vector<int> vis(n), dfsVis(n);
-        // for(int i = 0; i < n; i++)
-        // {
-        //     vector<int> v;
-        //     if(!vis[i] && !cycle(i, vis, dfsVis, graph, v))
-        //     {
-        //         for(auto &it : v) 
-        //         {
-        //             ans.push_back(it);
-        //         }
-        //     }
-        // }
-        
-        for(int i = 0; i < n; i++)
-        {
-            cycle(i, vis, dfsVis, graph);
+        vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+            int n = graph.size();
+            vector<int> ans, vis(n, 0), dfsVis(n, 0);
+            
+            for(int i = 0; i < n; i++)
+            {
+                cycle(i, vis, dfsVis, graph);
+            }
+            
+            for(int i = 0; i < n; i++)
+            {
+                if(!dfsVis[i])
+                    ans.push_back(i);
+            }
+            sort(ans.begin(), ans.end());
+            return ans;
         }
-        
-        for(int i = 0; i < n; i++)
-        {
-            if(!dfsVis[i])
-                ans.push_back(i);
-        }
-        
-        sort(ans.begin(), ans.end());
-        return ans;
-    }
 };
+    
+    
+//     Runtime: 176 ms, faster than 61.87% of C++ online submissions for Find Eventual Safe States.
+// Memory Usage: 47.8 MB, less than 77.56% of C++ online submissions for Find Eventual Safe States.
+    
+//     bool cycle(int src, vector<int> &vis, vector<int> &dfsVis, vector<vector<int>> &graph)
+//     {
+//         vis[src] = 1;
+//         dfsVis[src] = 1;
+//         for(auto it : graph[src])
+//         {
+//             if(dfsVis[it])
+//                 return true;
+//             else if(!vis[it] && cycle(it, vis, dfsVis, graph))
+//                 return true;
+//         }
+//         dfsVis[src] = 0;
+//         return false;
+//     }
+    
+//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+//         int n = graph.size();
+//         vector<int> ans;
+//         vector<int> vis(n), dfsVis(n);
+//         // for(int i = 0; i < n; i++)
+//         // {
+//         //     vector<int> v;
+//         //     if(!vis[i] && !cycle(i, vis, dfsVis, graph, v))
+//         //     {
+//         //         for(auto &it : v) 
+//         //         {
+//         //             ans.push_back(it);
+//         //         }
+//         //     }
+//         // }
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             cycle(i, vis, dfsVis, graph);
+//         }
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(!dfsVis[i])
+//                 ans.push_back(i);
+//         }
+        
+//         sort(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
