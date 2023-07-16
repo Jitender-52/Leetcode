4b260b4def76c4316135c7bb44c858e5086e0133
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-16 18:58:18 +0530

    Time: 108 ms (59.17%), Space: 12.7 MB (28.59%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    
+    static bool comp(vector<int> &a, vector<int> &b)
+    {
+        if(a[0] == b[0])
+            return a[1] > b[1];
+        return a[0] < b[0];
+    }
+    
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> v(n, vector<int> (n, 1e9));
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i == j)
+                    v[i][j] = 0;
+            }
+        }
+        
+        for(auto i : edges)
+        {
+            int a = i[0], b = i[1], wt = i[2];
+            v[a][b] = wt;
+            v[b][a] = wt;
+        }
+        
+        for(int k = 0; k < n; k++)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if(v[i][k] + v[k][j] < v[i][j])
+                        v[i][j] = v[i][k] + v[k][j];
+                }
+            }
+        }
+        
+        vector<vector<int>> ans;
+        for(int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for(int j = 0; j < n; j++)
+            {
+                if(v[i][j] != 0 && v[i][j] <= distanceThreshold)
+                {
+                    count++;
+                }
+            }
+            ans.push_back({count, i});
+        }
+        
+        sort(ans.begin(), ans.end(), comp);
+        return ans[0][1];
+    }
+};
