95dd03b5b913a43239c577e53061a2f53c610de2
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-24 23:39:24 +0530

    Time: 134 ms (52.80%), Space: 47.3 MB (26.08%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.cpp
+++ 0851-loud-and-rich/0851-loud-and-rich.cpp
@@ -1,37 +1,83 @@
 class Solution {
 public:
     
-    int dfs(int src, vector<vector<int>> &adj, vector<int> &quiet, vector<int> &v)
-    {
-        if(v[src] != -1)
-            return v[src];
-        
-        v[src] = src;
-        for(auto i : adj[src])
-        {
-            int a = dfs(i, adj, quiet, v);
-            if(quiet[a] < quiet[v[src]])
-                v[src] = a;
-        }
-        return v[src];
-    }
-    
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
         int n = quiet.size();
         vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+
         for(auto i : richer)
         {
-            adj[i[1]].push_back(i[0]);
+            indegree[i[1]]++;
+            adj[i[0]].push_back(i[1]);
         }
         
-        vector<int> v(n, -1);
+        queue<int> q;
         for(int i = 0; i < n; i++)
         {
-            dfs(i, adj, quiet, v);
+            if(indegree[i] == 0)
+                q.push(i);
         }
-        return v;
+        
+        vector<int> ans(n);
+        
+        for(int i = 0; i < n; i++)
+            ans[i] = i;
+        
+        while(!q.empty())
+        {
+            auto node = q.front();
+            q.pop();
+            for(auto child : adj[node])
+            {
+                if(quiet[ans[child]] > quiet[ans[node]])
+                    ans[child] = ans[node];
+                indegree[child]--;
+                if(indegree[child] == 0)
+                    q.push(child);
+            }
+        }
+        return ans;        
     }
 };
+    
+    
+//     Runtime: 156 ms, faster than 23.19% of C++ online submissions for Loud and Rich.
+// Memory Usage: 46.7 MB, less than 55.59% of C++ online submissions for Loud and Rich.
+    
+    // DFS solution
+    
+//     int dfs(int src, vector<vector<int>> &adj, vector<int> &quiet, vector<int> &v)
+//     {
+//         if(v[src] != -1)
+//             return v[src];
+        
+//         v[src] = src;
+//         for(auto i : adj[src])
+//         {
+//             int a = dfs(i, adj, quiet, v);
+//             if(quiet[a] < quiet[v[src]])
+//                 v[src] = a;
+//         }
+//         return v[src];
+//     }
+    
+//     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+//         int n = quiet.size();
+//         vector<vector<int>> adj(n);
+//         for(auto i : richer)
+//         {
+//             adj[i[1]].push_back(i[0]);
+//         }
+        
+//         vector<int> v(n, -1);
+//         for(int i = 0; i < n; i++)
+//         {
+//             dfs(i, adj, quiet, v);
+//         }
+//         return v;
+//     }
+// };
     
     
 //     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
