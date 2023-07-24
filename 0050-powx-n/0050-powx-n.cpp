5fc16a0238a391172a0cee22346014e6a352a6dd
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-24 14:28:59 +0530

    Time: 0 ms (100.00%), Space: 6 MB (62.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
+    
+    double power(double x, long long n)
+    {
         if(n == 0)
             return 1;
-        if(n == 1)
-            return x;
-        
         if(n < 0)
-        {
-            if(n == INT_MIN)
-            {
-                double half = myPow(1/x, -(n/2));
-                return half * half;
-            }
-            return myPow(1/x, -n);
-        }
-        
-        double half = myPow(x, (n/2));
-        if(n & 1)
-            return half * half * x;
-        return half * half;
+            return power(1/x, -n);
+        double ans = power(x, n/2);
+        if(n % 2 == 0)
+            return ans * ans;
+        else
+            return ans * ans * x;
+    }
+    
+    double myPow(double x, int n) {
+        return power(x, n);
     }
 };
+    
+    
+    
+//     Runtime: 4 ms, faster than 40.58% of C++ online submissions for Pow(x, n).
+// Memory Usage: 6 MB, less than 35.64% of C++ online submissions for Pow(x, n).
+    
+    
+//     double myPow(double x, int n) {
+//         if(n == 0)
+//             return 1;
+//         if(n == 1)
+//             return x;
+        
+//         if(n < 0)
+//         {
+//             if(n == INT_MIN)
+//             {
+//                 double half = myPow(1/x, -(n/2));
+//                 return half * half;
+//             }
+//             return myPow(1/x, -n);
+//         }
+        
+//         double half = myPow(x, (n/2));
+//         if(n & 1)
+//             return half * half * x;
+//         return half * half;
+//     }
+// };
