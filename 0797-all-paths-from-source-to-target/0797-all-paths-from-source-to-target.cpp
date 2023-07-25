c3b7c191a019da9628a4c9c0098b9bac5347c120
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-25 11:36:47 +0530

    Time: 20 ms (39.66%), Space: 16.7 MB (15.96%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     
-    void dfs(int src, vector<vector<int>> &adj, vector<int> &v, vector<vector<int>> &ans)
-    {
-        int n = adj.size();
-        v.push_back(src);
-        if(src == n-1)
-        {
-            ans.push_back(v);
-            v.pop_back();
-            return;
-        }
-        for(auto i : adj[src])
-        {
-            dfs(i, adj, v, ans);
-        }
-        v.pop_back();
-    }
+    // BFS
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
@@ -24,12 +9,71 @@ public:
         for(int i = 0; i < n; i++)
         {
             for(auto j : graph[i])
+            {
                 adj[i].push_back(j);
+            }
         }
         
         vector<vector<int>> ans;
-        vector<int> v;
-        dfs(0, adj, v, ans);
+        queue<vector<int>> q;
+        q.push({0});
+        while(!q.empty())
+        {
+            auto path = q.front();
+            q.pop();
+            if(path.back() == n-1)
+            {
+                ans.push_back(path);
+            }
+            else
+            {
+                for(auto i : adj[path.back()])
+                {
+                    path.push_back(i);
+                    q.push(path);
+                    path.pop_back();
+                }
+            }
+        }
         return ans;
     }
 };
+    
+    
+    // DFS
+    
+//     Runtime: 11 ms, faster than 87.32% of C++ online submissions for All Paths From Source to Target.
+// Memory Usage: 10.6 MB, less than 64.13% of C++ online submissions for All Paths From Source to Target.
+    
+//     void dfs(int src, vector<vector<int>> &adj, vector<int> &v, vector<vector<int>> &ans)
+//     {
+//         int n = adj.size();
+//         v.push_back(src);
+//         if(src == n-1)
+//         {
+//             ans.push_back(v);
+//             v.pop_back();
+//             return;
+//         }
+//         for(auto i : adj[src])
+//         {
+//             dfs(i, adj, v, ans);
+//         }
+//         v.pop_back();
+//     }
+    
+//     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+//         int n = graph.size();
+//         vector<vector<int>> adj(n);
+//         for(int i = 0; i < n; i++)
+//         {
+//             for(auto j : graph[i])
+//                 adj[i].push_back(j);
+//         }
+        
+//         vector<vector<int>> ans;
+//         vector<int> v;
+//         dfs(0, adj, v, ans);
+//         return ans;
+//     }
+// };
