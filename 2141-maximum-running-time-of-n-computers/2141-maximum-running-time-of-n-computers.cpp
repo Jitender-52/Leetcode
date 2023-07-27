ecc600b522893665efb2dffa0fd2ab8074bde2fe
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-27 17:35:26 +0530

    Time: 175 ms (80.22%), Space: 55.7 MB (71.58%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,70 +1,95 @@
 class Solution {
 public:
     
+        long long maxRunTime(int n, vector<int>& batteries) {
+            long long sum = 0;
+            for(auto i : batteries)
+                sum += i;
+            long long left = 0, right = sum / n;
+            while(left <= right)
+            {
+                long long mid = (left + right) / 2;
+                sum = 0;
+                for(auto i : batteries)
+                    sum += min(1LL * i, mid);
+                if(sum >= mid * n)
+                    left = mid + 1;
+                else 
+                    right = mid - 1;
+            }
+            return left - 1;
+        }
+};
+    
+    
+    
+    
     // clean code
     
+//     Runtime: 2880 ms, faster than 5.40% of C++ online submissions for Maximum Running Time of N Computers.
+// Memory Usage: 351.4 MB, less than 6.83% of C++ online submissions for Maximum Running Time of N Computers.
     
-    bool isFeasible(long long mid, int n, vector<int> v)
-    {
-        int m = v.size();
-        vector<long long> batteries(m);
+//     bool isFeasible(long long mid, int n, vector<int> v)
+//     {
+//         int m = v.size();
+//         vector<long long> batteries(m);
         
-        for(int i = 0; i < m; i++)
-        {
-            batteries[i] = v[i];
-        }
+//         for(int i = 0; i < m; i++)
+//         {
+//             batteries[i] = v[i];
+//         }
         
-        sort(batteries.begin(), batteries.end());
-        for(int j = n; j < m; j++)
-        {
-            batteries[j] = min(batteries[j], mid);
-        }
+//         sort(batteries.begin(), batteries.end());
+//         for(int j = n; j < m; j++)
+//         {
+//             batteries[j] = min(batteries[j], mid);
+//         }
 
-        long long i = 0, j = m-1;
-        while(i < n && j >= n)
-        {
-            if(batteries[i] >= mid)
-                break;
-            while(i < n && j >= n && batteries[i] < mid)
-            {
-                long long a = mid - batteries[i];
-                if(a <= batteries[j])
-                {
-                    batteries[i] = mid;
-                    batteries[j] -= a;
-                    i++;
-                }
-                else
-                {
-                    batteries[i] += batteries[j];
-                    j--;
-                }
-            }
-        }
+//         long long i = 0, j = m-1;
+//         while(i < n && j >= n)
+//         {
+//             if(batteries[i] >= mid)
+//                 break;
+//             while(i < n && j >= n && batteries[i] < mid)
+//             {
+//                 long long a = mid - batteries[i];
+//                 if(a <= batteries[j])
+//                 {
+//                     batteries[i] = mid;
+//                     batteries[j] -= a;
+//                     i++;
+//                 }
+//                 else
+//                 {
+//                     batteries[i] += batteries[j];
+//                     j--;
+//                 }
+//             }
+//         }
 
-        bool check = true;
-        for(int i = 0; i < n; i++)
-        {
-            if(batteries[i] < mid)
-                check = false;
-        }
-        return check;
-    }
+//         bool check = true;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(batteries[i] < mid)
+//                 check = false;
+//         }
+//         return check;
+//     }
     
-    long long maxRunTime(int n, vector<int>& batteries) {
-        long long l = 0, r = 1e15;
+//     long long maxRunTime(int n, vector<int>& batteries) {
+//         long long l = 0, r = 1e15;
         
-        while(l <= r)
-        {
-            long long mid = (l + r) / 2;
-            if(isFeasible(mid, n, batteries))
-                l = mid + 1;
-            else
-                r = mid - 1;
-        }
-        return l - 1;
-    }
-};
+//         while(l <= r)
+//         {
+//             long long mid = (l + r) / 2;
+//             if(isFeasible(mid, n, batteries))
+//                 l = mid + 1;
+//             else
+//                 r = mid - 1;
+//         }
+//         return l - 1;
+//     }
+// };
     
     
     
