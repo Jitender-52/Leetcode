e8a1b33c1c07deb79386c54ae885dfa154dd9ced
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-07-28 18:42:31 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     
+//     Runtime: 137 ms, faster than 30.99% of C++ online submissions for Predict the Winner.
+// Memory Usage: 7.7 MB, less than 29.30% of C++ online submissions for Predict the Winner.
+    
     int diff(int i, int j, vector<int> &nums, vector<vector<int>> &dp)
     {
         int n = nums.size();
         if(i == j)
             return nums[i];
-        return max(nums[i] - diff(i+1, j, nums, dp), nums[j] - diff(i, j-1, nums, dp));
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = max(nums[i] - diff(i+1, j, nums, dp), nums[j] - diff(i, j-1, nums, dp));
     }
     
     bool PredictTheWinner(vector<int>& nums) {
