792aa658fe0c13eb654defe08c5a5561901d7081
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-08-12 06:23:36 +0530

    Time: 2 ms (68.17%), Space: 7.7 MB (81.25%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(obstacleGrid[i][j] == 1)
+                {
+                    obstacleGrid[i][j] = 0;
+                    continue;
+                }
+                if(i == 0 && j == 0)
+                    obstacleGrid[i][j] = 1;
+                if(i > 0)
+                    obstacleGrid[i][j] += obstacleGrid[i-1][j];
+                if(j > 0)
+                    obstacleGrid[i][j] += obstacleGrid[i][j-1];
+            }
+        }
+        return obstacleGrid[n-1][m-1];
+    }
+};
