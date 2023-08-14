6b8043093df93b12c30a5cef22f8ee61d0f3041b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-08-14 06:52:40 +0530

    Time: 65 ms (98.57%), Space: 49.4 MB (26.66%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
+    
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        priority_queue<int,vector<int>, greater<int>> pq;
+        vector<int> count(2e4+2, 0);
+        int &mini = *min_element(nums.begin(), nums.end());
         for(auto i : nums)
+            count[i - mini]++;
+  
+        for(int i = count.size()-1; i >= 0; i--)
         {
-            pq.push(i);
-            if(pq.size() > k)
-                pq.pop();
+            k -= count[i];
+            if(k <= 0)
+                return i + mini;
         }
-        return pq.top();
+        return mini;
     }
 };
+    
+    
+//     Runtime: 101 ms, faster than 73.98% of C++ online submissions for Kth Largest Element in an Array.
+// Memory Usage: 47.4 MB, less than 33.60% of C++ online submissions for Kth Largest Element in an Array.
+
+    
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         priority_queue<int,vector<int>, greater<int>> pq;
+//         for(auto i : nums)
+//         {
+//             pq.push(i);
+//             if(pq.size() > k)
+//                 pq.pop();
+//         }
+//         return pq.top();
+//     }
+// };
