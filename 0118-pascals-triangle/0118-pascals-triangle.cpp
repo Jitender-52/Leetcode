d93614b25974da84a1497c6ec32748e893b9a4e8
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-08 16:46:46 +0530

    Time: 4 ms (27.26%), Space: 6.9 MB (6.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 2 ms, faster than 50.82% of C++ online submissions for Pascal's Triangle.
+// Memory Usage: 6.8 MB, less than 18.64% of C++ online submissions for Pascal's Triangle.
+    
+    
     vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
         for(int i = 0; i < n; i++)
@@ -19,7 +24,7 @@ public:
                     }
                     else
                     {
-                        v.push_back(ans[i-1][j]+ ans[i-1][j-1]);
+                        v.push_back(ans[i-1][j] + ans[i-1][j-1]);
                     }
                 }
                 v.push_back(ans[i-1][i-1]);
