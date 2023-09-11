8deb8e351afb41277700857ba1f5c72fadc2f2af
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-11 18:31:20 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (88.22%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
+    
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         int n = groupSizes.size();
-        map<int, vector<int>> mp;
+        unordered_map<int, vector<int>> mp;
+        vector<vector<int>> ans;
         for(int i = 0; i < n; i++)
         {
             mp[groupSizes[i]].push_back(i);
-        }
-        
-        vector<vector<int>> ans;
-        vector<int> v;
-        for(auto i : mp)
-        {
-            for(int j = 0; j < i.second.size(); j++)
+            if(mp[groupSizes[i]].size() == groupSizes[i])
             {
-                v.push_back(i.second[j]);
-                if(v.size() == i.first)
-                {
-                    ans.push_back(v);
-                    v.clear();
-                }
+                ans.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]] = {};
             }
         }
         return ans;
     }
 };
+    
+    
+//     Runtime: 14 ms, faster than 35.21% of C++ online submissions for Group the People Given the Group Size They Belong To.
+// Memory Usage: 13.1 MB, less than 66.73% of C++ online submissions for Group the People Given the Group Size They Belong To.
+    
+//     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+//         int n = groupSizes.size();
+//         map<int, vector<int>> mp;
+//         for(int i = 0; i < n; i++)
+//         {
+//             mp[groupSizes[i]].push_back(i);
+//         }
+        
+//         vector<vector<int>> ans;
+//         vector<int> v;
+//         for(auto i : mp)
+//         {
+//             for(int j = 0; j < i.second.size(); j++)
+//             {
+//                 v.push_back(i.second[j]);
+//                 if(v.size() == i.first)
+//                 {
+//                     ans.push_back(v);
+//                     v.clear();
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
