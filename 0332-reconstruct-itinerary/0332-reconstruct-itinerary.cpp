19e156253fc7083148f78131577436dd074435b4
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-15 10:25:58 +0530

    Time: 23 ms (49.31%), Space: 15.5 MB (29.55%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -59,6 +59,120 @@ public:
 //     void dfs(string s, map<string, set<string>> &mp, vector<string> &ans)
 //     {
 //         // ans.push_back(s);
+//         // if(mp[s].size() == 0)
+//         //     return;
+        
+//         // for(auto i : mp[s])
+//         // {
+//         //     auto x = i;
+//         //     mp[s].erase(x);
+//         //     dfs(i, mp, ans);
+//         // }
+        
+//         while(mp[s].size())
+//         {
+//             auto it = *mp[s].begin();
+//             mp[s].erase(it);
+//             dfs(it, mp, ans);
+//         }
+//         ans.push_back(s);
+        
+//         // auto it = mp[s].begin();
+//         // while(it != mp[s].end())
+//         // {
+//         //     auto p = it;
+//         //     auto x = *it;
+//         //     it++;
+//         //     mp[s].erase(p);
+//         //     dfs(x, mp, ans);
+//         // }
+        
+//         // string x = *mp[s].begin();
+//         // mp[s].erase(x);
+//         // dfs(x, mp, ans);
+//     }
+    
+//     vector<string> findItinerary(vector<vector<string>>& tickets) {
+//         map<string, set<string>> mp;
+//         for(auto v : tickets)
+//         {
+//             mp[v[0]].insert(v[1]);
+//         }
+//         string s = tickets[0][0];
+//         for(auto i : mp["JFK"])
+//         {
+//             s = min(s, i);
+//         }
+        
+//         vector<string> ans;
+//         dfs("JFK", mp, ans);
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+    
+    
+//     Runtime: 20 ms, faster than 66.94% of C++ online submissions for Reconstruct Itinerary.
+// Memory Usage: 15.5 MB, less than 29.44% of C++ online submissions for Reconstruct Itinerary.
+    
+//     void dfs(string s,  map<string, priority_queue<string, vector<string>, greater<string>> > &mp, vector<string> &ans)
+//     {
+//         // ans.push_back(s);
+        
+//         auto &pq = mp[s];
+//         while(!pq.empty())
+//         {
+//             auto it = pq.top();
+//             pq.pop();
+//             dfs(it, mp, ans);
+//         }
+        
+//         // for(auto i : mp[s])
+//         // {
+//         //     auto x = i;
+//         //     mp[s].erase(x);
+//         //     dfs(i, mp, ans);
+//         // }
+        
+//         // auto it = mp[s].begin();
+//         // while(it != mp[s].end())
+//         // {
+//         //     auto p = it;
+//         //     auto x = *it;
+//         //     it++;
+//         //     mp[s].erase(p);
+//         //     dfs(x, mp, ans);
+//         // }
+        
+//         ans.push_back(s);
+//         // string x = *mp[s].begin();
+//         // mp[s].erase(x);
+//         // dfs(x, mp, ans);
+//     }
+    
+//     vector<string> findItinerary(vector<vector<string>>& tickets) {
+//         map<string, priority_queue<string, vector<string>, greater<string>> > mp;
+//         for(auto v : tickets)
+//         {
+//             // mp[v[0]].insert(v[1]);
+//             mp[v[0]].push(v[1]);
+//         }
+//         // string s = tickets[0][0];
+//         // for(auto i : mp["JFK"])
+//         // {
+//         //     s = min(s, i);
+//         // }
+        
+//         vector<string> ans;
+//         dfs("JFK", mp, ans);
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+    
+//     void dfs(string s, map<string, set<string>> &mp, vector<string> &ans)
+//     {
+//         // ans.push_back(s);
 //         if(mp[s].size() == 0)
 //             return;
         
