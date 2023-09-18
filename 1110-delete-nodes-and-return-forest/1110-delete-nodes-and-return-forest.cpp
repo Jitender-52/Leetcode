fae85d5ab92488fb494c7c247c9a7f560e09f778
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-18 23:31:09 +0530

    Time: 19 ms (62.93%), Space: 25.4 MB (57.59%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode *dfs(TreeNode *root, vector<int> &to_delete, vector<TreeNode *> &ans)
+    {
+        if(!root)
+            return NULL;
+        int n = to_delete.size();
+        bool check = false;
+        for(int i = 0; i < n; i++)
+        {
+            if(root->val == to_delete[i])
+            {
+                check = true;
+            }
+        }
+        root->left = dfs(root->left, to_delete, ans);
+        root->right = dfs(root->right, to_delete, ans);
+        if(check)
+        {
+            if(root->left)
+                ans.push_back(root->left);
+            if(root->right)
+                ans.push_back(root->right);
+            return NULL;
+        }
+        return root;
+    }
+    
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode *> ans;
+        if(dfs(root, to_delete, ans))
+            ans.push_back(root);
+        return ans;
+    }
+};
