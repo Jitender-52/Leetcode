41b6fa2381f5e2be291256d831f1a7544137f21b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-21 18:19:56 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -8,46 +8,20 @@ class Solution
 {
     public:
     //Function to find the maximum money the thief can get.
-    
-    int solve(int i, int arr[], int n, vector<int> &dp)
-    {
-        if(i >= n)
-            return 0;
-        if(dp[i] != -1)
-            return dp[i];
-        return dp[i] = max(arr[i] + solve(i+2, arr, n, dp), solve(i+1, arr, n, dp));
-    }
-    
     int FindMaxSum(int arr[], int n)
     {
         // Your code here
-        vector<int> dp(n+2, 0);
-        // return solve(0, arr, n, dp);
-        
-        // for(int i = 0; i < n; i++)
-        // {
-        //     dp[i] = arr[i];
-        //     if(i - 1 >= 0)
-        //         dp[i] = max(dp[i], dp[i-1]);
-        //     if(i - 2 >= 0)
-        //         dp[i] = max(dp[i], arr[i] + dp[i-2]);
-        // }
-        // return dp[n-1];
-        
-        // for(int i = n-1; i >= 0; i--)
-        // {
-        //     dp[i] = max(dp[i+1], arr[i] + dp[i+2]);
-        // }
-        // return dp[0];
-        
-        int next = 0, nnext = 0, curr = 0;
-        for(int i = n-1; i >= 0; i--)
+        vector<int> v(n, 0);
+        v[0] = arr[0];
+        v[1] = max(arr[0],  arr[1]);
+        for(int i = 2; i < n; i++)
         {
-            curr = max(next, nnext + arr[i]);
-            nnext = next;
-            next = curr;
+            v[i] = max(v[i-1], v[i-2] + arr[i]);
         }
-        return next;
+        // for(auto i : v)
+        //     cout << i << " ";
+        // cout << endl;
+        return v[n-1];
     }
 };
 
