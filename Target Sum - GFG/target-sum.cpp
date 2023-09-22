cc40addecd3c55d9b38bba670aef4eb15d430de3
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-22 14:07:04 +0530

    Added solution - LeetHub

--- Target Sum - GFG/target-sum.cpp
+++ Target Sum - GFG/target-sum.cpp
@@ -10,24 +10,55 @@ using namespace std;
 
 class Solution {
   public:
-  
-    int solve(int i, int target, vector<int> &a, vector<vector<int>> &dp)
+
+    int solve(int i, int sum, vector<int> &a, vector<vector<int>> &dp)
     {
-        int n = a.size();
-        if(i >= n)
-            return target == 0 ? 1 : 0;
-        // if(dp[i][target] != -1)
-        //     return dp[i][target + 1e4];
-        return solve(i + 1, target + a[i], a, dp) + solve(i + 1, target - a[i], a, dp);
+        int n = a.size();   
+        if(i == n)
+        {
+            return sum == 0 ? 1 : 0;   
+        }
+        if(i >= n || sum < 0)
+            return 0;
+        // if(dp[i][sum] != -1)
+        //     return dp[i][sum];
+        return dp[i][sum] = solve(i+1, sum - a[i], a, dp) + solve(i+1, sum, a, dp);
     }
-  
+
     int findTargetSumWays(vector<int>&a, int target) {
-        //Your code here
         int n = a.size();
-        vector<vector<int>> dp(n, vector<int> (1e4, -1));
-        return solve(0, target, a, dp);
+        // This problem is same as diff s1 - s2 == d; where d is target;
+        int sum = 0;
+        for(auto i : a)
+            sum += i;
+        sum -= target;
+        if(sum % 2 == 1 || sum < 0)
+            return 0;
+        sum /= 2;
+        vector<vector<int>> dp(n, vector<int> (sum + 1, -1));
+        return solve(0, sum, a, dp);
     }
 };
+
+
+  
+//     int solve(int i, int target, vector<int> &a, vector<vector<int>> &dp)
+//     {
+//         int n = a.size();
+//         if(i >= n)
+//             return target == 0 ? 1 : 0;
+//         // if(dp[i][target] != -1)
+//         //     return dp[i][target + 1e4];
+//         return solve(i + 1, target + a[i], a, dp) + solve(i + 1, target - a[i], a, dp);
+//     }
+  
+//     int findTargetSumWays(vector<int>&a, int target) {
+//         //Your code here
+//         int n = a.size();
+//         vector<vector<int>> dp(n, vector<int> (1e4, -1));
+//         return solve(0, target, a, dp);
+//     }
+// };
 
 //{ Driver Code Starts.
 
