e40cbc03f6cf7ebc5174e853411b6258674749b0
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-25 09:50:21 +0530

    Time: 3 ms (65.73%), Space: 7 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        map<char, int> mp;
+        for(auto i : t)
+            mp[i]++;
+        for(auto i : s)
+            mp[i]--;
+        for(auto i : mp)
+        {
+            if(i.second)
+                return i.first;
+        }
+        return '*';
+    }
+};
