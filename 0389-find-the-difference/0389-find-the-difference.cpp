db7b199665fb5834124f43c5384288946bd07dd9
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-25 09:52:06 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    
     char findTheDifference(string s, string t) {
-        map<char, int> mp;
+        int a = 0;
         for(auto i : t)
-            mp[i]++;
+            a += i - '0';
         for(auto i : s)
-            mp[i]--;
-        for(auto i : mp)
-        {
-            if(i.second)
-                return i.first;
-        }
-        return '*';
+            a -= i - '0';
+        return (a + '0');
     }
 };
+    
+    
+//     Runtime: 3 ms, faster than 65.73% of C++ online submissions for Find the Difference.
+// Memory Usage: 7 MB, less than 8.68% of C++ online submissions for Find the Difference.
+    
+//     char findTheDifference(string s, string t) {
+//         map<char, int> mp;
+//         for(auto i : t)
+//             mp[i]++;
+//         for(auto i : s)
+//             mp[i]--;
+//         for(auto i : mp)
+//         {
+//             if(i.second)
+//                 return i.first;
+//         }
+//         return '*';
+//     }
+// };
