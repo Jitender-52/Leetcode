83040e0de695f423839e8547a03260ee8020457e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-09-28 20:37:55 +0530

    Time: 4 ms (81.25%), Space: 16.7 MB (18.22%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,9 +1,31 @@
 class Solution {
 public:
+    
     vector<int> sortArrayByParity(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), [&](int a, int b){
-            return (a % 2 == 0 && b % 2 == 1);
-        });
-        return nums;
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] % 2 == 0)
+                ans.push_back(nums[i]);
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] % 2 == 1)
+                ans.push_back(nums[i]);
+        }
+        return ans;
     }
 };
+    
+    
+//     Runtime: 10 ms, faster than 32.57% of C++ online submissions for Sort Array By Parity.
+// Memory Usage: 16.5 MB, less than 36.74% of C++ online submissions for Sort Array By Parity.
+    
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         sort(nums.begin(), nums.end(), [&](int a, int b){
+//             return (a % 2 == 0 && b % 2 == 1);
+//         });
+//         return nums;
+//     }
+// };
