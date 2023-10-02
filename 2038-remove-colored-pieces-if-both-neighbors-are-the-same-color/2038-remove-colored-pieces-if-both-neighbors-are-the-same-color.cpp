208a156095587bdf36a31fc7a0eee0cc6bb4c803
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-02 20:48:59 +0530

    Time: 34 ms (57.13%), Space: 14 MB (5.97%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
-    bool winnerOfGame(string colors) {
+
+     bool winnerOfGame(string colors) {
         int n = colors.size();
         int a = 0, b = 0;
         for(int i = 1; i < n-1; i++)
         {
-            if(colors[i] == 'A' && colors[i-1] == colors[i] && colors[i+1] == colors[i])
-                a++;
-            else if(colors[i] == 'B' && colors[i-1] == colors[i] && colors[i+1] == colors[i])
-                b++;
+            if(colors[i] == colors[i-1] && colors[i] == colors[i+1])
+            {
+                if(colors[i] == 'A')
+                    a++;
+                else 
+                    b++;
+            }
         }
         return a > b;
     }
 };
+    
+//     Runtime: 41 ms, faster than 8.04% of C++ online submissions for Remove Colored Pieces if Both Neighbors are the Same Color.
+// Memory Usage: 13.6 MB, less than 17.42% of C++ online submissions for Remove Colored Pieces if Both Neighbors are the Same Color.
+
+    
+//     bool winnerOfGame(string colors) {
+//         int n = colors.size();
+//         int a = 0, b = 0;
+//         for(int i = 1; i < n-1; i++)
+//         {
+//             if(colors[i] == 'A' && colors[i-1] == colors[i] && colors[i+1] == colors[i])
+//                 a++;
+//             else if(colors[i] == 'B' && colors[i-1] == colors[i] && colors[i+1] == colors[i])
+//                 b++;
+//         }
+//         return a > b;
+//     }
+// };
