d8799076d45d4332e9fcef1ea30c058dc699b917
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-04 20:31:34 +0530

    Time: 11 ms (5.66%), Space: 15.7 MB (6.97%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
+    
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (2, 1));
-        
-        int ans = INT_MIN;
+        vector<int> prev(2, 1);
+        int ans = -1e9;
         for(int i = n-1; i >= 0; i--)
         {
-            int a = dp[i+1][0] * nums[i];
-            int b = dp[i+1][1] * nums[i];
-            dp[i][0] = min(nums[i], min(a, b));
-            dp[i][1] = max(nums[i], max(a, b));
-            ans = max(ans, max(dp[i][0], dp[i][1]));
+            vector<int> curr(2, nums[i]);
+            int a = prev[0] * nums[i];
+            int b = prev[1] * nums[i];
+            curr[0] = min(curr[0], min(a, b));
+            curr[1] = max(curr[1], max(a, b));
+            ans = max({ans, curr[0], curr[1]});
+            prev = curr;
         }
         return ans;
     }
 };
+    
+    
+//     Runtime: 15 ms, faster than 5.66% of C++ online submissions for Maximum Product Subarray.
+// Memory Usage: 17 MB, less than 6.97% of C++ online submissions for Maximum Product Subarray.
+    
+//     int maxProduct(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n+1, vector<int> (2, 1));
+        
+//         int ans = INT_MIN;
+//         for(int i = n-1; i >= 0; i--)
+//         {
+//             int a = dp[i+1][0] * nums[i];
+//             int b = dp[i+1][1] * nums[i];
+//             dp[i][0] = min(nums[i], min(a, b));
+//             dp[i][1] = max(nums[i], max(a, b));
+//             ans = max(ans, max(dp[i][0], dp[i][1]));
+//         }
+//         return ans;
+//     }
+// };
