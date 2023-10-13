37fc70bc554d15ba21e8306c8ebdd2ebcf6d6a00
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-13 10:05:17 +0530

    Time: 100 ms (61.57%), Space: 58.3 MB (64.17%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
+    
     int maxOperations(vector<int>& nums, int k) {
         int n = nums.size();
-        map<int,int> mp;
-        int ans = 0;
-        for(int i = 0; i < n; i++)
+        sort(nums.begin(), nums.end());
+        int i = 0, j = n-1, ans = 0;
+        while(i < j)
         {
-            if(mp.find(k - nums[i]) != mp.end())
+            if(nums[i] + nums[j] == k)
             {
-                mp[k - nums[i]]--;
-                if(mp[k-nums[i]] == 0)
-                    mp.erase(k - nums[i]);
                 ans++;
+                i++;
+                j--;
             }
+            else if(nums[i] + nums[j] > k)
+                j--;
             else
-            {
-                mp[nums[i]]++;
-            }
+                i++;
         }
         return ans;
     }
 };
+    
+//     Runtime: 175 ms, faster than 7.72% of C++ online submissions for Max Number of K-Sum Pairs.
+// Memory Usage: 70.7 MB, less than 21.84% of C++ online submissions for Max Number of K-Sum Pairs.
+    
+//     int maxOperations(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         map<int,int> mp;
+//         int ans = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(mp.find(k - nums[i]) != mp.end())
+//             {
+//                 mp[k - nums[i]]--;
+//                 if(mp[k-nums[i]] == 0)
+//                     mp.erase(k - nums[i]);
+//                 ans++;
+//             }
+//             else
+//             {
+//                 mp[nums[i]]++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
