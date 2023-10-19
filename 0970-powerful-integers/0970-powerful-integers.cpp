c21e6799b78da57efaff024fbfb5e1cdc8756cf3
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-19 22:50:42 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (52.75%) - LeetHub

--- 0970-powerful-integers/0970-powerful-integers.cpp
+++ 0970-powerful-integers/0970-powerful-integers.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+
     vector<int> powerfulIntegers(int x, int y, int bound) {
         set<int> st;
         int a = 1;
-        for(int i = 0; i <= 20; i++)
+        while(a < bound)
         {
             int b = 1;
-            if(a > bound)
-                break;
-            for(int j = 0; j <= 20; j++) // mybad using i++ here instead of j++;
+            while(a + b <= bound)
             {
-                if(a + b <= bound)
-                    st.insert(a + b);
-                else
+                st.insert(a + b);
+                if(y == 1)
                     break;
                 b *= y;
             }
+            if(x == 1)
+                break;
             a *= x;
         }
         vector<int> ans;
@@ -24,3 +24,32 @@ public:
         return ans;
     }
 };
+    
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Powerful Integers.
+// Memory Usage: 6.9 MB, less than 63.76% of C++ online submissions for Powerful Integers.
+    
+//     vector<int> powerfulIntegers(int x, int y, int bound) {
+//         set<int> st;
+//         int a = 1;
+//         for(int i = 0; i <= 20; i++)
+//         {
+//             int b = 1;
+//             if(a > bound)
+//                 break;
+//             for(int j = 0; j <= 20; j++) // mybad using i++ here instead of j++;
+//             {
+//                 if(a + b <= bound)
+//                     st.insert(a + b);
+//                 else
+//                     break;
+//                 b *= y;
+//             }
+//             a *= x;
+//         }
+//         vector<int> ans;
+//         for(auto i : st)
+//             ans.push_back(i);
+//         return ans;
+//     }
+// };
