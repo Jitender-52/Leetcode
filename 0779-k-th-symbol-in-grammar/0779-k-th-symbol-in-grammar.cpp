8d35eb21c907e7c5c72ce7bcadc24b8661e15d1f
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-25 21:44:36 +0530

    Time: 2 ms (51.37%), Space: 6.4 MB (12.22%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int depthFirstSearch(int n, int k, int rootVal) {
+        if (n == 1) {
+            return rootVal;
+        }
+
+        int totalNodes = pow(2, n - 1);
+        if (k > (totalNodes / 2)) 
+        {
+            int nextRootVal = (rootVal == 0) ? 1 : 0;
+            return depthFirstSearch(n - 1, k - (totalNodes / 2), nextRootVal);
+        }
+        else 
+        {
+            int nextRootVal = (rootVal == 0) ? 0 : 1;
+            return depthFirstSearch(n - 1, k, nextRootVal);
+        }
+    }
+
+    int kthGrammar(int n, int k) {
+        return depthFirstSearch(n, k, 0);
+    }
+};
