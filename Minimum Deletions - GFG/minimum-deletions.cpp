f6620f82101c529dce1693373b13c50227934d76
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-27 13:21:03 +0530

    Added solution - LeetHub

--- Minimum Deletions - GFG/minimum-deletions.cpp
+++ Minimum Deletions - GFG/minimum-deletions.cpp
@@ -15,14 +15,16 @@ class Solution{
         int n = s.length();
         vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
         
-        for(int i = 0; i < n; i++)
-            dp[i][i] = 1;
+        // for(int i = 0; i < n; i++)
+        //     dp[i][i] = 1;
             
         for(int i = n-1; i >= 0; i--)
         {
-            for(int j = i+1; j < n; j++)
+            for(int j = i; j < n; j++)
             {
-                if(s[i] == s[j])
+                if(i == j)
+                    dp[i][j] = 1;
+                else if(s[i] == s[j])
                     dp[i][j] = max(dp[i][j], 2 + dp[i+1][j-1]);
                 else
                     dp[i][j] = max(dp[i][j], max(dp[i+1][j], dp[i][j-1]));
