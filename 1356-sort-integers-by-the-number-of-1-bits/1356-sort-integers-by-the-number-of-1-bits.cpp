e68a872d8dfda01f7cbacbde038a956fa419d519
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-30 23:15:33 +0530

    Time: 9 ms (43.13%), Space: 10.5 MB (66.29%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
+    
     vector<int> sortByBits(vector<int>& arr) {
         sort(arr.begin(), arr.end(), [&](int a, int b){
-            int x = 0, y = 0;
-            for(int i = 0; i < 20; i++)
-            {
-                if((1 << i) & a)
-                    x++;
-                if((1 << i) & b)
-                    y++;
-            }
+            int x = __builtin_popcount(a), y = __builtin_popcount(b);
             if(x == y)
                 return a < b;
             return x < y;
@@ -17,3 +11,24 @@ public:
         return arr;
     }
 };
+    
+//     Runtime: 22 ms, faster than 6.90% of C++ online submissions for Sort Integers by The Number of 1 Bits.
+// Memory Usage: 10.4 MB, less than 66.29% of C++ online submissions for Sort Integers by The Number of 1 Bits.
+    
+//     vector<int> sortByBits(vector<int>& arr) {
+//         sort(arr.begin(), arr.end(), [&](int a, int b){
+//             int x = 0, y = 0;
+//             for(int i = 0; i < 20; i++)
+//             {
+//                 if((1 << i) & a)
+//                     x++;
+//                 if((1 << i) & b)
+//                     y++;
+//             }
+//             if(x == y)
+//                 return a < b;
+//             return x < y;
+//         });
+//         return arr;
+//     }
+// };
