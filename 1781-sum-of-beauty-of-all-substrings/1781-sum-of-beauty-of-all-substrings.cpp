3b5c614aa7d6a8f143011d4d9841d81f19c8e81c
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-10-31 21:59:59 +0530

    Time: 257 ms (77.66%), Space: 36.8 MB (7.77%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 230 ms, faster than 84.69% of C++ online submissions for Sum of Beauty of All Substrings.
+// Memory Usage: 36.6 MB, less than 7.77% of C++ online submissions for Sum of Beauty of All Substrings.
+    
     int beautySum(string s) {
         int n = s.length(), ans = 0;
         for(int i = 0; i < n; i++)
@@ -11,7 +15,8 @@ public:
             {
                 v[s[j] - 'a']++;
                 int a = v[s[j] - 'a'];
-                if(a - 1 > 0)
+                // if(a - 1 > 0) // it's is better than down condition. Accepted with it
+                if(mp.find(a-1) != mp.end()) // just for checking 
                 {
                     mp[a-1]--;
                     if(mp[a-1] == 0)
