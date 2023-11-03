780a5964335189ebb17c832e1c9a15052b881e7a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-11-03 21:49:02 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (49.67%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 5 ms, faster than 39.00% of C++ online submissions for Build an Array With Stack Operations.
+// Memory Usage: 8.2 MB, less than 49.67% of C++ online submissions for Build an Array With Stack Operations.
+    
     vector<string> buildArray(vector<int>& target, int n) {
          vector<string> res;
         string ph = "Push";
