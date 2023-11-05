be9b07998b1950cf2448d7cdf5c8e4941b4325fd
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-11-05 18:33:59 +0530

    Time: 22 ms (25.86%), Space: 23.3 MB (15.95%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     
-//     Runtime: 17 ms, faster than 68.53% of C++ online submissions for Last Moment Before All Ants Fall Out of a Plank.
-// Memory Usage: 23 MB, less than 76.29% of C++ online submissions for Last Moment Before All Ants Fall Out of a Plank.
-    
-     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         int ans = 0;
-        for (int num : left) {
-            ans = max(ans, num);
-        }
-        
-        for (int num : right) {
-            ans = max(ans, n - num);
-        }
-        
+        for(auto i : left)
+            ans = max(ans, i);
+        for(auto i : right)
+            ans = max(ans, n - i);
         return ans;
     }
 };
+    
+//     Runtime: 17 ms, faster than 68.53% of C++ online submissions for Last Moment Before All Ants Fall Out of a Plank.
+// Memory Usage: 23 MB, less than 76.29% of C++ online submissions for Last Moment Before All Ants Fall Out of a Plank.
+    
+//      int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+//         int ans = 0;
+//         for (int num : left) {
+//             ans = max(ans, num);
+//         }
+        
+//         for (int num : right) {
+//             ans = max(ans, n - num);
+//         }
+        
+//         return ans;
+//     }
+// };
