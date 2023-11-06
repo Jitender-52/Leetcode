a590ef4d655d8dcb2614633d80f2621e3eb4c5fc
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-11-06 22:40:35 +0530

    Time: 326 ms (38.31%), Space: 148 MB (67.86%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -1,22 +1,42 @@
 class SeatManager {
 public:
-    
-    set<int> st;
+   
+    priority_queue<int, vector<int>, greater<int>> pq;
     SeatManager(int n) {
         for(int i = 1; i <= n; i++)
-            st.insert(i);
+            pq.push(i);
     }
     
     int reserve() {
-        int ans = *st.begin();
-        st.erase(st.begin());
+        int ans = pq.top();
+        pq.pop();
         return ans;
     }
     
     void unreserve(int seatNumber) {
-        st.insert(seatNumber);
+        pq.push(seatNumber);
     }
 };
+    
+//     Runtime: 469 ms, faster than 15.78% of C++ online submissions for Seat Reservation Manager.
+// Memory Usage: 190.8 MB, less than 12.34% of C++ online submissions for Seat Reservation Manager.
+    
+//     set<int> st;
+//     SeatManager(int n) {
+//         for(int i = 1; i <= n; i++)
+//             st.insert(i);
+//     }
+    
+//     int reserve() {
+//         int ans = *st.begin();
+//         st.erase(st.begin());
+//         return ans;
+//     }
+    
+//     void unreserve(int seatNumber) {
+//         st.insert(seatNumber);
+//     }
+// };
 
 /**
  * Your SeatManager object will be instantiated and called as such:
