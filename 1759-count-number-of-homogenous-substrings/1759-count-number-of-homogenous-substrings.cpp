8ed831315914b977bc1f1ef3292598155b05fe98
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-11-09 22:01:58 +0530

    Time: 29 ms (34.13%), Space: 12.1 MB (53.48%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     
+//     Runtime: 25 ms, faster than 63.26% of C++ online submissions for Count Number of Homogenous Substrings.
+// Memory Usage: 12.3 MB, less than 16.96% of C++ online submissions for Count Number of Homogenous Substrings.
+
+    
     const int mod = 1e9 + 7;
     
     int countHomogenous(string s) {
