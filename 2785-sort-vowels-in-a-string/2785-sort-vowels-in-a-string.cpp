7634e59d4d36e97bd7552017a3c3dad3ca8076ba
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-11-13 23:22:59 +0530

    Time: 33 ms (87.60%), Space: 12.3 MB (82.05%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     
+//     Runtime: 42 ms, faster than 52.39% of C++ online submissions for Sort Vowels in a String.
+// Memory Usage: 12.3 MB, less than 82.05% of C++ online submissions for Sort Vowels in a String.
+    
     bool isVowel(char a)
     {
         if(a == 'a' || a == 'e' || a == 'i' || a =='o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a =='O' || a == 'U')
