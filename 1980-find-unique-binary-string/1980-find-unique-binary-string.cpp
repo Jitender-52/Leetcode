036da3b19a93518812a5769fccd8133104b12286
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-11-16 22:03:39 +0530

    Time: 3 ms (74.62%), Space: 10.5 MB (75.23%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
     
-    int n;
-    set<string> st;
-    string ans;
-    
-    void find(int i, string &s)
-    {
-        if(i == n)
-        {
-            if(st.find(s) == st.end())
-                ans = s;
-            return;
-        }
-        s += '0';
-        find(i+1, s);
-        s.pop_back();
-        s += '1';
-        find(i+1, s);
-        s.pop_back();
-    }
-    
     string findDifferentBinaryString(vector<string>& nums) {
-        n = nums[0].size();
-        for(auto i : nums)
-            st.insert(i);
-        string s = "";
-        find(0, s);
+        int n = nums.size();
+        string ans = "";
+        for(int i = 0; i < n; i++)
+        {
+            ans += nums[i][i] == '0' ? '1' : '0';
+        }
         return ans;
     }
 };
+    
+//     Runtime: 371 ms, faster than 16.92% of C++ online submissions for Find Unique Binary String.
+// Memory Usage: 10.8 MB, less than 43.66% of C++ online submissions for Find Unique Binary String.
+    
+//     int n;
+//     set<string> st;
+//     string ans;
+    
+//     void find(int i, string &s)
+//     {
+//         if(i == n)
+//         {
+//             if(st.find(s) == st.end())
+//                 ans = s;
+//             return;
+//         }
+//         s += '0';
+//         find(i+1, s);
+//         s.pop_back();
+//         s += '1';
+//         find(i+1, s);
+//         s.pop_back();
+//     }
+    
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         n = nums[0].size();
+//         for(auto i : nums)
+//             st.insert(i);
+//         string s = "";
+//         find(0, s);
+//         return ans;
+//     }
+// };
