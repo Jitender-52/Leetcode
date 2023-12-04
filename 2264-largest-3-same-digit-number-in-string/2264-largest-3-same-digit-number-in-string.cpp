837bcb4683ff2817088badd1e298eb123be3afbc
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-04 23:17:28 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (31.59%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     string largestGoodInteger(string num) {
         int n = num.size();
         char a = '*';
@@ -13,11 +14,33 @@ public:
                     a = max(a, num[i]);
             }
         }
-        if(a == '*')
-            return "";
-        string ans = "";
-        for(int i = 0; i < 3; i++)
-            ans += a;
-        return ans;
+        return (a == '*') ? "" : string(3, a);
     }
 };
+    
+    
+//     Runtime: 4 ms, faster than 47.83% of C++ online submissions for Largest 3-Same-Digit Number in String.
+// Memory Usage: 6.9 MB, less than 31.59% of C++ online submissions for Largest 3-Same-Digit Number in String.
+
+    
+//     string largestGoodInteger(string num) {
+//         int n = num.size();
+//         char a = '*';
+//         for(int i = 1; i < n-1; i++)
+//         {
+//             if(num[i] == num[i-1] && num[i] == num[i+1])
+//             {
+//                 if(a == '*')
+//                     a = num[i];
+//                 else
+//                     a = max(a, num[i]);
+//             }
+//         }
+//         if(a == '*')
+//             return "";
+//         string ans = "";
+//         for(int i = 0; i < 3; i++)
+//             ans += a;
+//         return ans;
+//     }
+// };
