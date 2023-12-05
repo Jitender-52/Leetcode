faee03055011abf3ab8f37a4690a59729526946d
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-05 23:33:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (73.09%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    
     int numberOfMatches(int n) {
-        int ans = 0;
-        while(n > 1)
-        {
-            ans += n / 2;
-            n = n / 2 + n % 2;
-        }
-        return ans;
+        return n-1;
     }
 };
+
+// Runtime: 3 ms, faster than 17.67% of C++ online submissions for Count of Matches in Tournament.
+// Memory Usage: 6.3 MB, less than 73.09% of C++ online submissions for Count of Matches in Tournament.
+    
+//     int numberOfMatches(int n) {
+//         int ans = 0;
+//         while(n > 1)
+//         {
+//             ans += n / 2;
+//             n = n / 2 + n % 2;
+//         }
+//         return ans;
+//     }
+// };
