151957f345a8a0b27708259de1228ca2bef7c941
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-06 22:48:07 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (6.05%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 3 ms, faster than 13.68% of C++ online submissions for Calculate Money in Leetcode Bank.
+// Memory Usage: 6.4 MB, less than 15.79% of C++ online submissions for Calculate Money in Leetcode Bank.
+    
     int totalMoney(int n) {
         int lastMonday = 0;
         int ans = 0, count = 0, money = 1;
