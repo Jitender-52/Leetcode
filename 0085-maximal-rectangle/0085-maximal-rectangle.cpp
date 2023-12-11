5f17ad058e2daa379bf55e52c68933e3ee8d8e42
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-11 17:37:02 +0530

    Time: 27 ms (92.29%), Space: 12.3 MB (97.19%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -2,35 +2,87 @@ class Solution {
 public:
     
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
-        for(int i = n-1; i >= 0; i--)
-        {
-            for(int j = m-1; j >= 0; j--)
-            {
-                if(matrix[i][j] == '1')
-                    dp[i][j] = dp[i][j+1] + 1;
-                else
-                    dp[i][j] = 0;
-            }
-        }
-        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> left(m, 0), right(m, m), height(m, 0);
         int ans = 0;
         for(int i = 0; i < n; i++)
         {
+            int l = 0, r = m;
             for(int j = 0; j < m; j++)
             {
-                int col = m;
-                for(int k = i; k < n; k++)
+                if(matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+            
+            for(int j = 0; j < m; j++)
+            {
+                if(matrix[i][j] == '1')
+                    left[j] = max(left[j], l);
+                else
                 {
-                    col = min(col, dp[k][j]);
-                    ans = max(ans, (k - i + 1) * col);
+                    left[j] = 0;
+                    l = j + 1;
                 }
             }
+            
+            for(int j = m-1; j >= 0; j--)
+            {
+                if(matrix[i][j] == '1')
+                    right[j] = min(right[j], r);
+                else
+                {
+                    right[j] = m;
+                    r = j;
+                }
+            }
+            
+            for(int j = 0; j < m; j++)
+                ans = max(ans, (right[j] - left[j]) * height[j]);
         }
         return ans;
     }
 };
+    
+    
+//     Time Complexity - O(MN * N); 
+//     Space Complexity - O(1);
+    
+//     Runtime: 96 ms, faster than 6.70% of C++ online submissions for Maximal Rectangle.
+// Memory Usage: 13.3 MB, less than 88.70% of C++ online submissions for Maximal Rectangle.
+    
+//     int maximalRectangle(vector<vector<char>>& matrix) {
+//         int n = matrix.size(), m = matrix[0].size();
+//         vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+//         for(int i = n-1; i >= 0; i--)
+//         {
+//             for(int j = m-1; j >= 0; j--)
+//             {
+//                 if(matrix[i][j] == '1')
+//                     dp[i][j] = dp[i][j+1] + 1;
+//                 else
+//                     dp[i][j] = 0;
+//             }
+//         }
+        
+//         int ans = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             for(int j = 0; j < m; j++)
+//             {
+//                 int col = m;
+//                 for(int k = i; k < n; k++)
+//                 {
+//                     col = min(col, dp[k][j]);
+//                     ans = max(ans, (k - i + 1) * col);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
     
 //     Time Complexity - O(MN * MN);
 //     Space Complexity - O(1);
