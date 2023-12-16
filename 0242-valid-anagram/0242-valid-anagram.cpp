beb3f3ffd927848bbd8071cb3e51d41fd92f4fea
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-16 21:28:52 +0530

    Time: 8 ms (62.96%), Space: 7.7 MB (45.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+    
     bool isAnagram(string s, string t) {
         vector<int> v(26, 0);
         for(int i = 0; i < s.length(); i++)
             v[s[i] - 'a']++;
         for(int i = 0; i < t.length(); i++)
+        {
             v[t[i] - 'a']--;
+            if(v[t[i] - 'a'] < 0)
+                return false;
+        }
         for(int i = 0; i < 26; i++)
         {
             if(v[i] != 0)
@@ -14,3 +19,21 @@ public:
         return true;
     }   
 };
+    
+//     Runtime: 3 ms, faster than 97.78% of C++ online submissions for Valid Anagram.
+// Memory Usage: 7.7 MB, less than 45.96% of C++ online submissions for Valid Anagram.
+    
+//     bool isAnagram(string s, string t) {
+//         vector<int> v(26, 0);
+//         for(int i = 0; i < s.length(); i++)
+//             v[s[i] - 'a']++;
+//         for(int i = 0; i < t.length(); i++)
+//             v[t[i] - 'a']--;
+//         for(int i = 0; i < 26; i++)
+//         {
+//             if(v[i] != 0)
+//                 return false;
+//         }
+//         return true;
+//     }   
+// };
