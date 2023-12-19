8a6306f75263c5b79c26cbe5beea49860e0e1b05
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-19 18:42:58 +0530

    Time: 45 ms (47.08%), Space: 22 MB (59.38%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int n = img.size();
+        int m = img[0].size();
+        vector<vector<int>> ans(n, vector<int> (m, 0));
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                int sum = 0, count = 0;
+                for(int k = -1; k <= 1; k++)
+                {
+                    for(int l = -1; l <= 1; l++)
+                    {
+                        int r = i + k, c = j + l;
+                        if(r >= 0 && r < n && c >= 0 && c < m)
+                        {
+                            sum += img[r][c];
+                            count++;
+                        }
+                    }
+                }
+                ans[i][j] = sum / count;
+            }
+        }
+        return ans;
+    }
+};
