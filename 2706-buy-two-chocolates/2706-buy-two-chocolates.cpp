1b64df666227df6fd810b55453a2f471be8e2f27
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-20 21:10:07 +0530

    Time: 11 ms (73.75%), Space: 47 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 19 ms, faster than 19.09% of C++ online submissions for Buy Two Chocolates.
+// Memory Usage: 46.9 MB, less than 58.30% of C++ online submissions for Buy Two Chocolates.
+    
     int buyChoco(vector<int>& prices, int money) {
         sort(prices.begin(), prices.end());
         int ans = money - (prices[1] + prices[0]);
