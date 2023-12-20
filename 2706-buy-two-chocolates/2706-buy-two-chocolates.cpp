34c32e1f364ab718da60e64161809db94eee6cb6
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-20 16:35:12 +0530

    Time: 19 ms (19.09%), Space: 46.9 MB (58.30%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int ans = money - (prices[1] + prices[0]);
+        if(ans < 0)
+            return money;
+        return ans;
+    }
+};
