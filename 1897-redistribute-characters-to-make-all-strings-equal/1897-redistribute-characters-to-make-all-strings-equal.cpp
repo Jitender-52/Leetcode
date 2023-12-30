40d384686214538026a2441ee876a30bbc4699e2
Jitender <97593224+Jitender-52@users.noreply.github.com>
2023-12-30 20:17:10 +0530

    Time: 8 ms (90.66%), Space: 12.5 MB (44.75%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
+    
     bool makeEqual(vector<string>& words) {
         int n = words.size();
-        map<char, int> mp;
-        for(auto s : words)
+        vector<int> v(26, 0);
+        for(auto i : words)
         {
-            for(auto c : s)
-                mp[c]++;
+            for(auto j : i)
+                v[j-'a']++;
         }
-        for(auto i : mp)
+        for(int i = 0; i < 26; i++)
         {
-            if(i.second % n)
+            if(v[i] % n)
                 return false;
         }
         return true;
     }
 };
+    
+//     Runtime: 58 ms, faster than 5.06% of C++ online submissions for Redistribute Characters to Make All Strings Equal.
+// Memory Usage: 12.7 MB, less than 10.12% of C++ online submissions for Redistribute Characters to Make All Strings Equal.
+    
+//     bool makeEqual(vector<string>& words) {
+//         int n = words.size();
+//         map<char, int> mp;
+//         for(auto s : words)
+//         {
+//             for(auto c : s)
+//                 mp[c]++;
+//         }
+//         for(auto i : mp)
+//         {
+//             if(i.second % n)
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
