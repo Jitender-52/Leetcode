5973946be5ece92a8b4a837b971addc07c80a545
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-01-02 19:02:12 +0530

    Time: 12 ms (56.87%), Space: 29.6 MB (97.69%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
+    
     vector<vector<int>> findMatrix(vector<int>& nums) {
         int n = nums.size();
-        map<int,int> mp;
-        int maxi = 0;
-        for(int i = 0; i < n; i++)
-        {
-            mp[nums[i]]++;
-            maxi = max(maxi, mp[nums[i]]);
-        }
+        vector<int> v(n+1, 0);
+        for(auto i : nums)
+            v[i]++;
+        int maxi = *max_element(v.begin(), v.end());
         vector<vector<int>> ans(maxi);
-        for(auto i : mp)
+        for(int i = 0; i <= n; i++)
         {
-            for(int j = 0; j < i.second; j++)
-                ans[j].push_back(i.first);
+            for(int j = 0; j < v[i]; j++)
+                ans[j].push_back(i);
         }
         return ans;
     }
 };
+    
+//     Runtime: 27 ms, faster than 5.95% of C++ online submissions for Convert an Array Into a 2D Array With Conditions.
+// Memory Usage: 29.9 MB, less than 70.96% of C++ online submissions for Convert an Array Into a 2D Array With Conditions.
+    
+//     vector<vector<int>> findMatrix(vector<int>& nums) {
+//         int n = nums.size();
+//         map<int,int> mp;
+//         int maxi = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             mp[nums[i]]++;
+//             maxi = max(maxi, mp[nums[i]]);
+//         }
+//         vector<vector<int>> ans(maxi);
+//         for(auto i : mp)
+//         {
+//             for(int j = 0; j < i.second; j++)
+//                 ans[j].push_back(i.first);
+//         }
+//         return ans;
+//     }
+// };
