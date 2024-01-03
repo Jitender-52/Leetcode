460bee0839768ec5025be44bc352cc8f3be8d4ea
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-01-03 16:41:58 +0530

    Time: 89 ms (61.51%), Space: 29 MB (34.88%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
+    
     int numberOfBeams(vector<string>& bank) {
-        int n = bank.size();
         int ans = 0;
         int last = 0;
-        for(int i = 0; i < bank[0].size(); i++)
-            last += bank[0][i] == '1';
-        for(int i = 1; i < n; i++)
+        for(auto i : bank)
         {
-            auto s = bank[i];
             int count = 0;
-            for(auto j : s)
+            for(auto j : i)
                 count += j == '1';
             if(count == 0)
                 continue;
@@ -20,3 +17,27 @@ public:
         return ans;
     }
 };
+    
+//     Runtime: 89 ms, faster than 61.51% of C++ online submissions for Number of Laser Beams in a Bank.
+// Memory Usage: 29.2 MB, less than 29.62% of C++ online submissions for Number of Laser Beams in a Bank.
+    
+//     int numberOfBeams(vector<string>& bank) {
+//         int n = bank.size();
+//         int ans = 0;
+//         int last = 0;
+//         for(int i = 0; i < bank[0].size(); i++)
+//             last += bank[0][i] == '1';
+//         for(int i = 1; i < n; i++)
+//         {
+//             auto s = bank[i];
+//             int count = 0;
+//             for(auto j : s)
+//                 count += j == '1';
+//             if(count == 0)
+//                 continue;
+//             ans += count * last;
+//             last = count;
+//         }
+//         return ans;
+//     }
+// };
