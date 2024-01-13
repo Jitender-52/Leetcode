48b2573177f400187bb42c815d37a669de249851
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-01-13 16:58:01 +0530

    Time: 42 ms (94.43%), Space: 16.8 MB (97.67%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 39 ms, faster than 96.96% of C++ online submissions for Minimum Number of Steps to Make Two Strings Anagram.
+// Memory Usage: 16.9 MB, less than 82.29% of C++ online submissions for Minimum Number of Steps to Make Two Strings Anagram.
+    
     int minSteps(string s, string t) {
         int ans = 0;
         vector<int> v(26);
