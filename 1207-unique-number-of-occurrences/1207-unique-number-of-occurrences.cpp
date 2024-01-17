4765882925192e808f6838804da32605e47248fd
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-01-17 14:27:36 +0530

    Time: 2 ms (65.84%), Space: 8.8 MB (20.45%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int> mp;
+        for(auto i : arr)
+            mp[i]++;
+        set<int> st;
+        for(auto i : mp)
+        {
+            if(st.find(i.second) != st.end())
+                return false;
+            st.insert(i.second);
+        }
+        return true;
+    }
+};
