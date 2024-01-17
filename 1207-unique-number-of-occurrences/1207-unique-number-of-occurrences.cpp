873c9c9379509f2c5c79768c2a698be440f115bf
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-01-17 22:30:21 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (9.43%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 2 ms, faster than 65.84% of C++ online submissions for Unique Number of Occurrences.
+// Memory Usage: 8.8 MB, less than 20.45% of C++ online submissions for Unique Number of Occurrences.
+    
     bool uniqueOccurrences(vector<int>& arr) {
         map<int,int> mp;
         for(auto i : arr)
