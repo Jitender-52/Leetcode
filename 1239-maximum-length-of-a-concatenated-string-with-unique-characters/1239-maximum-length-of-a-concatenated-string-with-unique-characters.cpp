517bfc7f7ff62955c03c17c8d4953be4b8b7d5cd
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-01-23 23:22:03 +0530

    Time: 93 ms (46.12%), Space: 94.6 MB (24.62%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,6 +1,46 @@
 class Solution {
 public:
     
+    // Why its not working??
+    
+//     int ans = 0;
+    
+//     void maximum(int i, string &s, vector<string> &arr, vector<int> &v)
+//     {
+//         int n = arr.size();
+//         if(i >= n)
+//             return;
+//         string x = s + arr[i];
+//         maximum(i+1, s, arr, v);
+//         for(auto j : arr[i])
+//         {
+//             if(v[j - 'a'] > 0)
+//                 return;
+//         }
+//         for(auto j : arr[i])
+//             v[j - 'a']++;
+//         ans = max(ans, (int)s.length());
+//         maximum(i+1, x, arr, v);
+//         for(auto j : arr[i])
+//             v[j - 'a']--;
+//     }
+    
+//     int maxLength(vector<string>& arr) {
+//         string s = "";
+//         vector<int> v(26, 0);
+//         maximum(0, s, arr, v);
+//         return ans;
+//     }
+// };
+    
+    
+//     Runtime: 57 ms, faster than 53.46% of C++ online submissions for Maximum Length of a Concatenated String with Unique Characters.
+// Memory Usage: 94.7 MB, less than 24.62% of C++ online submissions for Maximum Length of a Concatenated String with Unique Characters.
+    
+// Time Complexity - (2 ^ n) * m,  where n = arr.size() and m = arr[i].length();
+//     Inside every recursive call there is time complexity of s.size();
+// Auxillary Space - 2 ^ n
+    
     int ans = 0;
     
     void maximum(int i, string &s, vector<string> &arr)
