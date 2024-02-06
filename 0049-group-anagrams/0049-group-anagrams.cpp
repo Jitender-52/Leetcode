803c9cbb384fe1c461119cb2cd5e7dbf60f748f2
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-02-06 22:26:10 +0530

    Time: 21 ms (93.86%), Space: 22 MB (42.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 25 ms, faster than 82.98% of C++ online submissions for Group Anagrams.
+// Memory Usage: 22.1 MB, less than 41.23% of C++ online submissions for Group Anagrams.
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         unordered_map<string, int> mp;
