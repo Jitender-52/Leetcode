f95a446995a84f4702dc66295eed7e0d62b3ece2
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-02-10 21:36:06 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (74.15%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    
+//     Runtime: 4 ms, faster than 76.20% of C++ online submissions for Palindromic Substrings.
+// Memory Usage: 7.3 MB, less than 78.14% of C++ online submissions for Palindromic Substrings.
+    
     int countSubstrings(string s) {
         int n = s.length();
         int ans = 0;
         for(int i = 0; i < n; i++)
         {
             int j = i-1, k = i;
-            ans++; // for single character
-            while(j >= 0 && k < n && s[j] == s[k]) // for even length substrings
+            ans++;
+            while(j >= 0 && k < n && s[j] == s[k])
             {
                 j--;
                 k++;
                 ans++;
             }
             j = i-1, k = i+1;
-            while(j >= 0 && k < n && s[j] == s[k]) // for odd length substrings
+            while(j >= 0 && k < n && s[j] == s[k])
             {
                 j--;
                 k++;
