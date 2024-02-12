0f2a26f46021aab18a729c618b6027ce16fd441b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-02-12 22:43:13 +0530

    Time: 14 ms (60.88%), Space: 22.1 MB (18.20%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 17 ms, faster than 35.13% of C++ online submissions for Majority Element.
+// Memory Usage: 22.2 MB, less than 18.20% of C++ online submissions for Majority Element.
+    
     int majorityElement(vector<int>& nums) {
         map<int, int> mp;
     for (int i = 0; i < nums.size(); i++)
