d0bd9223b3a479cbe273d9a9afc2ec2af37aac84
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-02-16 16:02:48 +0530

    Time: 184 ms (40.44%), Space: 67.6 MB (40.09%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
+    
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         map<int,int> mp;
         for(auto i : arr)
             mp[i]++;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        priority_queue<int, vector<int>, greater<int>> pq;
         for(auto i : mp)
-        {
-            pq.push({i.second, i.first});
-        }
+            pq.push(i.second);
         while(!pq.empty() && k--)
         {
-            int a = pq.top()[0];
-            int b = pq.top()[1];
+            int a = pq.top()-1;
             pq.pop();
-            a--;
             if(a != 0)
-                pq.push({a, b});
+                pq.push(a);
         }
         return pq.size();        
     }
 };
+    
+//     Runtime: 285 ms, faster than 9.50% of C++ online submissions for Least Number of Unique Integers after K Removals.
+// Memory Usage: 91.2 MB, less than 5.91% of C++ online submissions for Least Number of Unique Integers after K Removals.
+    
+//     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+//         map<int,int> mp;
+//         for(auto i : arr)
+//             mp[i]++;
+//         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+//         for(auto i : mp)
+//         {
+//             pq.push({i.second, i.first});
+//         }
+//         while(!pq.empty() && k--)
+//         {
+//             int a = pq.top()[0];
+//             int b = pq.top()[1];
+//             pq.pop();
+//             a--;
+//             if(a != 0)
+//                 pq.push({a, b});
+//         }
+//         return pq.size();        
+//     }
+// };
