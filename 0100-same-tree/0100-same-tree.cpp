b644db4352099c26485ccc6146cd511cf0fb5c14
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-02-26 14:11:46 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (25.40%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,18 +12,11 @@
 class Solution {
 public:
     
-//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Same Tree.
-// Memory Usage: 10.2 MB, less than 8.85% of C++ online submissions for Same Tree.
-    
-    
-    bool isSameTree(TreeNode* root1, TreeNode* root2) {
-     if (!root1 && !root2)
-        return true;
-    if (!root1 || !root2 || root1->val != root2->val)
-        return false;
-
-    if(isSameTree(root1->left, root2->left) && isSameTree(root1->right, root2->right))
-        return true;
-    return false;   
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+       if(!p && !q) 
+           return true;
+        if(!p || !q || (p->val != q->val))
+            return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
