6f708a5f593d7192524073239aadf7c7bd13463a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-03-03 11:54:14 +0530

    Time: 427 ms (47.08%), Space: 268.4 MB (6.59%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,33 +1,87 @@
+
+// Solving by understanding trie from discussion part
+
+class TrieNode{
+  public:
+    unordered_map<int, TrieNode*> child;
+};
+
+TrieNode* insert(TrieNode *root, string &s)
+{
+    TrieNode *curr = new TrieNode();
+    curr = root;
+    for(auto i : s)
+    {
+        if(curr->child[i] == NULL)  // curr->child is a map
+        {
+            curr->child[i] = new TrieNode();
+        }
+        curr = curr->child[i];
+    }
+    return root;
+}
+
 class Solution {
 public:
     
+    
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        set<string> st;
+        TrieNode *root = new TrieNode();
         for(auto i : arr1)
         {
             string s = to_string(i);
-            string x = "";
-            for(auto j : s)
-            {
-                x += j;
-                st.insert(x);
-            }
+            root = insert(root, s);
         }
+        
         int ans = 0;
-        for(auto i : arr2)
+        for(auto j : arr2)
         {
-            string s = to_string(i);
-            string x = "";
-            for(auto j : s)
+            string s = to_string(j);
+            TrieNode *curr = root;
+            int count = 0;
+            for(auto i : s)
             {
-                x += j;
-                if(st.find(x) != st.end())
-                    ans = max(ans, (int)x.length());
+                if(curr->child[i] == NULL)
+                    break;
+                curr = curr->child[i];
+                count++;
             }
+            ans = max(ans, count);
         }
         return ans;
     }
 };
+    
+//     Runtime: 1290 ms, faster than 10.28% of C++ online submissions for Find the Length of the Longest Common Prefix.
+// Memory Usage: 155.5 MB, less than 62.00% of C++ online submissions for Find the Length of the Longest Common Prefix.
+    
+//     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+//         set<string> st;
+//         for(auto i : arr1)
+//         {
+//             string s = to_string(i);
+//             string x = "";
+//             for(auto j : s)
+//             {
+//                 x += j;
+//                 st.insert(x);
+//             }
+//         }
+//         int ans = 0;
+//         for(auto i : arr2)
+//         {
+//             string s = to_string(i);
+//             string x = "";
+//             for(auto j : s)
+//             {
+//                 x += j;
+//                 if(st.find(x) != st.end())
+//                     ans = max(ans, (int)x.length());
+//             }
+//         }
+//         return ans;
+//     }
+// };
     
 //     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
 //         int ans = 0;
