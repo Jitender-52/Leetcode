f008e296315b7fcd4cfb60775ea171d7b6281d1b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-03-18 23:14:48 +0530

    Time: 12 ms (80.00%), Space: 11.2 MB (60.00%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
+    
     long long countSubstrings(string s, char c) {
-        map<char,int> mp;
+        int count = 0;
         for(auto i : s)
-            mp[i]++;
-        return (mp[c] * 1LL * (mp[c]+1)) / 2;
+            count += i == c;
+        return (count * 1LL * (count+1)) / 2;
     }
 };
+    
+//     Runtime: 28 ms, faster than 20.00% of C++ online submissions for Count Substrings Starting and Ending with Given Character.
+// Memory Usage: 12.8 MB, less than 20.00% of C++ online submissions for Count Substrings Starting and Ending with Given Character.
+    
+//     long long countSubstrings(string s, char c) {
+//         map<char,int> mp;
+//         for(auto i : s)
+//             mp[i]++;
+//         return (mp[c] * 1LL * (mp[c]+1)) / 2;
+//     }
+// };
