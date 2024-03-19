adb714a049507582f44bf548b9d507b9b878ca69
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-03-19 22:05:29 +0530

    Time: 17 ms (44.30%), Space: 18.9 MB (96.34%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -11,15 +11,11 @@
  */
 class Solution {
 public:
+    
     TreeNode* recoverFromPreorder(string traversal) {
         int n = traversal.size();
-        stack<TreeNode*> st;
-        map<TreeNode*, int> mp;
-        
+        map<int, TreeNode*> mp;
         int i = 0;
-        TreeNode *root = new TreeNode(-1);
-        mp[root] = -1;
-        st.push(root);
         while(i < n)
         {
             int count = 0;
@@ -35,39 +31,87 @@ public:
                 i++;
             }
             int x = stoi(s);
-            // cout << x << " " << count << endl;
-            stack<TreeNode*> st1;
-            while(!st.empty())
+            TreeNode *root = new TreeNode(x);
+            if(count > 0)
             {
-                auto it = st.top();
-                st.pop();
-                // cout << it->val << endl;
-                if(mp[it] == count-1)
-                {
-                    st.push(it);
-                    if(!it->left)
-                    {
-                        it->left = new TreeNode(x);
-                        st.push(it->left);
-                        mp[it->left] = count;
-                    }
-                    else
-                    {
-                        it->right = new TreeNode(x);
-                        st.push(it->right);
-                        mp[it->right] = count;
-                    }
-                    break;
-                }
-                // st1.push(it);
+                if(!mp[count-1]->left)
+                    mp[count-1]->left = root;
+                else
+                    mp[count-1]->right = root;
             }
-            // while(!st1.empty())
-            // {
-            //     cout << st1.top()->val << endl;
-            //     st.push(st1.top());
-            //     st1.pop();
-            // }
+            mp[count] = root;
         }
-        return root->left;
+        return mp[0]; // i.e. root of the tree at zeroth level
     }
 };
+
+    
+
+// God gifted solution to me
+
+// Time complexity = O(N)
+// Space Complexity = O(N)
+    
+//     Runtime: 32 ms, faster than 10.54% of C++ online submissions for Recover a Tree From Preorder Traversal.
+// Memory Usage: 29.9 MB, less than 9.89% of C++ online submissions for Recover a Tree From Preorder Traversal.
+    
+//     TreeNode* recoverFromPreorder(string traversal) {
+//         int n = traversal.size();
+//         stack<TreeNode*> st;
+//         map<TreeNode*, int> mp;
+        
+//         int i = 0;
+//         TreeNode *root = new TreeNode(-1);
+//         mp[root] = -1;
+//         st.push(root);
+//         while(i < n)
+//         {
+//             int count = 0;
+//             while(i < n && traversal[i] == '-')
+//             {
+//                 count++;
+//                 i++;
+//             }
+//             string s = "";
+//             while(i < n && traversal[i] >= '0' && traversal[i] <= '9')
+//             {
+//                 s += traversal[i];
+//                 i++;
+//             }
+//             int x = stoi(s);
+//             // cout << x << " " << count << endl;
+//             stack<TreeNode*> st1;
+//             while(!st.empty())
+//             {
+//                 auto it = st.top();
+//                 st.pop();
+//                 // cout << it->val << endl;
+//                 if(mp[it] == count-1)
+//                 {
+//                     st.push(it);
+//                     if(!it->left)
+//                     {
+//                         it->left = new TreeNode(x);
+//                         st.push(it->left);
+//                         mp[it->left] = count;
+//                     }
+//                     else
+//                     {
+//                         it->right = new TreeNode(x);
+//                         st.push(it->right);
+//                         mp[it->right] = count;
+//                     }
+//                     break;
+//                 }
+//                 // st1.push(it);
+//             }
+//             // while(!st1.empty())
+//             // {
+//             //     cout << st1.top()->val << endl;
+//             //     st.push(st1.top());
+//             //     st1.pop();
+//             // }
+//         }
+//         return root->left;
+//     }
+// };
