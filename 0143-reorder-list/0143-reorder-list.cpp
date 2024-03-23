ac895088a63f6d31978ca3d3425c6bb50e8c991f
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-03-23 06:15:47 +0530

    Time: 26 ms (46.48%), Space: 21.8 MB (32.40%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,6 +10,10 @@
  */
 class Solution {
 public:
+    
+//     Runtime: 22 ms, faster than 86.75% of C++ online submissions for Reorder List.
+// Memory Usage: 18.7 MB, less than 29.66% of C++ online submissions for Reorder List.
+    
     void reorderList(ListNode* head) {
         vector<int> v;
         ListNode *temp = head;
