bd36f85b95973e66950c83bcac3ec0dedfae7f39
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-03-25 21:08:44 +0530

    Time: 50 ms (51.42%), Space: 35.8 MB (90.94%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    
     vector<int> findDuplicates(vector<int>& nums) {
-        map<int, int> mp;
-        vector<int> v;
-        for(int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 0; i < n; i++)
         {
-            mp[nums[i]]++;
-            if(mp[nums[i]] == 2)
-            {
-                v.push_back(nums[i]);
-            }
+            int x = abs(nums[i]);
+            int idx = x-1;
+            if(nums[idx] < 0)
+                ans.push_back(x);
+            nums[idx] *= -1;
         }
-        
-        return v;
+        return ans;
     }
-};
+};  
+    
+//     Runtime: 137 ms, faster than 5.17% of C++ online submissions for Find All Duplicates in an Array.
+// Memory Usage: 49.1 MB, less than 8.67% of C++ online submissions for Find All Duplicates in an Array.
+
+    
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         map<int, int> mp;
+//         vector<int> v;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             mp[nums[i]]++;
+//             if(mp[nums[i]] == 2)
+//             {
+//                 v.push_back(nums[i]);
+//             }
+//         }
+        
+//         return v;
+//     }
+// };
