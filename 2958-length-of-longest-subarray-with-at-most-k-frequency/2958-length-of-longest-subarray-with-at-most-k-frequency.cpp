ef3d6b08f7c32060690d0218002ce4ed990be6f1
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-03-28 09:33:32 +0530

    Time: 349 ms (21.07%), Space: 148.6 MB (33.73%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
+    
     int maxSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size();
         int i = 0, j = 0;
-        int check = 0;
         map<int,int> mp;
         int ans = 0;
         while(j < n && i < n)
         {
             mp[nums[i]]++;
-            if(mp[nums[i]] > k)
-                check++;
-            while(check > 0 && j <= i)
+            while(mp[nums[i]] > k)
             {
-                if(mp[nums[j]] == k+1)
-                    check--;
                 mp[nums[j]]--;
                 j++;
             }
-            // cout << i << " " << j << endl;
             ans = max(ans, i - j + 1);
             i++;
         }
-        // cout << endl;
         return ans;
     }
 };
+    
+    
+//     Runtime: 379 ms, faster than 8.41% of C++ online submissions for Length of Longest Subarray With at Most K Frequency.
+// Memory Usage: 148.5 MB, less than 33.73% of C++ online submissions for Length of Longest Subarray With at Most K Frequency.
+    
+//     int maxSubarrayLength(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int i = 0, j = 0;
+//         int check = 0;
+//         map<int,int> mp;
+//         int ans = 0;
+//         while(j < n && i < n)
+//         {
+//             mp[nums[i]]++;
+//             if(mp[nums[i]] > k)
+//                 check++;
+//             while(check > 0 && j <= i)
+//             {
+//                 if(mp[nums[j]] == k+1)
+//                     check--;
+//                 mp[nums[j]]--;
+//                 j++;
+//             }
+//             // cout << i << " " << j << endl;
+//             ans = max(ans, i - j + 1);
+//             i++;
+//         }
+//         // cout << endl;
+//         return ans;
+//     }
+// };
