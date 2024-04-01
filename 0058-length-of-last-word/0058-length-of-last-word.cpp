35ffd9eb11a02f9fc01ebd5a5678f3839432eaf6
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-04-01 21:52:18 +0530

    Time: 4 ms (47.87%), Space: 7.7 MB (57.50%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 2 ms, faster than 54.22% of C++ online submissions for Length of Last Word.
+// Memory Usage: 7.7 MB, less than 20.26% of C++ online submissions for Length of Last Word.
+    
     int lengthOfLastWord(string s) {
         int n = s.length();
         int i = n-1;
