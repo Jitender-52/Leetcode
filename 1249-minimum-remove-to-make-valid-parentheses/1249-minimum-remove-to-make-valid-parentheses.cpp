6a1aa97ae0ee8c50a312cdf0e3e4925334166eca
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-04-06 22:51:25 +0530

    Time: 17 ms (84.34%), Space: 12.2 MB (74.21%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,32 +1,71 @@
 class Solution {
 public:
+    
+    
     string minRemoveToMakeValid(string s) {
-        int openParenthesesCount = 0;
-        vector<char> arr(s.begin(), s.end()); 
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] == '(')
-                openParenthesesCount++;
-            else if (arr[i] == ')') {
-                if (openParenthesesCount == 0)
-                    arr[i] = '*'; 
+        int n = s.length();
+        int count = 0;
+        string ans = "";
+        for(int i = 0; i < n; i++)
+        {
+            if(s[i] == '(')
+                count++;
+            else if(s[i] == ')')
+            {
+                if(count == 0)
+                    s[i] = '*';
                 else
-                    openParenthesesCount--;
+                    count--;
             }
         }
-
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            if (openParenthesesCount > 0 && arr[i] == '(') {
-                arr[i] = '*'; 
-                openParenthesesCount--;
+        for(int i = n-1; i >= 0 && count > 0; i--)
+        {
+            if(s[i] == '(')
+            {
+                s[i] = '*';
+                count--;
             }
         }
-        
-        string result = "";
-        for (char c : arr) {
-            if (c != '*')
-                result += c;
+        for(auto &i : s)
+        {
+            if(i != '*')
+                ans += i;
         }
-
-        return result;
+        return ans;
     }
 };
+    
+    
+//     Runtime: 14 ms, faster than 95.19% of C++ online submissions for Minimum Remove to Make Valid Parentheses.
+// Memory Usage: 12.8 MB, less than 56.98% of C++ online submissions for Minimum Remove to Make Valid Parentheses.
+    
+//     string minRemoveToMakeValid(string s) {
+//         int openParenthesesCount = 0;
+//         vector<char> arr(s.begin(), s.end()); 
+//         for (int i = 0; i < arr.size(); i++) {
+//             if (arr[i] == '(')
+//                 openParenthesesCount++;
+//             else if (arr[i] == ')') {
+//                 if (openParenthesesCount == 0)
+//                     arr[i] = '*'; 
+//                 else
+//                     openParenthesesCount--;
+//             }
+//         }
+
+//         for (int i = arr.size() - 1; i >= 0; i--) {
+//             if (openParenthesesCount > 0 && arr[i] == '(') {
+//                 arr[i] = '*'; 
+//                 openParenthesesCount--;
+//             }
+//         }
+        
+//         string result = "";
+//         for (char c : arr) {
+//             if (c != '*')
+//                 result += c;
+//         }
+
+//         return result;
+//     }
+// };
