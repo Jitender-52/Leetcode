3e1a848a89f668a385633f1a1e0d784ef19755e8
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-04-09 23:01:42 +0530

    Time: 4 ms (41.19%), Space: 9.3 MB (67.56%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int n = tickets.size();
         int ans = (k + 1) * tickets[k] + (n-1-k) * (tickets[k] - 1);
@@ -16,3 +17,23 @@ public:
         return ans;
     }
 };
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Time Needed to Buy Tickets.
+// Memory Usage: 9.3 MB, less than 91.31% of C++ online submissions for Time Needed to Buy Tickets.
+    
+//     int timeRequiredToBuy(vector<int>& tickets, int k) {
+//         int n = tickets.size();
+//         int ans = (k + 1) * tickets[k] + (n-1-k) * (tickets[k] - 1);
+//         for(int i = 0; i <= k; i++)
+//         {
+//             if(tickets[i] < tickets[k])
+//                 ans -= tickets[k] - tickets[i];
+//         }
+//         for(int i = k; i < n; i++)
+//         {
+//             if(tickets[i] < tickets[k])
+//                 ans -= tickets[k] - tickets[i] - 1;
+//         }
+//         return ans;
+//     }
+// };
