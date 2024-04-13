fe1b4d78d0b64346bf69899edb5694ffee325ac8
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-04-13 22:01:00 +0530

    Time: 146 ms (100.00%), Space: 110.8 MB (100.00%) - LeetHub

---
+++ 3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum/3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    
+    long long numberOfSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        stack<pair<int,int>> st;
+        long long ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.empty() && nums[i] > st.top().first)
+                st.pop();
+            if(!st.empty() && st.top().first == nums[i])
+                st.top().second++;
+            else
+                st.push({nums[i], 1});
+            ans += st.top().second;
+        }
+        return ans;
+    }
+};
+    
+    
+//     long long numberOfSubarrays(vector<int>& nums) {
+//         int n = nums.size();
+//         long long ans = 0;
+//         stack<int> st;
+        
+//         vector<int> v(n);
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(!st.empty())
+//             {
+//                 while(!st.empty() && nums[st.top()] <= nums[i])
+//                     st.pop();
+//                 if(st.empty())
+//                     v[i] = -1;
+//                 else
+//                     v[i] = st.top();
+//             }
+//             st.push(i);
+//         }
+        
+//         unordered_map<int, vector<int>> mp;
+//         for (int i = 0; i < n; i++)
+//         {
+//             mp[nums[i]].push_back(i);
+//         }
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             int ub = upper_bound(mp[nums[i]].begin(), mp[nums[i]].end(), i) - mp[nums[i]].begin();
+//             int lb = lower_bound(mp[nums[i]].begin(), mp[nums[i]].end(), v[i]) - mp[nums[i]].begin();
+//             ans += (ub - lb);
+//         }
+
+        
+//         // for(int i = 0; i < n; i++)
+//         // {
+//         //     if(st.empty())
+//         //     {
+//         //         st.push(nums[i]);
+//         //         continue;
+//         //     }
+//         //     int maxi = 0;
+//         //     map<int,int> mp;
+//         //     while(!st.empty() && st.top() < nums[i])
+//         //     {
+//         //         if(maxi == -1)
+//         //         {
+//         //             maxi = st.top();
+//         //             st.pop();
+//         //             mp.clear();
+//         //             mp[maxi]++;
+//         //             continue;
+//         //         }
+//         //         if(st.top() > maxi)
+//         //         {
+//         //             ans += (mp[maxi] * 1LL * (mp[maxi] + 1)) / 2;
+//         //             maxi = nums[i];
+//         //             mp.clear();
+//         //             st.pop();
+//         //         }
+//         //         else
+//         //         {
+//         //             mp[st.top()]++;
+//         //             st.pop();
+//         //         }
+//         //     }
+//         //     st.push(nums[i]);
+//         // }
+//         return ans;
+//     }
+// };
