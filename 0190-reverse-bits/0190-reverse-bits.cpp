22cd5819ec3b22f83eaf6f8d2f38af8348bd9afe
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-04-13 12:06:17 +0530

    Time: 5 ms (16.98%), Space: 7.2 MB (28.71%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Reverse Bits.
+// Memory Usage: 7.2 MB, less than 7.24% of C++ online submissions for Reverse Bits.
+
     uint32_t reverseBits(uint32_t n) {
         long long ans = 0;
         for(int i = 0; i < 32; i++)
