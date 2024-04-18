55b7c802f7d0140c1985f017e1a764a7fceff59b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-04-18 21:40:29 +0530

    Time: 96 ms (13.20%), Space: 101.1 MB (32.36%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> dx = {1, -1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
+        int ans = 0;
+        
+        auto isValid = [&](int x, int y)
+        {
+            return x >= 0 && x < n && y >= 0 && y < m;  
+        };
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    for(int k = 0; k < 4; k++)
+                    {
+                        int x = i + dx[k];
+                        int y = j + dy[k];
+                        if(!isValid(x, y) || grid[x][y] == 0)
+                            ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
