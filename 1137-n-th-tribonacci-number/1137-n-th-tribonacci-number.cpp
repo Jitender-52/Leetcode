4b09bc8fecb98aad14b4c82289f9d9b7c73e4a34
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-04-24 21:29:20 +0530

    Time: 2 ms (50.95%), Space: 6.8 MB (99.95%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for N-th Tribonacci Number.
+// Memory Usage: 7.1 MB, less than 53.59% of C++ online submissions for N-th Tribonacci Number.
+    
     int tribonacci(int n) {
         long long a = 0, b = 1, c = 1;
         
