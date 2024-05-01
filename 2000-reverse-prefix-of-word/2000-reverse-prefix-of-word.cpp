ae47b1b8f1ba7dc99c2df578922a40dd6029f9fc
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-05-01 22:05:16 +0530

    Time: 3 ms (42.21%), Space: 7.6 MB (26.16%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Reverse Prefix of Word.
+// Memory Usage: 7.5 MB, less than 26.16% of C++ online submissions for Reverse Prefix of Word.
+    
     string reversePrefix(string word, char ch) {
         int n = word.size();
         string ans = "";
