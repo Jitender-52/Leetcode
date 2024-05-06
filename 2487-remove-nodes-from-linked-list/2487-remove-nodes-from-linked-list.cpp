e886a36eb4cd0930a76524e491cce4d95e7af05b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-05-06 15:37:06 +0530

    Time: 254 ms (86.97%), Space: 185.1 MB (5.47%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+     
+    void insert(ListNode** root, int item)
+    {
+        ListNode *ptr, *temp;
+        temp = new ListNode;
+        temp->val = item;
+        temp->next = NULL;
+
+        if (*root == NULL)
+            *root = temp;
+        else {
+            ptr = *root;
+            while (ptr->next != NULL)
+                ptr = ptr->next;
+
+            ptr->next = temp;
+        }
+    }
+    
+    ListNode* removeNodes(ListNode* head) {
+        vector<int> v;
+        
+        ListNode *temp = head;
+        while(temp)
+        {
+            v.push_back(temp->val);
+            temp= temp->next;
+        }
+        
+        int maxi = 0;
+        vector<int> v1;
+        for(int i = v.size()-1; i >= 0; i--)
+        {
+            if(v[i] >= maxi)
+            {
+                v1.push_back(v[i]);
+                maxi = v[i];
+            }
+        }
+        
+        reverse(v1.begin(), v1.end());
+        
+        ListNode *t = new ListNode(-1);
+        ListNode *t1 = t;
+        
+        for(int i = 0; i < v1.size(); i++)
+        {
+                ListNode *help = new ListNode(v1[i]);
+                t->next = help;
+                t = t->next;
+        }
+        return t1->next;
+    }
+};
