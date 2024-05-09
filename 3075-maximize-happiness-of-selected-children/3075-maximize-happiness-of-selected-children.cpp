a3ebbf0b29689041814e3f94aeb6226f6d83df41
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-05-09 20:57:21 +0530

    Time: 218 ms (17.99%), Space: 108.8 MB (16.00%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 211 ms, faster than 20.94% of C++ online submissions for Maximize Happiness of Selected Children.
+// Memory Usage: 108.6 MB, less than 29.47% of C++ online submissions for Maximize Happiness of Selected Children.
+
+    
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         long long ans = 0;
         sort(happiness.rbegin(), happiness.rend());
