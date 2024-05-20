68ad6ae1e1e980c0c2e13b76a1871ee6927a6c5e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-05-20 21:27:21 +0530

    Time: 3 ms (68.97%), Space: 8.3 MB (65.34%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 3 ms, faster than 68.97% of C++ online submissions for Sum of All Subset XOR Totals.
+// Memory Usage: 8.3 MB, less than 88.36% of C++ online submissions for Sum of All Subset XOR Totals.
+    
     int subsetXORSum(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
