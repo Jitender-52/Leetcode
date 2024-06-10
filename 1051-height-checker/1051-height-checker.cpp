9447f72f2a4ce756ee91a92b9f4017ec1269e321
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-06-10 20:56:39 +0530

    Time: 2 ms (48.33%), Space: 10.1 MB (44.04%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Height Checker.
+// Memory Usage: 10.2 MB, less than 10.73% of C++ online submissions for Height Checker.
+    
     int heightChecker(vector<int>& heights) {
         int n = heights.size();
         vector<int> v = heights;
