6aad95aae1c89c477cd3beac925638cd96cab8e6
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-06-14 21:35:46 +0530

    Time: 117 ms (68.70%), Space: 69 MB (79.13%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 117 ms, faster than 68.70% of C++ online submissions for Minimum Increment to Make Array Unique.
+// Memory Usage: 69.1 MB, less than 50.39% of C++ online submissions for Minimum Increment to Make Array Unique.
+
+    
     int minIncrementForUnique(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
