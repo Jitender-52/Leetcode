1263cc9b438f60c5a3910286ea723b4dc376a16d
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-06-15 21:20:33 +0530

    Time: 9 ms (37.72%), Space: 26.7 MB (62.12%) - LeetHub

---
+++ 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int duplicateNumbersXOR(vector<int>& nums) {
+        map<int,int> mp;
+        for(auto i : nums)
+            mp[i]++;
+        int ans = 0;
+        for(auto i : mp)
+        {
+            if(i.second > 1)
+                ans ^= i.first;
+        }
+        return ans;
+    }
+};
