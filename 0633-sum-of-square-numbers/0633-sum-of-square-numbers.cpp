cdf47f34acc908a585d7ae23e0d845d414c533dc
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-06-17 21:34:35 +0530

    Time: 8 ms (21.40%), Space: 7.1 MB (61.35%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+//     Runtime: 6 ms, faster than 27.80% of C++ online submissions for Sum of Square Numbers.
+// Memory Usage: 7.1 MB, less than 61.35% of C++ online submissions for Sum of Square Numbers.
+
+    
     bool judgeSquareSum(int c) {
         for(int i = 0; i <= sqrt(c); i++)
         {
