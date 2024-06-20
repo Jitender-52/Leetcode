d36bd8b6e1f3c1946d2b7cef2bd246d49e7d5a8a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-06-20 10:14:47 +0530

    Time: 107 ms (95.68%), Space: 61.5 MB (35.79%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    
+//     Runtime: 137 ms, faster than 16.37% of C++ online submissions for Magnetic Force Between Two Balls.
+// Memory Usage: 61.4 MB, less than 35.79% of C++ online submissions for Magnetic Force Between Two Balls.
+    
     int maxDistance(vector<int>& position, int m) {
         int n = position.size();
         sort(position.begin(), position.end());
-        int low = 0, high = 1e9;
+        int low = 0, high = (position[n-1] - position[0]) / (m - 1);
         while(low <= high)
         {
             int mid = (low + high) / 2;
