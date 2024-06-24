3687fdfd96dc872b1fb2f605fba881ca8997e296
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-06-24 20:47:26 +0530

    Time: 87 ms (72.54%), Space: 113.2 MB (34.84%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
-    int minKBitFlips(vector<int> &nums, int k)
-    {
+    
+//     Runtime: 90 ms, faster than 61.07% of C++ online submissions for Minimum Number of K Consecutive Bit Flips.
+// Memory Usage: 113.3 MB, less than 10.25% of C++ online submissions for Minimum Number of K Consecutive Bit Flips.
+
+    
+    int minKBitFlips(vector<int> &nums, int k){
         vector<int> v(nums.size(), false);
         int count = 0;
         int ans = 0;
