f5c8a69f98cb4a49fc4e03fc4bd97455f30744a1
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-06-30 22:46:36 +0530

    Time: 110 ms (87.50%), Space: 94.3 MB (100.00%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+    
+//     Runtime: 126 ms, faster than 37.50% of C++ online submissions for Find the Maximum Length of Valid Subsequence I.
+// Memory Usage: 94.5 MB, less than 75.00% of C++ online submissions for Find the Maximum Length of Valid Subsequence I.
+// Next challenges
+    
     int maximumLength(vector<int>& nums) {
         int n = nums.size();
         int odd = 0, even = 0;
         int ans = 0, count = 0;
         int totalOdd = 0, totalEven = 0;
-        for(int i = 0; i < n; i++) // Taking odd sum;
+        for(int i = 0; i < n; i++) // Either only odd or even 
         {
             totalOdd += nums[i] % 2;
             totalEven += !(nums[i] % 2);
@@ -14,7 +19,7 @@ public:
         ans = max(ans, count);
             
         int a = 1, count1 = 0, count2 = 0;
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n; i++) // odd, even, odd, even....
         {
             if(nums[i] % 2 == a)
             {
@@ -23,7 +28,7 @@ public:
             }
         }
         a = 0;
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n; i++) // even, odd, even, odd....
         {
             if(nums[i] % 2 == a)
             {
