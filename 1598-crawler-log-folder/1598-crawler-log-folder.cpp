9b1f0d41e8d5254aecd82ce94742317f5c19379e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-07-10 20:24:28 +0530

    Time: 0 ms (100.00%), Space: 14 MB (61.11%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Crawler Log Folder.
+// Memory Usage: 13.9 MB, less than 61.11% of C++ online submissions for Crawler Log Folder.
+    
     int minOperations(vector<string>& logs) {
         int ans = 0;
         for(auto i : logs)
