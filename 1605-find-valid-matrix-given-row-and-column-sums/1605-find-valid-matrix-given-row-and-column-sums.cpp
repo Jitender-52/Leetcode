db82393bae87ae6ee15c6b0ba8471ae9040159df
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-07-20 21:10:22 +0530

    Time: 44 ms (20.54%), Space: 36.2 MB (11.51%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 35 ms, faster than 73.82% of C++ online submissions for Find Valid Matrix Given Row and Column Sums.
+// Memory Usage: 36.1 MB, less than 11.51% of C++ online submissions for Find Valid Matrix Given Row and Column Sums.
+    
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
         int n = rowSum.size(), m = colSum.size();
         vector<vector<int>> ans(n, vector<int> (m, 0));
