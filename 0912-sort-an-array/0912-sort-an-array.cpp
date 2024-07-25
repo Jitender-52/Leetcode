9f0b6995ed3c338141f8122371f1ee8943ee185b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-07-25 19:15:57 +0530

    Time: 410 ms (33.51%), Space: 180 MB (23.10%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,21 +1,85 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-        map<int,int> mp;
-        for(int i = 0; i < n; i++)
-        {
-            mp[nums[i]]++;
-        }
+    
+    void merge(vector<int> &nums, int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        
+        vector<int> a(n1);
+        vector<int> b(n2);
+        for (int i = 0; i < n1; i++)
+            a[i] = nums[l + i];
+        for (int i = 0; i < n2; i++)
+            b[i] = nums[m + i + 1];
+
+        int i = 0;
         int j = 0;
-        for(auto i : mp)
+        int k = l;
+        while (i < n1 && j < n2)
         {
-            for(auto k = 0; k < i.second; k++)
+            if (a[i] < b[j])
             {
-                nums[j] = i.first;
+                nums[k] = a[i];
+                k++;
+                i++;
+            }
+            else
+            {
+                nums[k] = b[j];
+                k++;
                 j++;
             }
         }
+        while (i < n1)
+        {
+            nums[k] = a[i];
+            k++;
+            i++;
+        }
+        while (j < n2)
+        {
+            nums[k] = b[j];
+            k++;
+            j++;
+        }
+    }
+    
+    void mergeSort(vector<int> &nums, int l, int r)
+    {
+        if (l < r)
+        {
+            int m = ((l + r) / 2);
+            mergeSort(nums, l, m);
+            mergeSort(nums, m + 1, r);
+            merge(nums, l, m, r);
+        }
+    }
+    
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        mergeSort(nums, 0, n-1);
         return nums;
     }
 };
+
+
+//     vector<int> sortArray(vector<int>& nums) {
+//         int n = nums.size();
+//         map<int,int> mp;
+//         for(int i = 0; i < n; i++)
+//         {
+//             mp[nums[i]]++;
+//         }
+//         int j = 0;
+//         for(auto i : mp)
+//         {
+//             for(auto k = 0; k < i.second; k++)
+//             {
+//                 nums[j] = i.first;
+//                 j++;
+//             }
+//         }
+//         return nums;
+//     }
+// };
