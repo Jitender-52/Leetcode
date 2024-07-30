0c3b748db40af572249c9ee45f42671943fe3e21
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-07-30 15:47:07 +0530

    Time: 80 ms (81.18%), Space: 53.2 MB (14.35%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,65 +1,17 @@
 class Solution {
 public:
-    
     int minimumDeletions(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n+1, vector<int> (2, 0));
-        
-        for(int i = n-1; i >= 0; i--)
-        {
-            for(int j = 0; j < 2; j++)
-            {
-                if(s[i] == 'b')
-                {
-                    if(j == 0)
-                        dp[i][j] = dp[i+1][j];
-                    else
-                        dp[i][j] = min(1 + dp[i+1][j], dp[i+1][j-1]);
-                }
-                else
-                {
-                    if(j == 0)
-                        dp[i][j] = min(n-i, 1 + dp[i+1][j]);
-                    else
-                        dp[i][j] = dp[i+1][j];
-                }
-            }
-        }
-        return dp[0][1];
+        vector<int> a(n), b(n);
+        a[n-1] = s[n-1] == 'a';
+        b[0] = s[0] == 'b';
+        for(int i = 1; i < n; i++)
+            b[i] = b[i-1] + (s[i] == 'b');
+        for(int i = n-2; i >= 0; i--)
+            a[i] = a[i+1] + (s[i] == 'a');
+        int ans = min(a[0], b[n-1]);
+        for(int i = 1; i < n-1; i++)
+            ans = min(ans, a[i+1] + b[i-1]);
+        return ans;
     }
 };
-    
-//     Runtime: 722 ms, faster than 5.95% of C++ online submissions for Minimum Deletions to Make String Balanced.
-// Memory Usage: 258.1 MB, less than 5.12% of C++ online submissions for Minimum Deletions to Make String Balanced.
-    
-//     vector<vector<int>> dp;
-//     int minimum(int i, int j, string &s)
-//     {
-//         int n = s.length();
-//         if(i >= n)
-//             return 0;
-//         if(dp[i][j] != -1)
-//             return dp[i][j];
-//         if(s[i] == 'b')
-//         {
-//             if(j == 0)
-//                 return dp[i][j] = minimum(i+1, j, s);
-//             else
-//                 return dp[i][j] = min(1 + minimum(i+1, j, s), minimum(i+1, j-1, s));
-//         }
-//         else
-//         {
-//             if(j == 0)
-//                 return dp[i][j] = min(n - i, 1 + minimum(i+1, j, s)); // mybad used n-i-1
-//             else
-//                 return dp[i][j] = minimum(i+1, j, s);
-//         }
-//         // return dp[i][j] = minimum(i+1, j, s);
-//     }
-    
-//     int minimumDeletions(string s) {
-//         int n = s.length();
-//         dp.resize(n+1, vector<int> (2, -1));
-//         return minimum(0, 1, s);
-//     }
-// };
