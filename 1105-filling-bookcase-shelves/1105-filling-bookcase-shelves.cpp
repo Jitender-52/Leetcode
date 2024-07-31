628e758a6bb77cc7c64e6a376909dbb0825b1686
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-07-31 20:52:09 +0530

    Time: 3 ms (87.31%), Space: 11.2 MB (46.57%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Filling Bookcase Shelves.
+// Memory Usage: 11.2 MB, less than 46.57% of C++ online submissions for Filling Bookcase Shelves.
 
-    vector<int> memo;
-    int solve(vector<vector<int>>& books, int shelfWidth, int ind) 
+    vector<int> dp;
+    int solve(int idx, vector<vector<int>>& books, int shelfWidth) 
     {
-        if (ind == books.size())
+        int n = books.size();
+        if (idx == n)
             return 0;
-        if (memo[ind] != -1)
-            return memo[ind];
+        if (dp[idx] != -1)
+            return dp[idx];
         int ans = INT_MAX, maxH = 0, width = 0;
-        for (int i = ind; i < books.size(); i++) 
+        for (int i = idx; i < n; i++) 
         {
             width += books[i][0];
             if (width > shelfWidth) break;
             maxH = max(maxH, books[i][1]);
-            ans = min(ans, maxH + solve(books, shelfWidth, i + 1));
+            ans = min(ans, maxH + solve(i + 1, books, shelfWidth));
         }
-        return memo[ind] = ans;
+        return dp[idx] = ans;
     }
     
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
         int n = books.size();
-        memo.resize(n, -1);
-        return solve(books, shelfWidth, 0);
+        dp.resize(n, -1);
+        return solve(0, books, shelfWidth);
     }
 };
