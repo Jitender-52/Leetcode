e56b819fa8563dc7e6f78c08cb7bbb012e387b0e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-08-02 22:08:33 +0530

    Time: 52 ms (98.43%), Space: 83.1 MB (57.56%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 63 ms, faster than 80.35% of C++ online submissions for Minimum Swaps to Group All 1's Together II.
+// Memory Usage: 83.1 MB, less than 57.56% of C++ online submissions for Minimum Swaps to Group All 1's Together II.
+    
     int minSwaps(vector<int>& nums) {
         int s = nums.size();
         int ones = 0;
