b618c8dcc79e4d194b0fd2262be3c59a27ea70d3
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-08-03 21:34:33 +0530

    Time: 16 ms (10.57%), Space: 21.1 MB (6.79%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 11 ms, faster than 43.49% of C++ online submissions for Make Two Arrays Equal by Reversing Subarrays.
+// Memory Usage: 21.1 MB, less than 6.79% of C++ online submissions for Make Two Arrays Equal by Reversing Subarrays.
+    
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         map<int, int> mp1, mp2;
         for(auto i : target)
