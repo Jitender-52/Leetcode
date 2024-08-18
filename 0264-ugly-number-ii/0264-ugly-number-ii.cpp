a1fd57eec9047d99ee5d4b60b2c88b0525e71b22
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-08-18 19:31:33 +0530

    Time: 101 ms (9.82%), Space: 35.6 MB (7.76%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        priority_queue<long long,vector<long long>, greater<long long>> pq;
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
         pq.push(1);
-        int i = 1;
-        // vector<int> vis(1e6, 0);
-        map<long long,int> vis;
+        vector<int> v = {2, 3, 5};
+        set<int> st;
         while(1)
         {
-            int x = pq.top();
-            // cout << x << " ";
-            if(i == n)
-                return x;
-            pq.pop();
-            vector<int> v = {2, 3, 5};
-            i++;
-            if(pq.size() > n)
-                continue;
-            for(int i = 0; i < 3; i++)
+            n--;
+            if(n == 0)
+                return pq.top();
+            for(auto i : v)
             {
-                if(!vis[v[i] * 1LL * x])
+                if(st.find(i * pq.top()) == st.end())
                 {
-                    pq.push(v[i] * 1LL * x);
-                    vis[v[i] * 1LL * x] = 1;
+                    pq.push(i * pq.top());
+                    st.insert(i * pq.top());
                 }
             }
+            pq.pop();
         }
-        return -1;
+        return pq.top();
     }
 };
