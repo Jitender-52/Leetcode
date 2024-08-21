1dd32ae277c020c07c1b2cd24c813484d09df09b
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-08-21 21:57:50 +0530

    Time: 61 ms (25.19%), Space: 12 MB (17.51%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 57 ms, faster than 29.34% of C++ online submissions for Strange Printer.
+// Memory Usage: 12.2 MB, less than 17.20% of C++ online submissions for Strange Printer.
+    
   int dp[1001][1001];
     int f(int i, int j, string &s)
     {
