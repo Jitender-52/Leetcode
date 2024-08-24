4b6351e23a5f5b3e0e047ed2e1e04fca3e3c167d
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-08-24 21:10:49 +0530

    Time: 4 ms (34.51%), Space: 9.3 MB (32.16%) - LeetHub

---
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string nearestPalindromic(string s) {
+        int n = s.length();
+        if (n == 1)
+            return to_string(stol(s) - 1);
+
+        vector<long long> v;
+
+        v.push_back(pow(10, n - 1) - 1);
+        v.push_back(pow(10, n) + 1);
+        long long prefix = stol(s.substr(0, (n + 1) / 2));
+        vector<long long> nearPrefixes = {prefix - 1, prefix, prefix + 1};
+
+        for (auto it : nearPrefixes)
+        {
+            string prefix = to_string(it);
+            if (n % 2)
+                prefix.pop_back();
+            reverse(prefix.begin(), prefix.end());
+            v.push_back(stol(to_string(it) + prefix));
+        }
+
+        long long mini = LONG_MAX;
+        long long close = 0;
+        for (auto it : v)
+        {
+            long long diff = abs(it - stol(s));
+            if (mini > diff && it != stol(s))
+            {
+                mini = diff;
+                close = it;
+            }
+        }
+        return to_string(close);
+    }
+};
