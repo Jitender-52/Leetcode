57be04e06ea453c562775e0a0645058eb563ebb0
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-09-01 21:09:18 +0530

    Time: 67 ms (95.32%), Space: 88.1 MB (55.59%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 67 ms, faster than 95.32% of C++ online submissions for Convert 1D Array Into 2D Array.
+// Memory Usage: 88.1 MB, less than 55.59% of C++ online submissions for Convert 1D Array Into 2D Array.
+    
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         if(m * n != original.size())
             return {};
