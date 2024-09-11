12cf8e77f3c383d317ff88b6cc33ad35806eb23e
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-09-11 21:26:00 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (93.51%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+
+//         Runtime: 0 ms, faster than 100.00% of C++ online submissions for Minimum Bit Flips to Convert Number.
+// Memory Usage: 7.6 MB, less than 44.87% of C++ online submissions for Minimum Bit Flips to Convert Number.
+    
     int minBitFlips(int start, int goal) {
         int ans = 0;
         for(int i = 0; i < 31; i++)
