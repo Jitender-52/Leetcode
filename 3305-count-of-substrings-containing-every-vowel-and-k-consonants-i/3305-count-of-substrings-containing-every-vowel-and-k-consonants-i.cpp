0925be3b0bc4984c1dc618a21582125dfca76855
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-09-29 22:04:24 +0530

    Time: 6 ms (100.00%), Space: 10.8 MB (72.73%) - LeetHub

--- 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -1,39 +1,97 @@
 class Solution {
 public:
     
-//     Runtime: 157 ms, faster than 18.18% of C++ online submissions for Count of Substrings Containing Every Vowel and K Consonants I.
-// Memory Usage: 31 MB, less than 9.09% of C++ online submissions for Count of Substrings Containing Every Vowel and K Consonants I.
-    
     bool isVowel(char c)
     {
-        string s = "aeiou";
-        for(auto i : s)
-        {
-            if(i == c)
-                return true;
-        }
-        return false;
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     }
     
-    int countOfSubstrings(string word, int k) {
+    long long countOfSubstrings(string word, int k) {
         int n = word.size();
-        int ans = 0;
-        for(int i = 0; i < n; i++)
+        int lastConsonant = n;
+        vector<int> nextConsonant(n);
+        for(int i = n-1; i >= 0; i--)
         {
-            string s = "";
-            set<int> st;
-            int consonant = 0;
-            for(int j= i; j < n; j++)
+            nextConsonant[i] = lastConsonant;
+            if(!isVowel(word[i]))
+                lastConsonant = i;
+        }
+        
+        map<char, int> vowels;
+        long long ans = 0;
+        int consonant = 0;
+        int i = 0, j = 0;
+        while(i < n)
+        {
+            if(isVowel(word[i]))
+                vowels[word[i]]++;
+            else
+                consonant++;
+            
+            while(j <= i && consonant > k)
             {
-                s += word[i];
                 if(isVowel(word[j]))
-                    st.insert(word[j]);
+                {
+                    vowels[word[j]]--;
+                    if(vowels[word[j]] == 0) 
+                        vowels.erase(word[j]);
+                }
                 else
-                    consonant++;
-                if(st.size() == 5 && consonant == k)
-                    ans++;
+                    consonant--;
+                j++;
             }
+            while(j < i && vowels.size() == 5 && consonant == k)
+            {
+                ans += (nextConsonant[i] - i);
+                if (isVowel(word[j])) 
+                {
+                    vowels[word[j]]--;
+                    if(vowels[word[j]] == 0) 
+                        vowels.erase(word[j]);
+                } 
+                else 
+                    consonant--;
+                j++;
+            }
+            i++;
         }
         return ans;
     }
 };
+    
+//     Runtime: 157 ms, faster than 18.18% of C++ online submissions for Count of Substrings Containing Every Vowel and K Consonants I.
+// Memory Usage: 31 MB, less than 9.09% of C++ online submissions for Count of Substrings Containing Every Vowel and K Consonants I.
+    
+//     bool isVowel(char c)
+//     {
+//         string s = "aeiou";
+//         for(auto i : s)
+//         {
+//             if(i == c)
+//                 return true;
+//         }
+//         return false;
+//     }
+    
+//     int countOfSubstrings(string word, int k) {
+//         int n = word.size();
+//         int ans = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             string s = "";
+//             set<int> st;
+//             int consonant = 0;
+//             for(int j= i; j < n; j++)
+//             {
+//                 s += word[i];
+//                 if(isVowel(word[j]))
+//                     st.insert(word[j]);
+//                 else
+//                     consonant++;
+//                 if(st.size() == 5 && consonant == k)
+//                     ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
