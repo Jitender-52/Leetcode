b5a6da9080b7c1c082fd9e59cbc2da4a9cedb2de
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-09-29 16:35:42 +0530

    Time: 183 ms (18.18%), Space: 31 MB (9.09%) - LeetHub

--- 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     
+//     Runtime: 157 ms, faster than 18.18% of C++ online submissions for Count of Substrings Containing Every Vowel and K Consonants I.
+// Memory Usage: 31 MB, less than 9.09% of C++ online submissions for Count of Substrings Containing Every Vowel and K Consonants I.
+    
     bool isVowel(char c)
     {
         string s = "aeiou";
@@ -24,13 +27,9 @@ public:
             {
                 s += word[i];
                 if(isVowel(word[j]))
-                {
                     st.insert(word[j]);
-                }
                 else
-                {
                     consonant++;
-                }
                 if(st.size() == 5 && consonant == k)
                     ans++;
             }
