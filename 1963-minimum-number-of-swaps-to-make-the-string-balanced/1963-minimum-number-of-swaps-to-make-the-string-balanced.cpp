21a6bc10b84ea9732331c4adeb28bf102bc579fd
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-10-08 18:29:36 +0530

    Time: 98 ms (80.19%), Space: 32.6 MB (91.34%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 101 ms, faster than 70.15% of C++ online submissions for Minimum Number of Swaps to Make the String Balanced.
+// Memory Usage: 32.7 MB, less than 79.33% of C++ online submissions for Minimum Number of Swaps to Make the String Balanced.
+    
     int minSwaps(string s) {
         int ans = 0, opening = 0, closing = 0;
         for(auto i : s)
@@ -15,9 +19,7 @@ public:
                     opening--;
             }
             else
-            {
                   opening++;
-            }
         }
         return ans;
     }
