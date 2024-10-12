a10032cdd4555e58268dc21f7648b5ef1df5feaa
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-10-12 21:32:56 +0530

    Time: 293 ms (55.86%), Space: 92.7 MB (81.98%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    static bool cmp(vector<int> &a, vector<int> &b)
+    {
+        if(a[0] == b[0])
+        {
+            return a[1] > b[1];
+        }
+        return a[0] < b[0];
+    }
+    
+    int minGroups(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), cmp);
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        // for(int i = 0; i < n; i++)
+        // {
+        //     for(int j = 0; j < 2; j++)
+        //     {
+        //         cout << intervals[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // cout << endl;
+        
+        int mini = intervals[0][1];
+        pq.push(mini);
+        // pq.push(-1);
+        int count = 1;
+        
+        for(int i = 1; i < n; i++)
+        {
+            // if(i == 1)
+            // {
+            //     cout << pq.top() << " ";
+            // }
+            if(intervals[i][0] > pq.top())
+            {
+                pq.pop();
+                pq.push(intervals[i][1]);
+            }
+            else
+            {
+                pq.push(intervals[i][1]);
+                count++;
+            }
+        }
+        return count;
+    }
+};
