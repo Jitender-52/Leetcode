1b64e7b48b10e687e2a39ee720b0ac118c4a433a
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-10-17 21:22:41 +0530

    Time: 0 ms (100.00%), Space: 8 MB (12.29%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Maximum Swap.
+// Memory Usage: 7.8 MB, less than 12.29% of C++ online submissions for Maximum Swap.
+    
     int maximumSwap(int num) {
         string s = to_string(num);
         int n = s.size();
