c88fcac548109ecb26ffc30e77e0c7d855ce50db
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-10-23 21:21:49 +0530

    Time: 138 ms (91.98%), Space: 487.3 MB (5.57%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        queue<TreeNode *> q;
+        q.push(root);
+        root->val = 0;
+        while(!q.empty())
+        {
+            int n = q.size();
+            queue<TreeNode *> q1;
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                auto it = q.front();
+                q.pop();
+                q1.push(it);
+                if(it->left)
+                {
+                    sum += it->left->val;
+                    q.push(it->left);
+                }
+                if(it->right)
+                {
+                    sum += it->right->val;
+                    q.push(it->right);
+                }
+            }
+            for(int i = 0; i < n; i++)
+            {
+                auto it = q1.front();
+                q1.pop();
+                int sum2 = 0;
+                if(it->left)
+                    sum2 += it->left->val;
+                if(it->right)
+                    sum2 += it->right->val;
+                if(it->left)
+                    it->left->val = sum - sum2;
+                if(it->right)
+                    it->right->val = sum - sum2;
+            }
+        }
+        return root;
+    }
+};
