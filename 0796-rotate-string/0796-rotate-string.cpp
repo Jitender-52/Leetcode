d7b086e5c15ab906f3e1596106bc1bf4134678b8
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-11-03 21:14:06 +0530

    Time: 1 ms (18.38%), Space: 8.8 MB (16.05%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Rotate String.
+// Memory Usage: 8.8 MB, less than 10.19% of C++ online submissions for Rotate String.
+    
     bool rotateString(string s, string goal) {
         int n = s.length();
         int m = goal.length();
