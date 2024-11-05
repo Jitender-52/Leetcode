7c12d88aa184d540c16649f8430ce44f0ad49ee3
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-11-05 20:50:18 +0530

    Time: 7 ms (13.83%), Space: 12.3 MB (16.88%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Minimum Number of Changes to Make Binary String Beautiful.
+// Memory Usage: 12.2 MB, less than 16.88% of C++ online submissions for Minimum Number of Changes to Make Binary String Beautiful.
+    
     int minChanges(string s) {
         int n = s.length(), ans = 0;
         for(int i = 0; i < n; i++)
