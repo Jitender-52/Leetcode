cd6faef081275cdd1e9d5ec8439f89c0f2f7c0b3
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-11-24 20:51:56 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (12.50%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Maximum Matrix Sum.
+// Memory Usage: 42.9 MB, less than 8.11% of C++ online submissions for Maximum Matrix Sum.
+    
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         long long ans = 0, mini = INT_MAX, count = 0;
         for(auto v : matrix)
