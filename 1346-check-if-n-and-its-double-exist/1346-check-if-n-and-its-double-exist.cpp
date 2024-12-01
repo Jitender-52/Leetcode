5553aca227d5037956cc9ebf075b567af8d907d8
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-12-01 05:43:22 +0530

    Time: 0 ms (100.00%), Space: 13.9 MB (15.21%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        map<int,int> mp;
+        for(auto i : arr)
+        {
+            if(mp.find(2 * i) != mp.end() || (i % 2 == 0 && mp.find(i/2) != mp.end()))
+                return true;
+            mp[i]++;
+        }
+        return false;
+    }
+};
