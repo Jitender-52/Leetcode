289d4d128febdc749a4f50b44b58952286601dfd
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-12-09 21:02:46 +0530

    Time: 83 ms (10.16%), Space: 140.6 MB (5.15%) - LeetHub

---
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        
+        set<int> l, r;
+        l.insert(-1);
+        l.insert(n+2);
+        r.insert(n+2);
+        
+        for(int i = 1; i < n; i++)
+        {
+            if(nums[i] % 2 == nums[i-1] % 2)
+            {
+                l.insert(i-1);
+                // r.insert(i);
+            }
+        }
+        
+        vector<bool> ans(queries.size());
+        int count = 0;
+        for(auto v : queries)
+        {
+            int x = v[0], y = v[1];
+            auto it = *l.lower_bound(x);
+            if(x == y)
+            {
+                ans[count++] = 1;
+                continue;
+            }
+            if(it == x)
+            {
+                if(nums[x] % 2 == nums[x+1] % 2)
+                    ans[count++] = 0;
+                else
+                    ans[count++] = 1;
+            }
+            else if(it == y)
+            {
+                if(nums[y] % 2 == nums[y-1] % 2)
+                    ans[count++] = 0;
+                else
+                    ans[count++] = 1;
+            }
+            else if(it >= x && it <= y)
+            {
+                // cout << it << endl;
+                ans[count++] = 0;
+            }
+            else
+            {
+                ans[count++] = 1;
+            }
+        }
+        return ans;
+    }
+};
