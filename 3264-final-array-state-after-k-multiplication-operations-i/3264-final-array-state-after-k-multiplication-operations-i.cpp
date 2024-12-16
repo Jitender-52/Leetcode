159773836349a9dfc760076ac0d54df2f0152e00
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-12-16 20:09:22 +0530

    Time: 3 ms (30.95%), Space: 27.2 MB (45.93%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 13 ms, faster than 30.00% of C++ online submissions for Final Array State After K Multiplication Operations I.
+// Memory Usage: 26.2 MB, less than 80.00% of C++ online submissions for Final Array State After K Multiplication Operations I.
+    
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int n = nums.size();
         for(int i = 0; i < k; i++)
