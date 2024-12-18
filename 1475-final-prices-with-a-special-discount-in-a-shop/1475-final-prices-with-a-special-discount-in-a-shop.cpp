dfcac26450ce27244a86694767a1552f50a697d9
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-12-18 21:15:03 +0530

    Time: 0 ms (100.00%), Space: 14.3 MB (9.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Final Prices With a Special Discount in a Shop.
+// Memory Usage: 14.4 MB, less than 9.86% of C++ online submissions for Final Prices With a Special Discount in a Shop.
+    
     vector<int> finalPrices(vector<int>& prices) {
         vector<int> ans = prices;
         stack<int> st;
