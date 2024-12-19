9baa2d3726dcdf9e421bd234dfb4f19b40a5ef0f
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-12-19 21:12:10 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (35.22%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     
-//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Max Chunks To Make Sorted.
-// Memory Usage: 9.4 MB, less than 7.32% of C++ online submissions for Max Chunks To Make Sorted.
-    
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
-        set<int> st;
-        int ans = 0;
+        int maxi = 0, ans = 0;
         for(int i = 0; i < n; i++)
         {
-            st.insert(arr[i]);
-            if(*st.rbegin() == i)
+            maxi = max(maxi, arr[i]);
+            if(maxi == i)
                 ans++;
         }
         return ans;
     }
 };
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Max Chunks To Make Sorted.
+// Memory Usage: 9.4 MB, less than 7.32% of C++ online submissions for Max Chunks To Make Sorted.
+    
+//     int maxChunksToSorted(vector<int>& arr) {
+//         int n = arr.size();
+//         set<int> st;
+//         int ans = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             st.insert(arr[i]);
+//             if(*st.rbegin() == i)
+//                 ans++;
+//         }
+//         return ans;
+//     }
+// };
