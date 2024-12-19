d1d347f118eca81708f8ee6b0f1961c4227108e0
Jitender <97593224+Jitender-52@users.noreply.github.com>
2024-12-19 21:10:47 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (7.32%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+//     Runtime: 0 ms, faster than 100.00% of C++ online submissions for Max Chunks To Make Sorted.
+// Memory Usage: 9.4 MB, less than 7.32% of C++ online submissions for Max Chunks To Make Sorted.
+    
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
         set<int> st;
